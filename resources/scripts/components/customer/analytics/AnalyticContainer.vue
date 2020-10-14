6713c5d73e3bcc209a97715acc463b914a65b164
Didar <didar@enkaizen.com>
2020-10-14 14:14:10 +0600

    working on Customer Insight UI

--- resources/scripts/components/customer/analytics/AnalyticContainer.vue
+++ resources/scripts/components/customer/analytics/AnalyticContainer.vue
@@ -1,28 +1,31 @@
 <template>
-    <v-row>
-        <v-col sm="8">
-            <h2>Customer Insight</h2>
-        </v-col>
-        <v-col sm="4">
-            <date-range-picker/>
-        </v-col>
-        <v-col md="8">
-            <v-row>
-                <v-col lg="4" md="6" sm="12" class="box">
-                    <InfoCard icon="mdi-account-outline" title="New Users" value="37"></InfoCard>
-                </v-col>
-                <v-col lg="4" md="6" sm="12" class="box">
-                    <InfoCard icon="mdi-email-send" title="Message Sent" value="1281"></InfoCard>
-                </v-col>
-                <v-col lg="4" md="6" sm="12" class="box">
-                    <InfoCard icon="mdi-email-receive-outline" title="Message Received" value="1894"></InfoCard>
-                </v-col>
-            </v-row>
-        </v-col>
-        <v-col md="4">
-            <GenderChart/>
-        </v-col>
-    </v-row>
+    <v-card class="pa-4 purple darken-2 white--text">
+        <v-row>
+            <v-col sm="8">
+                <h2>Customer Insight</h2>
+            </v-col>
+            <v-col sm="4" class="white--text">
+                <date-range-picker/>
+            </v-col>
+            <v-col md="8">
+                <v-row>
+                    <v-col lg="4" md="6" sm="12" class="box">
+                        <InfoCard icon="mdi-account-outline" title="New Users" value="37"></InfoCard>
+                    </v-col>
+                    <v-col lg="4" md="6" sm="12" class="box">
+                        <InfoCard icon="mdi-email-send" title="Message Sent" value="1281"></InfoCard>
+                    </v-col>
+                    <v-col lg="4" md="6" sm="12" class="box">
+                        <InfoCard icon="mdi-email-receive-outline" title="Message Received" value="1894"></InfoCard>
+                    </v-col>
+                </v-row>
+            </v-col>
+            <v-col md="4">
+                <GenderChart/>
+            </v-col>
+        </v-row>
+    </v-card>
+
 </template>
 
 <script>
