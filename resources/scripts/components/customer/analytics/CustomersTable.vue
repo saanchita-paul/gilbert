f26f49bb70cc1c3c1c48ab8b767946971ae01957
lenin <justtlenin@gmail.com>
2020-10-14 17:45:37 +0600

    updated customer data table

--- resources/scripts/components/customer/analytics/CustomersTable.vue
+++ resources/scripts/components/customer/analytics/CustomersTable.vue
@@ -8,7 +8,28 @@
             :headers="headers"
             :items="desserts"
             :search="search"
-        ></v-data-table>
+        >
+            <template
+                v-slot:body="{ items }"
+            >
+                <tbody>
+                <tr
+                    v-for="(item, index) in items"
+                    :key="index"
+                >
+                    <td>{{ item.id }}</td>
+                    <td>{{item.full_name}}</td>
+                    <td>{{item.time_zone}}</td>
+                    <td>{{item.emotion}}</td>
+                    <td>{{item.nps}}</td>
+                    <td>{{item.last_active}}</td>
+                    <td class="d-flex justify-center">
+                        <v-icon @click="openConversation(item.id)">mdi-message-arrow-right</v-icon>
+                    </td>
+                </tr>
+                </tbody>
+            </template>
+        </v-data-table>
     </v-card>
 </template>
 
@@ -19,101 +40,67 @@ export default {
             search: '',
             headers: [
                 {
-                    text: 'Dessert (100g serving)',
+                    text: '#ID',
                     align: 'start',
                     filterable: false,
-                    value: 'name',
+                    value: 'id',
                 },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Iron (%)', value: 'iron' },
+                { text: 'Name', value: 'full_name' },
+                { text: 'Time Zone', value: 'time_zone' },
+                { text: 'Emotions', value: 'emotion' },
+                { text: 'NPS', value: 'nps' },
+                { text: 'Last Active', value: 'last_active' },
+                { text: 'Go to Conversation', value: 'last_active', align: 'center' },
             ],
             desserts: [
                 {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%',
+                    id: 1245784,
+                    full_name: 'MR Sku',
+                    time_zone: 'GTM +6',
+                    emotion: 24,
+                    nps: 4.0,
+                    last_active: '10 minutes ago',
                 },
                 {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: '1%',
+                    id: 1245784,
+                    full_name: 'MR Sku',
+                    time_zone: 'GTM +6',
+                    emotion: 24,
+                    nps: 4.0,
+                    last_active: '10 minutes ago',
                 },
                 {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: '7%',
+                    id: 1245784,
+                    full_name: 'MR Sku',
+                    time_zone: 'GTM +6',
+                    emotion: 24,
+                    nps: 4.0,
+                    last_active: '10 minutes ago',
                 },
                 {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: '8%',
+                    id: 1245784,
+                    full_name: 'MR Sku',
+                    time_zone: 'GTM +6',
+                    emotion: 24,
+                    nps: 4.0,
+                    last_active: '10 minutes ago',
                 },
                 {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: '16%',
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: '0%',
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: '2%',
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    iron: '45%',
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    iron: '22%',
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    iron: '6%',
+                    id: 1245784,
+                    full_name: 'MR Sku',
+                    time_zone: 'GTM +6',
+                    emotion: 24,
+                    nps: 4.0,
+                    last_active: '10 minutes ago',
                 },
             ],
         }
     },
+    methods: {
+        openConversation(id) {
+            this.$router.push({name: `messenger`, query: {customer: id}})
+        }
+    }
 }
 </script>
 <style scoped>
