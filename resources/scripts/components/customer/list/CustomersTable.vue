5f84cb5db485ffcfb71eba6dce7fbe77bbbe1c05
lenin <justtlenin@gmail.com>
2020-10-20 11:09:50 +0600

    fixed route abd avatar alignment

--- resources/scripts/components/customer/list/CustomersTable.vue
+++ resources/scripts/components/customer/list/CustomersTable.vue
@@ -103,7 +103,7 @@ export default {
     },
     methods: {
         openConversation(id) {
-            this.$router.push({name: `messenger.customers`, query: {customer: id}})
+            this.$router.push({name: `customers.conversation`, query: {customer: id}})
         }
     }
 }
