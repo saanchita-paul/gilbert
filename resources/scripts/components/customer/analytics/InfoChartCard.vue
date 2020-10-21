e4b255cbc9254a4557d9888c928cd1df06f601eb
lenin <justtlenin@gmail.com>
2020-10-21 16:28:32 +0600

    updated bar chart

--- resources/scripts/components/customer/analytics/InfoChartCard.vue
+++ resources/scripts/components/customer/analytics/InfoChartCard.vue
@@ -5,9 +5,9 @@
                 <h5>Total messages</h5>
                 <p class="mb-0 app-title-big text--primary">15,940 <span class="app-title-small"></span> </p>
             </div>
-            <v-divider></v-divider>
-            <div class="chart" style="height: 60px">
-                <canvas  height="50px" :id="chartId"></canvas>
+            <v-divider class="mb-2"></v-divider>
+            <div class="chart" style="height: 70px">
+                <canvas  height="70px" :id="chartId"></canvas>
             </div>
         </div>
     </v-card>
@@ -20,24 +20,32 @@ export default {
     props: ['chartId'],
     data() {
         return {
+            chart: {
+                data: [90, 70, 62, 80, 50, 88, 80, 30, 40, 50],
+                labels: ["0", "1", "2", '3', '4', '5', '6', '7', '8', '9']
+            }
         }
     },
     mounted() {
         const ctx = document.getElementById(this.chartId);
-        var myBarChart = new Chart(ctx, {
+        const allColor = ['rgba(92, 34, 154, .9)', 'rgba(92, 34, 154, .5)'];
+        let barColors = [];
+        let next = 0;
+        this.chart.data.map(() => {
+            barColors.push(allColor[next]);
+            next = next ? 0 : 1;
+        })
+        const myBarChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: [
-                    "Foo",
-                    "Bar",
-                    "Baz"
-                ],
+                labels: [...this.chart.labels],
                 datasets: [{
-                    barPercentage: 1,
+                    barPercentage: .5,
                     barThickness: 10,
                     maxBarThickness: 10,
-                    minBarLength: 2,
-                    data: [90, 70, 62, 80, 50, 88, 80],
+                    minBarLength: 50,
+                    data: [...this.chart.data],
+                    backgroundColor: [...barColors]
                 }]
             },
             options: {
