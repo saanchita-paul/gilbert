0eaf02f4dfb37965bc1279c1bd8369c42f0320cb
Lenin <justtlenin@gmail.com>
2020-10-22 09:18:56 +0600

    updated InfoCard

--- resources/scripts/components/customer/analytics/InfoCard.vue
+++ resources/scripts/components/customer/analytics/InfoCard.vue
@@ -2,12 +2,12 @@
     <v-card class="white black--text" style="position: relative">
         <v-row class="px-4 py-3">
             <v-col cols="8" class="pb-0">
-                <p  class="info-value  primary--text" v-text="value"></p>
+                <p  class="info-value pa-0 ma-0 primary--text" v-text="value"></p>
             </v-col>
             <v-col cols="8" class="pb-0">
                 <p class="app-title-small primary--text" v-text="title"></p>
             </v-col>
-            <v-icon  class="card-icon" color="primary" size="80">{{icon}}</v-icon>
+            <v-icon  class="card-icon" color="primary" size="100">{{icon}}</v-icon>
         </v-row>
         <!-- <div class="box d-flex justify-space-between align-content-center">
             <div class="d-flex flex-column justify-space-around" style="height: 100%">
@@ -48,6 +48,6 @@ export default {
     opacity: .2;
     position: absolute;
     bottom: 0px;
-    right: 0px
+    right: 5px
 }
 </style>
