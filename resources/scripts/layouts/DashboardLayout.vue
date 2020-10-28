09f4161aa93a9b908b4cb965e399030f1ac1dee9
lenin <justtlenin@gmail.com>
2020-10-28 17:17:30 +0600

    added expandable side menu

--- resources/scripts/layouts/DashboardLayout.vue
+++ resources/scripts/layouts/DashboardLayout.vue
@@ -5,12 +5,12 @@
             :mini-variant="miniDrawer"
             mini-variant-width="80"
             app
-            style="background-color: rgba(92, 34, 154, .6)"
+            style="background-color: #444348"
             class="navigation-drawer-menu"
         >
-            <v-list-item  style="padding-bottom: 8px !important; background-color:rgba(92, 34, 154, .6)">
+            <v-list-item style="padding-bottom: 8px !important; background-color: white">
                 <v-list-item-avatar class="ml-0">
-                        <v-img src="/assets/images/logo/hood-small.png" />
+                    <v-img src="/assets/images/logo/hood-small.png"/>
                 </v-list-item-avatar>
 
                 <v-list-item-content>
@@ -23,14 +23,30 @@
             <v-list dense class="main-nav-items">
                     <span class="mb-6" v-for="r in routes">
                         <v-list-item
-                                     :to="{name: r.route_name}"
-                                     link
-                                     exact-active-class="app-active-nav white--text"
-                                     exact
-                                     class="pa-2 app-nav"
-                                     :key="r.title"
+                            v-if="r.type ==='group'"
+                            exact
+                            link
+                            @click="onMenuClick(r)"
+                            exact-active-class="app-active-nav white--text"
+                            class="pa-2 app-nav"
+                            :key="r.title"
                         >
-                        <v-list-item-icon><v-icon size="35" color="white">{{r.icon}}</v-icon></v-list-item-icon>
+                            <v-list-item-icon><v-icon size="35" color="white">{{ r.icon }}</v-icon></v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ r.title }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item
+                            v-else
+                            :to="{name: r.route_name}"
+                            exact
+                            link
+                            @click="onMenuClick(r)"
+                            exact-active-class="app-active-nav white--text"
+                            class="pa-2 app-nav"
+                            :key="r.title"
+                        >
+                        <v-list-item-icon><v-icon size="35" color="white">{{ r.icon }}</v-icon></v-list-item-icon>
                         <v-list-item-content>
                             <v-list-item-title>{{ r.title }}</v-list-item-title>
                         </v-list-item-content>
@@ -41,9 +57,9 @@
 
         <v-app-bar app class="white">
             <v-toolbar-title class="primary--text">Hood</v-toolbar-title>
-<!--            <v-btn icon color="primary" @click="miniDrawer = !miniDrawer">-->
-<!--                <v-icon>{{ miniDrawer ? 'mdi-menu' : 'mdi-menu-open' }}</v-icon>-->
-<!--            </v-btn>-->
+            <!--            <v-btn icon color="primary" @click="miniDrawer = !miniDrawer">-->
+            <!--                <v-icon>{{ miniDrawer ? 'mdi-menu' : 'mdi-menu-open' }}</v-icon>-->
+            <!--            </v-btn>-->
             <!--            <v-app-bar-nav-icon @click="miniDrawer = !miniDrawer"></v-app-bar-nav-icon>-->
             <!--            <v-toolbar-title>Hood</v-toolbar-title>-->
             <v-spacer/>
@@ -86,8 +102,40 @@
 
         </v-app-bar>
 
-        <v-main class="body-bg">
-            <router-view></router-view>
+        <v-main  class="body-bg" style="position: relative">
+            <v-slide-x-transition>
+                <v-navigation-drawer
+                    v-if="selectedMenus && subMenuDrawer"
+                    width="170px"
+                    style="background-color: white; border-radius: 8px; position: fixed; left: 82px; top:70px"
+                >
+                    <v-list
+                        dense
+                        nav
+                    >
+                        <v-list-item
+                            v-for="r in selectedMenus"
+                            :key="r.title"
+                            :to="{name: r.route_name}"
+                            exact
+                            @click="onSubmenuClick(r)"
+                        >
+                            <v-list-item-content>
+                                <v-list-item-title>{{ r.title }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-slide-x-transition>
+
+            <v-row>
+                <v-col v-if="selectedMenus" md="2" >
+                </v-col>
+                    <v-col :md="selectedMenus ? '10' : '12'">
+                        <router-view></router-view>
+                    </v-col>
+            </v-row>
+
         </v-main>
     </v-app>
 </template>
@@ -102,6 +150,8 @@ export default {
     name: "DashboardLayout",
     data() {
         return {
+            subMenuDrawer: false,
+            selectedMenus: null,
             drawer: true,
             miniDrawer: true,
             routes: ApplicationService.getMainNavigationRoutes()
@@ -110,6 +160,21 @@ export default {
     mounted() {
         setInterval(AuthService.authUser, 300000)
         initializeBroadcasting(getAuthUser().id);
+    },
+    methods: {
+        onMenuClick(menu) {
+            if (menu.type === 'route') {
+                this.$router.push({name: menu.route_name})
+                this.selectedMenus = null;
+                this.subMenuDrawer = true;
+            } else {
+                this.selectedMenus = menu.children;
+                this.subMenuDrawer = true;
+            }
+        },
+        onSubmenuClick(menu) {
+            this.$router.push({name: menu.route_name})
+        }
     }
 }
 </script>
