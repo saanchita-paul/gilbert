b90ce657a9da19cb5afe864c2b9607fe80f0f0a2
Lenin <justtlenin@gmail.com>
2020-11-12 13:18:49 +0600

    basic customer details page design

---
+++ resources/scripts/pages/customer/customer-profile/CustomerProfilePage.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-container>
+        <page-header :breadcrumbs="getBreadcrumbs" title="Customer Details">
+            <date-range-picker v-model="dateRange" />
+        </page-header>
+    </v-container>
+</template>
+
+<script>
+import PageHeader from "@scripts/components/common/PageHeader";
+import DateRange from "@scripts/models/DateRange";
+import DateRangePicker from "@scripts/components/customer/analytics/DateRangePicker";
+
+export default {
+    name: "CustomerProfilePage",
+    components: { PageHeader, DateRangePicker },
+    props: ['customerId'],
+    computed: {
+        getBreadcrumbs() {
+            return [
+                {
+                    text: 'Customer',
+                    disabled: false,
+                    route_name: 'dashboard',
+                },
+                {
+                    text: 'List',
+                    disabled: false,
+                    route_name: 'customers.list',
+                },                {
+                    text: this.customerId,
+                    disabled: false,
+                    route_name: 'customers.profile',
+                },
+            ]
+        }
+    },
+    data() {
+        return {
+            dateRange: new DateRange()
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
