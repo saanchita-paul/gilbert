874e6eeabf4a9b6247103ccf67e8a3ac2f46c29d
Tonmoy Enkaizen <tonmoy@enkaizen.com>
2021-03-22 16:03:46 +0600

    Customers table create

---
+++ resources/scripts/pages/customer/CustomerListPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <CustomersNewTable class="mt-2"/>
+    </v-container>
+</template>
+
+<script>
+import CustomersNewTable from "@scripts/components/customer/CustomersNewTable";
+
+export default {
+    name: "CustomerInsightPage",
+    components: {CustomersNewTable},
+}
+</script>
+
+<style scoped>
+
+</style>
