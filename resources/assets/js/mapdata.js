2d5b43996301dbac401c8764e90ab3e79a875375
Lenin <justtlenin@gmail.com>
2021-03-24 13:52:33 +0600

    location insight

---
+++ resources/assets/js/mapdata.js
@@ -0,0 +1,143 @@
+var simplemaps_australiamap_mapdata = {
+    main_settings: {
+        //General settings
+        width: "130", //or 'responsive'
+        background_color: "#FFFFFF",
+        background_transparent: "yes",
+        border_color: "#ffffff",
+        pop_ups: "detect",
+
+        //State defaults
+        state_description: "",
+        state_color: "#E0E0E0",
+        state_opacity: 1,
+        state_hover_color: "#BB6BD9",
+        state_url: null,
+        border_size: 1.5,
+        all_states_inactive: "no",
+        all_states_zoomable: "false",
+
+        //Location defaults
+        location_description: "Location description",
+        location_color: "#FF0067",
+        location_opacity: 0.8,
+        location_hover_opacity: 1,
+        location_url: "",
+        location_size: 25,
+        location_type: "square",
+        location_image_source: "frog.png",
+        location_border_color: "#FFFFFF",
+        location_border: 2,
+        location_hover_border: 2.5,
+        all_locations_inactive: "no",
+        all_locations_hidden: "no",
+
+        //Label defaults
+        label_color: "#d5ddec",
+        label_hover_color: "#d5ddec",
+        label_size: 22,
+        label_font: "Arial",
+        hide_labels: "no",
+        hide_eastern_labels: "no",
+
+        //Zoom settings
+        zoom: "yes",
+        back_image: "no",
+        initial_back: "no",
+        initial_zoom: "-1",
+        initial_zoom_solo: "no",
+        region_opacity: 1,
+        region_hover_opacity: 0.6,
+        zoom_out_incrementally: "yes",
+        zoom_percentage: 0.99,
+        zoom_time: 0.5,
+
+        //Popup settings
+        popup_color: "white",
+        popup_opacity: 0.9,
+        popup_shadow: 1,
+        popup_corners: 5,
+        popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+        popup_nocss: "no",
+
+        //Advanced settings
+        div: "location_insight",
+        auto_load: "yes",
+        url_new_tab: "no",
+        images_directory: "default",
+        fade_time: 0.1,
+        link_text: "View Website"
+    },
+    state_specific: {
+        CT: {
+            name: "Australian Capital Territory",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        JB: {
+            name: "Jervis Bay Territory",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        NS: {
+            name: "New South Wales",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        NT: {
+            name: "Northern Territory",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        QL: {
+            name: "Queensland",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        SA: {
+            name: "South Australia",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        TS: {
+            name: "Tasmania",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        VI: {
+            name: "Victoria",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        },
+        WA: {
+            name: "Western Australia",
+            description: "default",
+            color: "default",
+            hover_color: "default",
+            url: "default"
+        }
+    },
+    // locations: {
+    //     "0": {
+    //         lat: -33.874,
+    //         lng: 151.203,
+    //         name: "Sydney",
+    //     }
+    // }
+};
--- resources/scripts/pages/dashboard/UtilityAnalyticPage.vue
+++ resources/scripts/pages/dashboard/UtilityAnalyticPage.vue
@@ -62,7 +62,10 @@
                     <v-col md="7" sm="12" style="height: 466px;">
                         <ConnectionLeadWidget/>
                     </v-col>
-                    <v-col md="5" sm="12"></v-col>
+                    <v-col md="5" sm="12">
+                        <div style="height: 282px; width: 100%; background: lightgreen"></div>
+                        <LocationInsight style="width: 100%; margin-top: 12px; height: 150px;"/>
+                    </v-col>
                 </v-row>
             </div>
 
@@ -103,6 +106,7 @@
 import LeadWidget from "@scripts/components/widgets/LeadWidget";
 import SummaryWidget from "@scripts/components/widgets/SummaryWidget";
 import AgeWidget from "@scripts/components/widgets/AgeWidget";
+import LocationInsight from "@scripts/components/widgets/LocationInsight";
 import ConnectionLeadWidget from "@scripts/components/widgets/ConnectionLeadWidget";
 import BarChart from "@scripts/components/charts/BarChart";
 import LineChart from "@scripts/components/charts/LineChart";
@@ -117,7 +121,8 @@ export default {
         BarChart,
         LineChart,
         SummaryWidget,
-        AgeWidget
+        AgeWidget,
+        LocationInsight
     },
     data() {
         return {
@@ -127,7 +132,6 @@ export default {
         }
     },
     mounted() {
-        Math.round()
         this.load()
     },
     methods:{
