42efd79267c04e8a435356ff2d907255212cdde7
DESKTOP-ME1PRFJ\ACER-3400 <sazzad@enkaizen.com>
2021-03-25 12:43:35 +0600

    connection summary test data

--- resources/scripts/pages/dashboard/UtilityAnalyticPage.vue
+++ resources/scripts/pages/dashboard/UtilityAnalyticPage.vue
@@ -108,6 +108,9 @@ import BarChart from "@scripts/components/charts/BarChart";
 import LineChart from "@scripts/components/charts/LineChart";
 import COLOR from "@scripts/data/constants/COLOR";
 import UtilityDashboardService from "@scripts/services/UtilityDashboardService";
+import {ConnectionManager} from "pusher-js";
+import ConnectionSummary from "@scripts/api/mappers/ConnectionSummary";
+import LeadToConnectionMapper from "@scripts/api/mappers/LeadToConnectionMapper";
 
 export default {
     name: "UtilityAnalyticPage",
@@ -132,6 +135,9 @@ export default {
     },
     methods:{
         async load() {
+             let response = ConnectionSummary.getSummaryData({"leadOverview":{"total_lead":{"total":8,"charts":[{"total":2,"date":"2021-03-21"}]},"qualified_lead":{"total":10,"charts":[{"total":2,"date":"2021-03-21"}]},"total_energy_connection":{"total":2,"charts":[{"total":1,"date":"2021-03-20"},{"total":1,"date":"2021-03-21"}]},"conversion_rate":{"total":25,"charts":{"2021-03-21":{"date":"2021-03-21","total_lead":2,"total_conversion":1}},"qualified_lead":125,"total_conversion":20},"automation_rate":{"total":745,"charts":[{"date":"","full_automation":90,"manual_intervention":10}],"full_automation":10,"manual_intervention":90}},"leadSentiment":[{"total":10,"sentiment_text":"NEGATIVE"},{"total":43,"sentiment_text":"NEUTRAL"},{"total":47,"sentiment_text":"POSITIVE"}],"leadToConnection":{"lead_by_channel":8,"service_connection":0,"connection_type":0,"connection_submitted":11,"connection_accepted":2},"locationInsight":[{"total":3,"state":"ACT"},{"total":2,"state":"NSW"},{"total":2,"state":"SA"},{"total":3,"state":"VIC"}],"connectionSummary":{ "plan":[{"total":10,"utility_plan":null},{"total":1,"utility_plan":"Total Plan (Home)"}],"connectionType":[{"total":9,"which_utility":"electricity_and_gas"},{"total":2,"which_utility":"gas"}],"tenancyType":[{"total":11,"rent":"1"}],"ageGroupSummary":{"18-24": 3,"25-34": 4,"35-44":4,"45-54":0,"45-64":0,"65+":0}}});
+             let response1 = LeadToConnectionMapper.getSummaryData({"leadOverview":{"total_lead":{"total":8,"charts":[{"total":2,"date":"2021-03-21"}]},"qualified_lead":{"total":10,"charts":[{"total":2,"date":"2021-03-21"}]},"total_energy_connection":{"total":2,"charts":[{"total":1,"date":"2021-03-20"},{"total":1,"date":"2021-03-21"}]},"conversion_rate":{"total":25,"charts":{"2021-03-21":{"date":"2021-03-21","total_lead":2,"total_conversion":1}},"qualified_lead":125,"total_conversion":20},"automation_rate":{"total":745,"charts":[{"date":"","full_automation":90,"manual_intervention":10}],"full_automation":10,"manual_intervention":90}},"leadSentiment":[{"total":10,"sentiment_text":"NEGATIVE"},{"total":43,"sentiment_text":"NEUTRAL"},{"total":47,"sentiment_text":"POSITIVE"}],"leadToConnection":{"lead_by_channel":8,"service_connection":0,"connection_type":0,"connection_submitted":11,"connection_accepted":2},"locationInsight":[{"total":3,"state":"ACT"},{"total":2,"state":"NSW"},{"total":2,"state":"SA"},{"total":3,"state":"VIC"}],"connectionSummary":{"plan":[{"total":10,"utility_plan":null},{"total":1,"utility_plan":"Total Plan (Home)"}],"connectionType":[{"total":9,"which_utility":"electricity_and_gas"},{"total":2,"which_utility":"gas"}],"tenancyType":[{"total":11,"rent":"1"}]}});
+             console.log(response, " ", response1 );
             this.leadsData = await UtilityDashboardService.getLeadAnalytics();
             this.connectionSummary = await UtilityDashboardService.getConnectionSummary();
             this.isLoaded = true;
