c88e2d34ac4c802e1531bfbe57dc20a1d600cfe8
Lenin Sheikh <justtlenin@gmail.com>
2021-03-30 10:22:55 +0600

    updated tooltip color

--- resources/scripts/components/widgets/LeadWidget.vue
+++ resources/scripts/components/widgets/LeadWidget.vue
@@ -1,5 +1,5 @@
 <template>
-        <v-tooltip bottom >
+        <v-tooltip bottom  color="#263238" class="white--text">
             <template v-slot:activator="{ on, attrs }">
                 <v-card v-if="tooltip" class="widget mx-2" v-bind="attrs"
                         v-on="on">
@@ -89,5 +89,8 @@ export default {
         margin: 0px;
         text-align: center;
     }
+}
+.v-tooltip__content.menuable__content__active {
+    opacity: 1!important;
 }
 </style>
