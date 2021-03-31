270dc14eae92fa785377451eef078742431b7314
Lenin <justtlenin@gmail.com>
2021-03-31 15:02:51 +0600

    updated hover effect of logout button

--- resources/scripts/layouts/DashboardLayout.vue
+++ resources/scripts/layouts/DashboardLayout.vue
@@ -1,213 +1,212 @@
 <template>
-    <v-app id="inspire">
+    <v-app id="inspire" v-if="user">
         <v-navigation-drawer
             v-model="drawer"
-            :mini-variant="miniDrawer"
-            mini-variant-width="75"
             app
-            class="navigation-drawer-menu app-nav-bg"
+            class="app-nav-bar"
+            dark
+            :width="252"
         >
-            <v-list-item class="app-logo-area">
-                <v-list-item-avatar class="ml-0">
-                    <v-img src="/assets/images/logo/hood-small.png"/>
+            <v-list class="nav-user-card hood-gradiant">
+                <v-list-item-avatar class="center-element nav-logo" size="59">
+                    <v-img src="/assets/images/logo/hood_logo_while.png"/>
                 </v-list-item-avatar>
-
-                <v-list-item-content>
-<!--                    <v-list-item-title class="white&#45;&#45;text">Hood</v-list-item-title>-->
+                <v-list-item-content class="text-center">
+                    <v-list-item-title class="nav-user-name">
+                        {{user.name}}
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="nav-user-email">
+                        {{user.email}}
+                    </v-list-item-subtitle>
                 </v-list-item-content>
-            </v-list-item>
+            </v-list>
 
-            <v-divider></v-divider>
-
-            <v-list dense class=" pt-0 main-nav-items">
-                    <span class="mb-2" v-for="r in routes" :key="r.title">
-                         <v-list-item
-                            v-if="r.type ==='group'"
-                            link
-                            @click="onGroupMenuClicked(r)"
-                            exact-active-class=""
-                            :class="{
-                                'py-1': true,
-                                'app-nav': true,
-                                'app-active-nav white--text': selectedRouteTitle === r.title
-                            }"
-                            :key="r.title"
+            <v-list class="navigation-menu" dense>
+                <v-img
+                    class="center-element rounded-circle nav-avatar"
+                    src="/assets/images/user_logo.png"
+                    max-width="96px"
+                ></v-img>
+                <span v-for="route in routes" :key="route.title">
+                        <v-list-group
+                            color="white"
+                            v-model="route.model"
+                            active-class="nav-active"
+                            v-if="route.type === 'group'"
                         >
-                            <v-list-item-icon>
-                                <v-icon size="26" :color="selectedRouteTitle === r.title ? 'white' : 'grey'">{{ r.icon }}</v-icon>
-                            </v-list-item-icon>
+                            <template v-slot:activator>
+                                <v-list-item-icon>
+                                    <v-img v-bind:src="route.icon"/>
+                                </v-list-item-icon>
+                                <v-list-item-title class="nav-main-item"> {{ route.title }} </v-list-item-title>
+                            </template>
+                            <span class="mb-2" v-for="subRoute in route.children" :key="subRoute.title">
+                                <v-list-item
+                                    exact-active-class="nav-active"
+                                    link
+                                    :to="{name:subRoute.route_name}"
+                                    exact
+                                >
+                                    <v-list-item-action>
+                                    </v-list-item-action>
+
+                                    <v-list-item-content>
+                                        <v-list-item-title class="nav-sub-item"> {{ subRoute.title }} </v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </span>
+                        </v-list-group>
+                        <v-list-item link exact-active-class="nav-active" v-if="route.type === 'route'" :to="{name:route.route_name}" exact>
+                           <v-list-item-icon>
+                               <v-img v-bind:src="route.icon"/>
+                           </v-list-item-icon>
                             <v-list-item-content>
-                                <v-list-item-title>{{ r.title }}</v-list-item-title>
+                                <v-list-item-title class="nav-main-item"> {{ route.title }} </v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
-                        <v-list-item
-                            v-else
-                            link
-                            @click="onMenuClicked(r)"
-                            :class="{
-                                'pa-1': true,
-                                'app-nav': true,
-                                'app-active-nav white--text': selectedRouteTitle === r.title
-                            }"
-                            :key="r.title"
-                        >
-                        <v-list-item-icon><v-icon size="26" :color="selectedRouteTitle === r.title ? 'white' : 'grey'">{{ r.icon }}</v-icon></v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ r.title }}</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
                     </span>
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar flat app class="white">
-<!--            <v-toolbar-title class="primary&#45;&#45;text">Hood</v-toolbar-title>-->
-            <!--            <v-btn icon color="primary" @click="miniDrawer = !miniDrawer">-->
-            <!--                <v-icon>{{ miniDrawer ? 'mdi-menu' : 'mdi-menu-open' }}</v-icon>-->
-            <!--            </v-btn>-->
-            <!--            <v-app-bar-nav-icon @click="miniDrawer = !miniDrawer"></v-app-bar-nav-icon>-->
-            <!--            <v-toolbar-title>Hood</v-toolbar-title>-->
+        <v-app-bar app class="app-app-bar" dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title v-if="false">
+                <h4>Dashboard</h4>
+                <h6>Customer / List</h6>
+            </v-toolbar-title>
             <v-spacer/>
-            <v-badge
-                color="red"
-                content="6"
-                offset-y="20"
-                offset-x="20"
-                overlap
-            >
-                <v-btn color="primary" icon>
-                    <v-icon>mdi-bell</v-icon>
-                </v-btn>
-            </v-badge>
+            <v-img  src="/assets/images/icons/Search.svg" max-width="24px"/>
+            <div class="vertical-divider"></div>
 
-            <v-menu
-                left
-                bottom
-            >
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                        color="primary"
-                        icon
-                        v-bind="attrs"
-                        v-on="on"
-                    >
-                        <v-icon>mdi-account</v-icon>
-                    </v-btn>
-                </template>
-
-                <v-list>
-                    <v-list-item>
-                        <v-list-item-title>Account</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-title>Log Out</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-
+            <p class="app-bar-user-name">{{user.name}}</p>
+<!--            <v-icon medium class="app-bar-icon app-bar-login-button"> mdi-login</v-icon>-->
+            <v-img style="cursor: pointer" alt="LOGOUT"  class="mx-9" @click="onLogout" src="/assets/images/icons/Logout.svg" max-width="24px"/>
         </v-app-bar>
 
-        <v-main  class="body-bg" :style="{position: 'relative', 'padding-left': subMenuDrawer ? '242px !important' : '64px !important' }">
-            <v-expand-x-transition>
-                <v-navigation-drawer
-                    v-if="selectedMenus && subMenuDrawer"
-                    width="170px"
-                    style="background-color: white; border-radius: 8px; position: fixed; left: 76px; top:66px"
-                >
-                    <v-list
-                        dense
-                        nav
-                    >
-                        <div class="d-flex justify-end">
-                            <div><v-icon @click="subMenuDrawer = false" small>mdi-arrow-left</v-icon></div>
-                        </div>
-                        <div>
-                            <v-list-item class="mt-0 pt-0">
-                                <v-list-item-title class="sub-menu-title mt-0 pt-0">
-                                    {{selectedMenus ? selectedMenus.title.toUpperCase() : ''}}
-                                </v-list-item-title>
-                            </v-list-item>
-                        </div>
-                        <v-list-item
-                            v-for="r in selectedMenus.children"
-                            :key="r.title"
-                            :to="{name: r.route_name}"
-                            exact
-                            @click="onSubmenuClick(r)"
-                        >
-                            <v-list-item-content>
-                                <v-list-item-title>{{ r.title }}</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-navigation-drawer>
-            </v-expand-x-transition>
-
+        <v-main>
             <router-view></router-view>
-
         </v-main>
     </v-app>
 </template>
 
 <script>
 import ApplicationService from "../services/ApplicationService";
-import {initializeBroadcasting} from "@scripts/plugins/LaravelEcho";
-import AuthService, {getAuthUser} from "@scripts/services/AuthService";
-
+import AuthService from "@scripts/services/AuthService";
 
 export default {
-    name: "DashboardLayout",
+    name: "NewDashboardLayout",
     data() {
         return {
-            subMenuDrawer: false,
-            //todo: update default route based on current route
-            selectedMenus: ApplicationService.getDefaultRoute(this.$route),
-            drawer: true,
-            miniDrawer: true,
+            user: null,
+            drawer: null,
             routes: ApplicationService.getMainNavigationRoutes()
         }
     },
-    computed: {
-        selectedRouteTitle() {
-            return this.selectedMenus?.title
-        }
-    },
     mounted() {
+        this.user = AuthService.getAuthUser();
         setInterval(AuthService.authUser, 300000)
-        initializeBroadcasting(getAuthUser().id);
     },
-    methods: {
-        onGroupMenuClicked(menu) {
-            if(menu.route_name === this.selectedMenus?.route_name) {
-                this.subMenuDrawer = !this.subMenuDrawer;
-            } else {
-                this.$router.push({name: menu.route_name})
-                this.subMenuDrawer = true;
-                this.selectedMenus = menu;
-            }
+     methods: {
+        async onLogout() {
+            await AuthService.logout();
         },
-        onMenuClicked(menu) {
-            this.subMenuDrawer = false;
-            if (menu.route_name !== this.selectedMenus?.route_name) {
-                this.$router.push({name: menu.route_name})
-                this.selectedMenus = menu;
-            }
-        },
-        onSubmenuClick(menu) {
-            this.$router.push({name: menu.route_name})
-        }
-    }
+    },
 }
 </script>
 
 <style scoped>
-.app-nav-bg {
-    background-color: #444348 !important;
+.app-nav-bar {
+    background-color: rgb(37, 40, 48, 1) !important;
 }
-.app-logo-area {
-    padding-bottom: 8px !important; background-color: white
+
+.app-app-bar {
+    background-color: rgb(38, 50, 56, 1) !important;
+    max-height: 76px !important;
 }
-.sub-menu-title {
-    color: darkgrey;font-size: 10px !important;
+
+.nav-user-card {
+    min-height: 235px !important;
+}
+
+.center-element {
+    display: block !important;
+    margin: 0px auto !important;
+}
+
+.nav-logo {
+    margin-top: 25px !important;
+    margin-bottom: 15px !important;
+}
+
+.nav-avatar {
+    border: 10px solid rgb(37, 40, 48, 1);
+    margin-top: -50px !important;
+    margin-bottom: 20px !important;
+}
+
+.nav-user-name {
+    font-size: 20px !important;
+    font-Weight: 500 !important;
+    line-height: 28px;
+    letter-spacing: 0.15px;
+}
+
+.nav-user-email {
+    font-size: 12px !important;
+    font-Weight: 400 !important;
+    line-height: 14.76px;
+    letter-spacing: 0.15px;
+}
+
+.nav-main-item {
+    font-size: 16px !important;
+    font-Weight: 500 !important;
+    line-height: 28px !important;
+    letter-spacing: 1.5px;
+}
+
+.nav-sub-item {
+    font-size: 16px !important;
+    font-Weight: 400 !important;
+    line-height: 24px !important;
+    letter-spacing: 1px;
+}
+
+.nav-active {
+   background-color: #542E89 !important;
+   color: white !important;
+}
+/*.navigation-menu .v-list-item--active {*/
+/*    background-color:rgb(84,46,137,1) !important;*/
+/*    color: white !important;*/
+/*}*/
+.vertical-divider {
+    border-left: 2px solid rgb(223, 224, 235, 1);
+    height: 30px;
+    margin-right: 25px;
+    margin-left: 25px;
+}
+
+.app-bar-icon {
+    opacity: .7;
+}
+
+.app-bar-user-name {
+    font-size: 14px !important;
+    font-Weight: 600 !important;
+    line-height: 20px !important;
+    letter-spacing: .2px;
+    margin-top: 15px;
+    color: rgb(223, 224, 235, 1);
+}
+
+.app-bar-login-button {
+    margin-right: 30px !important;
+    margin-left: 60px !important;
+}
+
+.nav-group .v-list-group-active {
+    background-color: #542E89 !important;
+    color: white !important;
 }
 </style>
