7cd9383d3b74dcd9754679f4f607cd0a2b287ec7
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-02 13:03:01 +0600

    Agent Application Dashboard UI

---
+++ resources/scripts/components/crm/agent/AgentApplicationTable.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+        <v-row class="mt-5">
+            <v-col cols="8" class="search-bg">
+                <Search></Search>
+            </v-col>
+            <v-col cols="4" class="text-right">
+                <v-btn color="primary" @click="addAgency"
+                ><v-icon left>add
+                </v-icon> Add New Application
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-card class="pa-4">
+            <v-row>
+                <v-col cols="12" class="crm-table">
+                    <v-simple-table>
+                        <template v-slot:default>
+                            <thead>
+                            <tr>
+                                <th class="text-left">
+                                    Name
+                                </th>
+                                <th class="text-left">
+                                    Moving date
+                                </th>
+                                <th class="text-left">
+                                    Mobile
+                                </th>
+                                <th class="text-left">
+                                    Service Type
+                                </th>
+                                <th class="text-left">
+                                    Status
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
+                                v-for="item in applications"
+                                :key="item.id"
+                            >
+                                <td>{{ item.application_name }}</td>
+                                <td>{{ item.moving_date }}</td>
+                                <td>{{ item.phone }}</td>
+                                <td>
+                                    <v-icon color="yellow">mdi-flash</v-icon>
+                                    <v-icon color="red">mdi-fire</v-icon>
+                                    <v-icon color="grey lighten-1">mdi-wifi</v-icon>
+                                    <v-icon color="grey lighten-1">mdi-water</v-icon>
+                                </td>
+                                <td>{{ item.status }}</td>
+                            </tr>
+                            </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-col>
+            </v-row>
+        </v-card>
+    </div>
+</template>
+
+<script>
+import Search from "@scripts/components/crm/Search";
+
+export default {
+name: "AgencyApplicationTable",
+    components: {
+        Search
+    },
+    data(){
+        return {
+            applications : [
+                {
+                    id: 1,
+                    application_name: 'Applicant Name goes here',
+                    moving_date: '06/08/2021',
+                    phone: '0410758782',
+                    service_types: 30,
+                    status: 'Not Confirmed Connection',
+                },
+                {
+                    id: 2,
+                    application_name: 'Applicant Name goes here',
+                    moving_date: '06/08/2021',
+                    phone: '0410758782',
+                    service_types: 30,
+                    status: 'Not Confirmed Connection',
+                },
+                {
+                    id: 3,
+                    application_name: 'Applicant Name goes here',
+                    moving_date: '06/08/2021',
+                    phone: '0410758782',
+                    service_types: 30,
+                    status: 'Not Confirmed Connection',
+                },
+                {
+                    id: 4,
+                    application_name: 'Applicant Name goes here',
+                    moving_date: '06/08/2021',
+                    phone: '0410758782',
+                    service_types: 30,
+                    status: 'Not Confirmed Connection',
+                },
+            ]
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/scripts/pages/agent/AgentApplicationPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <v-row class="mt-0">
+            <v-col cols="8">
+                <v-card class="pa-4">
+                    <div>
+                        <span class="user-details">Hi Dimuthusathrasinghe, </span>
+                        <span>heres a summary of your applications.</span>
+                    </div>
+                    <AgentLeadMetrics></AgentLeadMetrics>
+                </v-card>
+                <AgentApplicationTable></AgentApplicationTable>
+            </v-col>
+            <v-col cols="4">
+                <AgentApplicationSummary ></AgentApplicationSummary>
+            </v-col>
+        </v-row>
+
+    </v-container>
+</template>
+
+<script>
+import AgentLeadMetrics from "@scripts/components/crm/agent/AgentLeadMetrics";
+import AgentApplicationTable from "@scripts/components/crm/agent/AgentApplicationTable";
+import AgentApplicationSummary from "@scripts/components/crm/agent/AgentApplicationSummary";
+export default {
+    name: "AgentApplicationPage",
+    components: {
+        AgentApplicationTable,
+        AgentLeadMetrics,
+        AgentApplicationSummary
+    },
+}
+</script>
+
+<style scoped>
+.user-details {
+    font-size: 25px !important;
+    font-Weight: 700 !important;
+}
+
+</style>
