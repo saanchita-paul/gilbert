a399ba20b324429ac46ab366b59d3c1f3e1a7e7e
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-06 09:20:13 +0600

    Added User, Office, Agent profile migration

---
+++ database/migrations/2021_08_06_030231_create_offices_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('agency_id');
+            $table->foreign('agency_id')
+                ->references('id')
+                ->on('agencies')
+                ->onDelete('cascade');
+            $table->string('name', 100)->nullable();
+            $table->string('street_address', 100)->nullable();
+            $table->string('city', 45)->nullable();
+            $table->string('state', 45)->nullable();
+            $table->string('postcode', 10)->nullable();
+            $table->string('country', 45)->nullable();
+            $table->string('abn', 45)->nullable();
+            $table->string('phone', 45)->nullable();
+            $table->string('email', 45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices');
+    }
+}
