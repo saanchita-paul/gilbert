52e838abcba1be916e5a166c3dbf45561c6d0f65
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-06 11:30:33 +0600

    Model Relation Binding

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Agency extends Model
 {
     use HasFactory;
+
+    /**
+     * @return HasMany
+     */
+    public function offices()
+    {
+        return $this->hasMany(Office::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function agentProfiles()
+    {
+        return $this->hasMany(AgentProfile::class);
+    }
 }
--- app/Models/Office.php
+++ app/Models/Office.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Office extends Model
 {
     use HasFactory;
+
+    /**
+     * @return BelongsTo
+     */
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function agentProfiles()
+    {
+        return $this->hasMany(AgentProfile::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function officeCommissions()
+    {
+        return $this->hasMany(OfficeCommission::class);
+    }
 }
