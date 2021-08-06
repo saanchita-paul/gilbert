d2e232941f6aa935a6722c773f6fc0413e53640a
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-06 12:43:16 +0600

    Get Profile type dynamically

--- app/Models/HoodProfile.php
+++ app/Models/HoodProfile.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class HoodProfile extends Model
 {
     use HasFactory;
+
+    /**
+     * @return MorphOne
+     */
+    public function user()
+    {
+        return $this->morphOne(User::class, 'profile');
+    }
 }
