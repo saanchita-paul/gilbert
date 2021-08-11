0970c3dd07d08281f14e7bc2d6be7ba995a568e5
sarwarenkaizen <sarwar.sikder@enkaizen.com>
2021-08-11 17:29:58 +0600

    application seesder refectoring

--- database/seeders/ConnectionServiceSeeder.php
+++ database/seeders/ConnectionServiceSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\ConnectionApplication;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class ConnectionServiceSeeder extends Seeder
@@ -13,6 +15,11 @@ class ConnectionServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        ConnectionApplication::factory()
+            ->count(50)
+            ->state(new Sequence(
+                fn($sequence) => ['connection_application_id' => ConnectionApplication::all()->random()],
+            ))
+            ->create();
     }
 }
