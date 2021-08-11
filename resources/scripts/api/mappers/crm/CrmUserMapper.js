fc03415e05771602c07956b1d86d623f8c96d432
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-11 17:23:59 +0600

    Users list full functional

--- resources/scripts/api/mappers/crm/CrmUserMapper.js
+++ resources/scripts/api/mappers/crm/CrmUserMapper.js
@@ -1,15 +1,30 @@
 import CrmUser from "@scripts/models/crm/CrmUser";
+import PaginationMapper from "@scripts/api/mappers/crm/PaginationMapper";
 
 function mapUser(user) {
     return new CrmUser({...user});
 }
 
 export default {
-    mapUserList: (userList)=> {
-        return  userList.map(user=> {
-            return mapUser(user);
-        })
+    // mapUserList: (userList)=> {
+    //     return  userList.map(user=> {
+    //         return mapUser(user);
+    //     })
+    //
+    // },
 
+    mapUserList: (userList)=> {
+
+        const users =  userList?.data.map(user=> {
+            return mapUser(user);
+        });
+
+        const pagination =  PaginationMapper.mapPagination(userList?.meta);
+
+        return {
+            users: users,
+            pagination: pagination,
+        };
     },
 
     mapUser: (user) => {
@@ -25,6 +40,12 @@ export default {
         };
 
 
+    },
+    mapMetaData: (meta) => {
+        if(meta.sort_by === 'proerty_manager_name') meta.sort_by = 'first_name';
+        if(meta.sort_by === 'submitted_lead') meta.sort_by = 'first_name';
+        if(meta.sort_by === 'role') meta.sort_by = 'first_name';
+        return meta;
     }
 
 }
--- resources/scripts/services/crm/CrmUserService.js
+++ resources/scripts/services/crm/CrmUserService.js
@@ -2,7 +2,7 @@ import CrmUserMapper from "@scripts/api/mappers/crm/CrmUserMapper";
 import CrmUserAPI from "@scripts/api/crm/CrmUserAPI";
 
 export default {
-    loadUserData: () => CrmUserAPI.getUsersData(),
+    loadUserData: (meta, agencyId, officeId) => CrmUserAPI.getUsersData(meta, agencyId, officeId),
     saveUser: (crmUser, officeId)=> CrmUserAPI.saveUser(crmUser, officeId),
     loadAllUser: ()=> CrmUserAPI.getUserAllData(),
 }
