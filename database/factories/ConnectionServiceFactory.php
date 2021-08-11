50812e7484758c15792bc7da14aa073fb8c9229d
Tonmoy Enakizen <tonmoy@enkaizen.com>
2021-08-11 12:35:13 +0600

    Added Application Seeder (With Agent User)

---
+++ database/factories/ConnectionServiceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ConnectionService;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ConnectionServiceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ConnectionService::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'service_type' => 'gas',
+        ];
+    }
+}
---
+++ database/seeders/ApplicationSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ConnectionApplication;
+use App\Models\Office;
+use App\Models\AgentProfile;
+use App\Models\User;
+use App\Services\RolePermission;
+use Illuminate\Database\Seeder;
+use App\Models\ConnectionService;
+
+class ApplicationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $office = Office::first();
+
+        $profile = new AgentProfile();
+        $profile->office_id = $office->id;
+        $profile->agency_id = $office->agency->id;
+        $profile->first_name = 'Hood';
+        $profile->last_name = 'Agent';
+        $profile->{'f_id_12'} = '123';
+        $profile->email = 'agent@hood.ai';
+        $profile->phone = '1234567890';
+        $profile->save();
+
+        $user = new User();
+        $user->password = bcrypt('123456');
+        $user->email = 'agent@hood.ai';
+        $user->profile_type = User::PROFILE_TYPE_AGENT;
+        $user->profile_id = $profile->id;
+        $user->save();
+        $user->assignRole(RolePermission::ROLE_AGENCY_AGENT);
+
+        ConnectionApplication::factory()
+            ->count(20)
+            ->has(ConnectionService::factory()->count(1), 'connectionServices')
+            ->create();
+    }
+}
