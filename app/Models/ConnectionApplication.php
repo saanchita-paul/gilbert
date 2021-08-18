f647ce306befa065c3664fe6b3fa154f47790318
Lenin Sheikh <justtlenin@gmail.com>
2021-08-18 15:10:49 +0600

    added helper docs

--- app/Models/ConnectionApplication.php
+++ app/Models/ConnectionApplication.php
@@ -8,6 +8,88 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * App\Models\ConnectionApplication
+ *
+ * @property int $id
+ * @property int $office_id
+ * @property int $agency_id
+ * @property int $created_by
+ * @property int|null $assigned_to
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $email
+ * @property string|null $phone
+ * @property int|null $tenancy_type
+ * @property string|null $dob
+ * @property string|null $moving_date
+ * @property string|null $address_unit
+ * @property string|null $street_address
+ * @property string|null $city
+ * @property string|null $postcode
+ * @property string|null $state
+ * @property string|null $country
+ * @property string|null $additional_instruction
+ * @property string|null $address_text
+ * @property string|null $reason
+ * @property int|null $is_email_billing
+ * @property int|null $property_type
+ * @property int|null $has_life_support
+ * @property int|null $has_solar
+ * @property string|null $nmi
+ * @property string|null $mirn
+ * @property int|null $supplier
+ * @property int|null $plan_type
+ * @property int|null $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Agency $agency
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\ApplicationNote[] $applicationNotes
+ * @property-read int|null $application_notes_count
+ * @property-read \App\Models\AgentProfile|null $assignedTo
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\ConnectionService[] $connectionServices
+ * @property-read int|null $connection_services_count
+ * @property-read \App\Models\AgentProfile $createdBy
+ * @property-read \App\Models\Identification|null $identification
+ * @property-read \App\Models\Office $office
+ * @method static \Database\Factories\ConnectionApplicationFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereAdditionalInstruction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereAddressText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereAddressUnit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereAgencyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereAssignedTo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereDob($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereHasLifeSupport($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereHasSolar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereIsEmailBilling($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereMirn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereMovingDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereNmi($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereOfficeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication wherePlanType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication wherePostcode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication wherePropertyType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereStreetAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereSupplier($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereTenancyType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ConnectionApplication whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ConnectionApplication extends Model
 {
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,44 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $profile_type
+ * @property int|null $profile_id
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $profile
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
+ * @property-read int|null $roles_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProfileId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProfileType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
