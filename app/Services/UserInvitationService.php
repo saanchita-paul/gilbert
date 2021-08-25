3139726d28f1867ffe0bb6bbf5353d56a28c7870
sarwarenkaizen <sarwar.sikder@enkaizen.com>
2021-08-25 13:57:53 +0600

    token validation

--- app/Services/UserInvitationService.php
+++ app/Services/UserInvitationService.php
@@ -4,17 +4,32 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Models\UserInvitation;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
-use phpDocumentor\Reflection\Utils;
 
 class UserInvitationService
 {
-    public function getInvitationByToken(array $data){
+    public function getInvitationByToken(array $data)
+    {
         $token = $data['token'];
-        return UserInvitation::where('token_code', $token)->first();
+        return UserInvitation::where('token_code', $token)
+            ->where('status', 'pending')
+            ->whereDate('valid_till', '>', Carbon::now())
+            ->first();
     }
 
-    public function updatePassword($request){
+    public function createUserInvitation($inputData)
+    {
+        $userInvitation = new UserInvitation();
+        $userInvitation->email = $inputData['email'];
+        $userInvitation->user_id = $inputData['user_id'];
+        $userInvitation->user_id = $inputData['user_id'];
+        $userInvitation->valid_till = Carbon::now()->addHour(72)->format('Y-m-d H:i:s');
+        return UserInvitation::create($userInvitation);
+    }
+
+    public function updatePassword($request)
+    {
         $user = User::where('email' , $request['email']);
         if($user->first())
         {
