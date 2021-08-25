51920f963e4e08652a289b09ab8e7daf29f89c47
sazzad ahmed <sazzad@enkaizen.com>
2021-08-25 18:37:46 +0600

    binding Lead metric for agent and real estate page

--- resources/scripts/components/crm/agent/AgentLeadMetrics.vue
+++ resources/scripts/components/crm/agent/AgentLeadMetrics.vue
@@ -12,7 +12,7 @@
               <h3>Applications</h3>
               <div class="leade-icon">
                   <v-icon color="yellow">mdi-flash</v-icon>
-                  <span>104</span>
+                  <span>{{appMetrics.applications}}</span>
               </div>
               <p class="leade-text">Total submitted</p>
           </div>
@@ -21,7 +21,7 @@
               <h3>Non-payable</h3>
               <div class="leade-icon">
                   <v-icon color="red">mdi-link</v-icon>
-                  <span>2</span>
+                  <span>{{appMetrics.nonpayable}}</span>
               </div>
               <p class="leade-text">Non Connected</p>
           </div>
@@ -30,16 +30,16 @@
             <h3>Power</h3>
             <div class="leade-icon">
                 <v-icon color="yellow">mdi-flash</v-icon>
-                <span>43</span>
+                <span>{{appMetrics.power}}</span>
             </div>
             <p class="leade-text">Successfully connected</p>
           </div>
-          
+
           <div class="agent-leade">
             <h3>Gas</h3>
             <div class="leade-icon">
                 <v-icon color="red">mdi-fire</v-icon>
-                <span>104</span>
+                <span>{{appMetrics.gas}}</span>
             </div>
             <p class="leade-text">Successfully connected</p>
           </div>
@@ -48,7 +48,7 @@
             <h3>Internet</h3>
             <div class="leade-icon">
                 <v-icon color="grey lighten-1">mdi-wifi</v-icon>
-                <span>104</span>
+                <span>{{appMetrics.internet}}</span>
             </div>
             <p class="leade-text">Successfully connected</p>
           </div>
@@ -57,17 +57,38 @@
               <h3>Water</h3>
               <div class="leade-icon">
                   <v-icon color="grey lighten-1">mdi-water</v-icon>
-                  <span>104</span>
+                  <span>{{appMetrics.water}}</span>
               </div>
               <p class="leade-text">Successfully connected</p>
           </div>
       </div>
-      
+
     </v-row>
 </template>
 
 <script>
+import LeadApplicationService from "@scripts/services/crm/LeadApplicationService";
+
 export default {
   name: "AgentLeadMetrics",
+    data() {
+
+      return {
+          appMetrics: null,
+      }
+    },
+    methods: {
+
+      async loadMetrics() {
+          const allMetric = await LeadApplicationService.loadMetrics();
+          this.appMetrics = allMetric.data;
+
+          console.log('metricsadfasf ',this.appMetrics);
+      }
+
+    },
+    mounted() {
+      this.loadMetrics();
+    }
 };
 </script>
