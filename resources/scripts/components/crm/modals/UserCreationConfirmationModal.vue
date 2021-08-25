217f5e1562d874a3c477757a11bbd17592fb70b4
Lenin Sheikh <justtlenin@gmail.com>
2021-08-25 15:29:18 +0600

    updated text in user invite confirmation page

--- resources/scripts/components/crm/modals/UserCreationConfirmationModal.vue
+++ resources/scripts/components/crm/modals/UserCreationConfirmationModal.vue
@@ -16,7 +16,7 @@
                             <div class="dialogs-area">
                                 <p class="title">New user details</p>
                                 <v-row>
-                                    <v-col cols="4" class="pb-1 pt-0" ><h5 class="text-align-right" >Job Title</h5></v-col><v-col cols="8"  class="pb-1 pt-0">{{user.job_title}}</v-col>
+                                    <v-col cols="4" class="pb-1 pt-0" ><h5 class="text-align-right" >Job Title</h5></v-col><v-col cols="8"  class="pb-1 pt-0">{{role}}</v-col>
                                     <v-col cols="4"   class="pb-1 pt-0" ><h5 class="text-align-right" >First Name</h5></v-col><v-col cols="8"  class="pb-1 pt-0">{{user.first_name}}</v-col>
                                     <v-col cols="4"  class="pb-1 pt-0" ><h5 class="text-align-right" >Last Name</h5></v-col><v-col cols="8"  class="pb-1 pt-0">{{user.last_name}}</v-col>
                                     <v-col cols="4"  class="pb-1 pt-0" ><h5 class="text-align-right" >Phone Number</h5></v-col><v-col cols="8"  class="pb-1 pt-0">{{user.phone}}</v-col>
@@ -48,12 +48,20 @@
 </template>
 
 <script>
+import UserRoles from "@scripts/data/UserRoles";
+
 export default {
-    name: "UserCreationConfirmationModal"
-    ,
+    name: "UserCreationConfirmationModal",
     props:['dialog','user'],
     data() {
         return {
+        }
+    },
+    computed: {
+        role() {
+            return this.user
+                ? UserRoles.AGENCY.find(role => role.value === this.user.job_title)?.text
+                : this.user?.job_title;
         }
     },
     methods: {
