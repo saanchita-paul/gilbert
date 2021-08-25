ab5e455f145c6d3df96147bce89865ca7759ea69
sarwarenkaizen <sarwar.sikder@enkaizen.com>
2021-08-25 08:56:34 +0600

    user invitation and password validation

--- app/Http/Controllers/UserInvitationController.php
+++ app/Http/Controllers/UserInvitationController.php
@@ -9,19 +9,32 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 class UserInvitationController extends Controller
 {
     /**
-     * Getting Office List for an agency
+     * Getting User Invitation validation
      *
      * @param Request $request
-     * @param int $agencyId
      *
      * @return AnonymousResourceCollection|JsonResponse
      */
     public function index(Request $request): AnonymousResourceCollection | JsonResponse
     {
         try {
+            $data = $request->toArray();
             return response()->json(['success' => true]);
         } catch (\Exception $exception) {
             return response()->json(['success' => false, 'message' => $exception->getMessage()]);
         }
+    }
+
+    /**
+     * Creating new Office under Agency.
+     *
+     * @param PasswordChangeRequest $request
+     *
+     * @return UserInvitationResource|JsonResponse
+     */
+
+    public function createAgencyOffice(PasswordChangeRequest $request, int $agencyId): UserInvitationResource | JsonResponse
+    {
+
     }
 }
