ee1bbd866e0a43145d022a19809ffe5975c3f819
Sarwar Sikder <sarwar.sikder@enkaizen.com>
2021-09-02 01:17:23 +0600

    Make service class for FastConnect API

--- app/Http/Controllers/UtilityController.php
+++ app/Http/Controllers/UtilityController.php
@@ -2,57 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\UtilityService;
+use App\Services\Utility\FastConnectService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UtilityController extends Controller
 {
-    protected  $fc_auth_url = "https://sandbox.fastconnect.net.au/oauth/token?grant_type=client_credentials&scope=datafind";
-    protected  $fc_authorization = "Basic c2FuZGJveF9HcWxNejlaR3d0OE03dzdZNzdGUzV5elI6bndJQXM5WTJKR3NmVEVNVU1CU25JWWFKam4wMW9vT1hkNjZrMFhWY2E3S016SGI4";
-    protected  $fc_address_url = "https://sandbox.fastconnect.net.au/api/datafind/address";
     /**
      * Getting Address from third party API
      *
      * @param Request $request
-     *
+     * @return JsonResponse
      */
-    public function fcAuth(Request $request)
+    public function fcAuth(Request $request): JsonResponse
     {
         try {
+            $fcService = new FastConnectService();
+            $authentication = $fcService->authenticate();
 
-            $svcUtilities = new UtilityService();
-            $authentication = $svcUtilities->fcAPIAuth($this->fc_auth_url,$this->fc_authorization);
-
-            if($authentication == null)
+            if ($authentication == null)
             {
                 return response()->json(['success' => false, 'data' => $authentication]);
             }
-            return response()->json(['success' => true, 'data' => $authentication]);
 
+            return response()->json(['success' => true, 'data' => $authentication]);
         } catch (\Exception $exception) {
             return response()->json(['success' => false, 'message' => $exception->getMessage()]);
         }
     }
 
-    public function fcAddress(Request $request)
+    public function fcAddress(Request $request): JsonResponse
     {
         try {
-            $fc_authorization = "Bearer ".$request->bearerToken();
             $body = $request->getContent();
+            $fcService = new FastConnectService();
+            $authentication = $fcService->findAddress($body, $request->bearerToken());
 
-            $svcUtilities = new UtilityService();
-
-            $authentication = $svcUtilities->fcAPIReqAddress($this->fc_address_url, $fc_authorization, $body);
-
-            if($authentication == null)
+            if ($authentication == null)
             {
                 return response()->json(['success' => false, 'data' => $authentication]);
             }
-            return response()->json(['success' => true, 'data' => $authentication]);
 
+            return response()->json(['success' => true, 'data' => $authentication]);
         } catch (\Exception $exception) {
             return response()->json(['success' => false, 'message' => $exception->getMessage()]);
         }
     }
-
 }
