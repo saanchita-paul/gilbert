61bdb98caad1d1d6cfa10af4086b9360814eef9f
Lenin Sheikh <justtlenin@gmail.com>
2021-09-03 12:15:33 +0600

    disables services

--- resources/scripts/components/crm/office/OfficeProfile.vue
+++ resources/scripts/components/crm/office/OfficeProfile.vue
@@ -114,7 +114,7 @@
                     ></v-text-field>
                     </ValidationProvider>
 
-                    <ValidationProvider name="User ID" rules="required"  v-slot="{ errors }">
+                    <ValidationProvider name="User ID"   v-slot="{ errors }">
                     <v-text-field
                         label="User ID*"
                         v-model="agent.f_id_12"
@@ -202,16 +202,16 @@
                             <div class="leade-icon pb-2">
                                 <v-icon color="grey lighten-1">mdi-wifi</v-icon>
                                 <span class="mr-4">$</span>
-                                <ValidationProvider name="Internet" rules="numeric|max:2|min_value:1|required"  v-slot="{ errors }">
+<!--                                <ValidationProvider name="Internet" rules="numeric|max:2|min_value:1|required"  v-slot="{ errors }">-->
                                 <v-text-field
                                     v-model="commission.internet"
                                     outlined
+                                    disabled
                                     dense
                                     hide-details
                                     placeholder="50"
-                                    :error-messages=" errors[0]"
                                 ></v-text-field>
-                                </ValidationProvider>
+<!--                                </ValidationProvider>-->
                             </div>
                             <p class="leade-text pr-5">Per successful connection</p>
                         </div>
@@ -223,16 +223,16 @@
                             <div class="leade-icon pb-2">
                                 <v-icon color="grey lighten-1">mdi-water</v-icon>
                                 <span class="mr-4">$</span>
-                                <ValidationProvider name="Water"rules="numeric|max:2|min_value:1|required"  v-slot="{ errors }">
+<!--                                <ValidationProvider name="Water"rules="numeric|max:2|min_value:1|required"  v-slot="{ errors }">-->
                                 <v-text-field
+                                    disabled
                                     v-model="commission.water"
                                     outlined
                                     dense
                                     hide-details
                                     placeholder="50"
-                                    :error-messages=" errors[0]"
                                 ></v-text-field>
-                                </ValidationProvider>
+<!--                                </ValidationProvider>-->
                             </div>
                             <p class="leade-text pr-5">Per successful connection</p>
                         </div>
