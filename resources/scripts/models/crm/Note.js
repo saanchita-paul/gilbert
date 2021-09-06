595d97b73cbf38c73ab7129371c55e50011380bc
sazzad ahmed <sazzad@enkaizen.com>
2021-09-06 11:57:28 +0600

    hood crm bug fixing

--- resources/scripts/models/crm/Note.js
+++ resources/scripts/models/crm/Note.js
@@ -1,3 +1,8 @@
+import DATE_FORMAT from "@scripts/data/constants/DATE_FORMAT";
+import DayJs from "dayjs";
+import UserRoles from "@scripts/data/UserRoles";
+import User from "@scripts/models/User";
+
 export default class Note {
     id = null;
     text = null;
@@ -5,5 +10,59 @@ export default class Note {
     title = null;
     type = null;
     created_at = null;
-    constructor() {}
+    user_role = null;
+    constructor(
+        {
+            id = null,
+            text = null,
+            active = false,
+            title = null,
+            type = null,
+            created_at = null,
+            user_role = null,
+        }
+    ) {
+        this.id = id;
+        this.text = text;
+        this.active = active;
+        this.type = this.mapType(type);
+        this.created_at = new DayJs(created_at).format(DATE_FORMAT.NOTE_TIME);
+        this.user_role = this.mapRole(user_role);
+        this.title = this.mapTitle(type,title, this.user_role);
+    }
+
+    mapType(type)
+    {
+        switch (type)
+        {
+            case 'escalated':
+                return 'Escalated';
+                break;
+
+            case 'confirmed_connection':
+                return 'Confirmed Connection';
+                break;
+            case 'close_connection':
+                return 'Close Connection';
+                break;
+            case 'regular':
+                return 'Regular';
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    mapRole(role)
+    {
+        return UserRoles.HOOD.find(dt=> dt.value == role)?.text;
+    }
+    mapTitle(type,title, role)
+    {
+        if(type.toLowerCase() == 'regular')
+        {
+            return title + '['+role+']';
+        }
+    }
 }
