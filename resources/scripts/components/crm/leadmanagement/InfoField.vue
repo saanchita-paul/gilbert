19e53f025499d1665e5217c203ad0474a06334a5
Md Didar <didar@enkaizen.com>
2021-09-07 13:57:43 +0600

    HACT-37

--- resources/scripts/components/crm/leadmanagement/InfoField.vue
+++ resources/scripts/components/crm/leadmanagement/InfoField.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-row>
+    <v-row class="personal-details-row">
         <v-col cols="4">
-            <p class="sub-title title-align pl-6 property_width">Personal Details</p>
+            <p class="sub-title title-align">Personal Details</p>
             <div class="crm-text-field">
                 <div class="field-label">
                     <span>Title</span>
@@ -707,7 +707,5 @@ export default {
 </script>
 
 <style scoped>
-    .property_width{
-        width: 60% !important;
-    }
+   
 </style>
