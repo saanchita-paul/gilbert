3dc3ae88e58130ca71ea24649e9c7f02f4b22a4c
sazzad ahmed <sazzad@enkaizen.com>
2021-09-17 10:59:03 +0600

    [Bug fixing 149] on boarding email

--- resources/views/email/agency/invite_user.blade.php
+++ resources/views/email/agency/invite_user.blade.php
@@ -37,15 +37,17 @@
     ​
 
     <section class="main-content" style="display: flex">
-        <div style="width: 45%">
-            <p>Hi {{$profile->first_name}}!</p>
-            <p>Welcome to HOOD!</p>
+        <div style="width: 35%;color:black; padding:0px 20px">
+            <p >Hi {{$profile->first_name}}!</p>
+            <br/>
+            <h2 style="margin-top:10px;font-weight: bold; font-size: 18px">Welcome to HOOD!</h2>
+            <br/>
             <p>You are now part of a community that helps moving households
                 connect with service providers across Australia!</p>
             <p>You received this email as confirmation of your HOOD account.</p>
-            <br>
+            <br/>
             <p>Click the button below to get started!</p>
-            <br>
+            <br/>
             <div class="btn-area">
                 <a class="started-btn" style=" background-color: #532D86;
         color: #fff;
@@ -54,10 +56,12 @@
         border-radius: 4px;
         margin-bottom: 20px;" target="_blank" href="#">Let’s get started</a>
             </div>
+            <br/>
+            <br/>
             @include('email.agency.layout.footer');
         </div>
-        <div style="width: 50%">
-            <img src="https://d366-103-143-255-11.ngrok.io/hood-crm-dashboard/public/assets/images/agent.png" width="100%;">
+        <div style="width: 60%">
+            <img src="https://d32f-103-143-255-12.ngrok.io/hood-crm-dashboard/public/assets/images/agent.png" width="100%;">
         </div>
 
     </section>
--- resources/views/email/agency/layout/footer.blade.php
+++ resources/views/email/agency/layout/footer.blade.php
@@ -1,3 +1,3 @@
 
-<p><small>Thank you,</small></p>
+<p>Thank you,</p>
 <p>HOOD Support Team</p>
