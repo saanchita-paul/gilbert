c47b96634404b7d38f9e02c28b7342bec7a9f3d8
sazzad ahmed <sazzad@enkaizen.com>
2021-09-21 07:58:44 +0600

    [Bug fixing-166] add fabicon to hood-dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hood Dashboard</title>
+    <link rel="icon" href="{{ url('assets/images/favicon.png') }}">
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{mix('css/app.css')}}">
