a303d1c695b67b35fb680f8b1abdb56b30074c6a
sazzad ahmed <sazzad@enkaizen.com>
2021-09-22 09:16:56 +0600

    [Bug 145] image logo change

--- resources/views/email/agency/layout/header.blade.php
+++ resources/views/email/agency/layout/header.blade.php
@@ -1,3 +1,3 @@
 <header  style="width: 100%;max-height: 200px; border:2px solid transparent;background-color: #532D86;">
-    <img src="http://testhood.brc.technology/wp-content/uploads/2021/09/email-logo.png" width="210px;">
+    <img src="https://devcrmagency.hood.ai/assets/images/email_logo.png" width="210px;">
 </header>
