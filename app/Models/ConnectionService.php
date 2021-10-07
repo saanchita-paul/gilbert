d91e90ca674c02ddcfa6868cbbc078ea29810002
sazzad ahmed <sazzad@enkaizen.com>
2021-10-07 10:23:26 +0600

    [CRM-bug-fix-192] metrix data issue resolved

--- app/Models/ConnectionService.php
+++ app/Models/ConnectionService.php
@@ -49,22 +49,25 @@ class ConnectionService extends Model
         return $this->belongsTo(ConnectionApplication::class);
     }
 
-    public function allApplicationMetricsCount(array $matrixReq, $user)
+    public function allApplicationMetricsCount(array $matrixReq, User $user)
     {
 
         $service = DB::table('connection_services AS CS');
+        info('user',[$user->profile_type]);
         if(!empty($matrixReq['agency_id'])){
             $service->where('CA.agency_id', $matrixReq['agency_id']);
-        }else{
+        } else {
             if($user->profile_type === AgentProfile::class) {
-            $officeId = $user->profile->office_id;
-                $service->where('CA.office_id', $officeId);
+            $profileId = $user->profile->id;
+                $service->where('CA.created_by', $profileId);
             }
+
         }
 
         $copyService = $service;
 
-          $result = $service->leftJoin('connection_applications AS CA', 'CA.id', '=', 'CS.connection_application_id')
+          $result = $service->leftJoin('connection_applications AS CA', 'CA.id', '=',
+              'CS.connection_application_id')
             ->select(
                 DB::raw("SUM(CASE
             WHEN CA.status = 4 OR CA.status = 2 THEN 1 ELSE 0 END) AS applications"),
@@ -86,12 +89,33 @@ class ConnectionService extends Model
             )
             ->get();
 
-          $appCount = DB::table('connection_applications')->whereIn('status',[
-              ConnectionApplication::STATUS_ACCEPTED,
-              ConnectionApplication::STATUS_SUBMITTED,
-              ConnectionApplication::STATUS_ASSIGNED,
-              ])->count();
-        $nopayCount = DB::table('connection_applications')->where('status','=',ConnectionApplication::STATUS_REJECTED)->count();
+        if(!empty($matrixReq['agency_id'])){
+            $appCount = DB::table('connection_applications')->whereIn('status',[
+                ConnectionApplication::STATUS_ACCEPTED,
+                ConnectionApplication::STATUS_EA_PROCESSINF,
+                ConnectionApplication::STATUS_REJECTED,
+            ])->where('agency_id', $matrixReq['agency_id'])
+                ->count();
+        }
+        else {
+            if($user->profile_type === AgentProfile::class) {
+                $agency_id = $user->profile->agency_id;
+                $appCount = DB::table('connection_applications')->where('agency_id', $agency_id)
+                    ->count();
+            }
+            else {
+                $appCount = DB::table('connection_applications')->whereIn('status',[
+                    ConnectionApplication::STATUS_ACCEPTED,
+                    ConnectionApplication::STATUS_EA_PROCESSINF,
+                    ConnectionApplication::STATUS_REJECTED,
+                ])
+                    ->count();
+            }
+
+        }
+
+        $nopayCount = DB::table('connection_applications')->where('status','=',
+            ConnectionApplication::STATUS_CLOSED)->count();
 
 
 
