4cf1708959479cb88079d8c5148cdadc15e7e664
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-10-25 11:00:32 +0600

    Revert "module added"
    This reverts commit 314c4e163b1e12323eb5b7ad7e2a77601d29d544.

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Agency\ApplicationController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Foxie\FoxieController;
 use App\Http\Controllers\UserInvitationController;
+use App\Http\Controllers\UtilityController;
 use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
@@ -102,12 +103,11 @@ Route::post('/invitation/change-password', [UserInvitationController::class, 'pa
 Route::post('/register/email-validation', [AuthController::class, 'isValidUser']);
 Route::get('/users/is-unique-email', [AuthController::class, 'isEmailValid']);
 
-Route::namespace('Foxie')->group(function () {
-    Route::group(['middleware' => ['foxie.suger.leads']], function () {
-        Route::prefix('foxie')->group(function () {
-            Route::post('/lead', [FoxieController::class, 'store']);
-            Route::patch('/lead/{id}', [FoxieController::class, 'update']);
-        });
+
+Route::group(['middleware' => ['foxie.suger.leads']], function () {
+    Route::prefix('foxie')->group(function () {
+        Route::post('/lead', [FoxieController::class, 'store']);
+        Route::patch('/lead/{id}', [FoxieController::class, 'update']);
     });
 });
 
