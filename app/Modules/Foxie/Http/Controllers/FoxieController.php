ac0d46e9524a51f627c8d09b44cff418fc7f0b45
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-10-26 11:13:43 +0600

    namespace separated

---
+++ app/Modules/Foxie/Http/Controllers/FoxieController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Foxie\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Foxie\Services\SugerLeadService;
+use Illuminate\Http\Request;
+
+class FoxieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        // $sugerLead =  SugerLead::create(["all_fields_dump" => json_encode($request->all())]);
+        $sugerLead = new SugerLeadService($request);
+        $response = $sugerLead->create();
+        return response( $response , 200); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $sugerLead = new SugerLeadService($request , $id);
+        $msg =  $sugerLead->update($request , $id);
+        return response($msg["response"] , $msg["status"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Modules/Foxie/Services/SugerLeadService.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Foxie\Services;
+
+use App\Models\Agency;
+use Illuminate\Http\Request;
+use Foxie\Models\SugerLead;
+use Illuminate\Support\Facades\Log;
+use App\Models\ConnectionApplication;
+use Exception;
+
+class SugerLeadService
+{
+    /**
+     * @var mixed|null
+     */
+    private SugerLead $lead;
+    private ConnectionApplication $connectionApplication;
+
+    private function setAttribute(Request $request){
+        $this->connectionApplication->first_name = $request->first_name ?? 'Iron';
+        $this->connectionApplication->last_name = $request->last_name ?? 'Man' ;
+        $this->connectionApplication->source = ConnectionApplication::SOURCE_FOXIE ;
+        $this->connectionApplication->email = $request->email1 ?? 'abc@hood.ai';
+        $this->connectionApplication->dob = date("Y-m-d", strtotime($request->birthdate)) ?? '1991/08/09';
+        $this->connectionApplication->moving_date = date("Y-m-d", strtotime($request->move_in_date_c))  ?? '2021/10/02';
+        $this->connectionApplication->address_unit = $request->primary_address_unit_c ?? '';
+        $this->connectionApplication->street_address = $request->primary_address_street ?? 'Queen Street';
+        $this->connectionApplication->city = $request->primary_address_city ?? 'Brisbane City';
+        $this->connectionApplication->postcode = $request->alt_address_postcode ?? '4000';
+        $this->connectionApplication->state = $request->primary_address_state ?? 'Queensland';
+        $this->connectionApplication->country = $request->primary_address_country ?? 'Australia';
+        $this->connectionApplication->nmi = $request->electricity_nmi_c ?? '';
+        $this->connectionApplication->mirn = $request->gas_mirn_c ?? '';
+        $this->connectionApplication->unit_number = $request->primary_address_unit_c ?? '';
+        $this->connectionApplication->plan_type = $request->meter_plan_type_c ?? '3';
+        $this->connectionApplication->created_at = now();
+        $this->connectionApplication->updated_at = now();
+        // $this->connectionApplication->title = $request->salutation ?? 'Mr';
+        $this->connectionApplication->title = 'Mr';
+
+        $this->lead->created = now();
+        $this->lead->updated = now();
+    }
+
+
+    private function setOfficeAndAgencyId(){
+        try {
+            $agency = Agency::where('name' , "Foxie-Hood-Agent")->first();
+            $this->connectionApplication->agency_id = $agency?->id ?? 1;
+            $this->connectionApplication->office_id = $agency?->offices[0]?->id ?? 1;
+            if(!$agency) throw new Exception('Please run FoxieSeeder');
+        } catch (\Throwable $th) {
+            Log::error("Please run FoxieSeeder");
+        }
+    }
+
+    /**
+     * Create new ConnectionApplication.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return ConnectionApplication $newApplication
+     */
+    public function create(Request $request): ConnectionApplication
+    {   
+        $this->connectionApplication = new ConnectionApplication;
+        $this->lead = new SugerLead();
+        
+        $this->setOfficeAndAgencyId();
+        $this->setAttribute($request);
+        
+        $this->connectionApplication->status = ConnectionApplication::STATUS_UNASSIGNED;
+        $this->connectionApplication->save();
+        $this->lead->all_fields_dump = json_encode(request()->all());
+        $this->lead->connection_application_id = $this->connectionApplication->id;
+        $this->lead->save();
+
+        return $this->connectionApplication;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return bool $successOrFailed
+     */
+    public function update(Request $request, $id): bool
+    {
+        try {
+            $this->connectionApplication = ConnectionApplication::findOrFail($id);
+            $this->lead = $this->connectionApplication->SugerLead;
+            
+            $this->setAttribute($request);
+            
+            $this->connectionApplication->save();
+            $all_fields_dump =  json_decode($this->lead->all_fields_dump);
+            $mergedUpdatedData =  collect($all_fields_dump)->merge($request->all());
+            $this->lead->update(["all_fields_dump" => json_encode($mergedUpdatedData)]);
+            
+            return true;
+        } catch (\Throwable $th) {
+            throw new Exception("Error Processing Request", 1);
+        }
+    }
+
+    /**
+     * Show the specified resource in storage.
+     *
+     * @param  String $fromDate
+     * @param  String $toDate
+     * @param  int  $id
+     * @return Ojbect $leads
+     */
+    public function show(String $from = null , String $to = null , $id = null) : Object
+    {   
+        try {
+            $leads = null;
+            if($id == null){
+                $leads =  ConnectionApplication::where('created_at', '>=', $from)->where('created_at', '<=', $to)->where('source' , ConnectionApplication::SOURCE_FOXIE )->get();
+            }else{
+                $leads  = ConnectionApplication::where( 'source' , ConnectionApplication::SOURCE_FOXIE )->where('id' , $id)->get()[0];
+                $leads ?? throw new Exception("Error Processing Request", 1);
+            }
+            return $leads;
+        } catch (\Throwable $th) {
+            throw new Exception("Lead not found", 1);
+        }
+    }
+
+}
