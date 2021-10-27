a17872238b89729c0c812c61778aa8e4624df20c
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-10-27 15:36:20 +0600

    migration added

--- database/migrations/2021_10_27_085958_alter_roles_table.php
+++ database/migrations/2021_10_27_085958_alter_roles_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AlterRolesTable extends Migration
 {
@@ -13,7 +14,20 @@ class AlterRolesTable extends Migration
      */
     public function up()
     {
-        //
+        $roles = [
+            'agency_assistant_property_manager',
+            'agency_office_business_development_manager',
+            'agency_sales_pa',
+            'agency_receptionist',
+        ];
+
+        foreach ($roles as $value) {
+            try {
+                Role::firstOrCreate(['name' => $value]);
+            } catch (\Exception $ex) {
+                \Log::error($ex->getMessage());
+            }
+        }
     }
 
     /**
