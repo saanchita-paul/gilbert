00c1619319c80a4afb6663e657158ade5a7f6745
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-11-01 11:01:47 +0600

    only leads source with suger can be updated

--- app/Modules/Foxie/Services/SugerLeadService.php
+++ app/Modules/Foxie/Services/SugerLeadService.php
@@ -119,7 +119,7 @@ class SugerLeadService
     public function update(Request $request, $id): bool
     {
         try {
-            $this->connectionApplication = ConnectionApplication::findOrFail($id);
+            $this->connectionApplication = ConnectionApplication::where('id' , $id)->where('source' , ConnectionApplication::SOURCE_FOXIE)->first();
             $this->lead = $this->connectionApplication->SugerLead;
             
             $this->setAttributeUpdate($request);
