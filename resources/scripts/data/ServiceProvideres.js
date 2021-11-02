e82fb11b1e80135760e9420e4ff8a6ba55351ec8
sazzad ahmed <sazzad@enkaizen.com>
2021-11-02 13:09:39 +0600

    update service

--- resources/scripts/data/ServiceProvideres.js
+++ resources/scripts/data/ServiceProvideres.js
@@ -2,42 +2,48 @@ import ProviderPlan from "@scripts/models/crm/ProviderPlan";
 
 export default [
     {
-        name: 'Origin',
+        name: 'origin',
+        formatted_name: 'Origin',
         service_type: 'energy',
+        default_plant: 'origin_go',
         plans: [
             new ProviderPlan({
                 title:'Origin Go',
+                name: 'origin_go',
                 bgColor: 'red',
             }),
             new ProviderPlan({
                 title:'Origin Go Variable',
+                name: 'origin_go_variable',
                 bgColor: 'red',
             }),
             new ProviderPlan({
                 title:'Origin Basic',
+                name: 'origin_basic',
                 bgColor: 'red',
             }),
         ],
         logo: '/bla/bla'
     },
     {
-        name: 'Sumo',
+        name: 'sumo',
+        formatted_name: 'Sumo',
         service_type: 'energy',
+        default_plant: 'sumo_saver',
         plans: [
             new ProviderPlan({
                 title:'Sumo Saver',
+                name: 'sumo_saver',
                 bgColor: 'red',
             }),
             new ProviderPlan({
                 title:'Sumo ASSURE',
+                name: 'sumo_assure',
                 bgColor: 'red',
             }),
             new ProviderPlan({
                 title:'Sumo SELECT',
-                bgColor: 'red',
-            }),
-            new ProviderPlan({
-                title:'A',
+                name: 'sumo_assure',
                 bgColor: 'red',
             }),
         ],
