268f4f1cd67ae24cf734b1e7ac2953713c344585
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-11-16 15:54:35 +0600

    HOC-49 email template integration

--- resources/views/email/invite_user.blade.php
+++ resources/views/email/invite_user.blade.php
@@ -1,8 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+<title>HOOD</title>
+</head>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+    .wrapper {
+        max-width: 560px;
+        margin: 0px auto;
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 14px;
+    }
+    .main-content {
+        padding: 10px 5px;
+    }
+    .btn-area .started-btn {
+        background-color: #532D86;
+        color: #fff;
+        text-decoration: none;
+        padding: 10px 14px;
+        border-radius: 4px;
+        margin-bottom: 20px;
+    }
+    .email-footer {
+        background-color: #532D86;
+        padding: 5px 15px;
+    }
+    .email-footer img {
+        max-width: 150px;
+    }
+</style>
 <body>
-<p>Hi {{$name}}</p>
-<p>You are invited to join at HOOD CRM</p>
-<a href="{{url('confirm-invitation?token=' .$token)}}">ACCEPT INVITATION</a>
+    <div class="wrapper" style=" max-width: 560px;
+    margin: 0px auto;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 14px;">
+        <header>
+            {{-- <img src="{{ asset('assets/images/email/headerFrame.png' )}}" width="100%"> --}}
+            <img src="https://4e5c-45-124-169-170.ngrok.io/assets/images/email/headerFrame.png" width="100%">
+        </header>
+
+        <section class="main-content" style="padding: 10px 5px;">
+            <br>
+            <p>Hi {{$name}},</p>
+            <br>
+            <p>Welcome to HOOD!</p>
+            <br>
+            <p>Please click the button link below to create your password and  activate your account.</p>
+            <p>Once you’re activated, you’re all set to send through the details of your movers.</p>
+            <br>
+            <p> We look forward helping your movers connect their services with ease.</p>
+            <p>Thanks.</p>
+            <br>
+            <div class="btn-area">
+                <a  class="started-btn" 
+                    style="
+                           background-color: #532D86;
+                           color: #fff;
+                           text-decoration: none;
+                           padding: 10px 14px;
+                           border-radius: 4px;
+                           margin-bottom: 20px;"
+
+                    target="_blank" 
+                    href="{{url('confirm-invitation?token=' .$token)}}">
+                   Let’s get started
+                </a>
+            </div>
+            <br>
+            <p><small>Thank you,</small></p>
+            <p>HOOD Support Team</p>
+            <br>
+            <div class="email-footer" style="background-color: #532D86;
+        padding: 5px 15px;">
+                {{-- <img src="{{ asset('assets/images/email/HOODlogo.png')}}" style="max-width: 150px;"> --}}
+                <img src="https://4e5c-45-124-169-170.ngrok.io/assets/images/email/HOODlogo.png" style="max-width: 150px;">
+            </div>
+        </section>
+    </div>
+
 </body>
 </html>
