293fdf1611404c6e259857cdb855884f0a19aa10
Lenin Sheikh <justtlenin@gmail.com>
2021-11-23 19:36:42 +0600

    updated timezone for lead created_at value

--- app/Http/Resources/Agency/ApplicationResource.php
+++ app/Http/Resources/Agency/ApplicationResource.php
@@ -79,7 +79,9 @@ class ApplicationResource extends JsonResource
             'lead_source_description' => $this->SugerLead?->foxie_lead_source_description,
             'source' => $this->source,
             'plan_type' => $this->mapPlan($this->plan_type),
-            'created_at' => (new Carbon($this->created_at, '10'))->format('d/m/Y h:m a')
+
+            #todo: set timezone dynamically based on daylight saving
+            'created_at' => (new Carbon($this->created_at, '11'))->format('d/m/Y h:m a')
         ];
     }
 
