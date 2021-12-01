1ec729545ad6023e6922c2cd356e62cfbdf0d3f0
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-12-01 10:25:46 +0600

    [INT-9] When Title is null, hide title from Application card

--- resources/scripts/models/crm/ApplicationSummary.js
+++ resources/scripts/models/crm/ApplicationSummary.js
@@ -117,7 +117,7 @@ export default class ApplicationSummary {
     ) {
 
         this.id = id;
-        this.applicant_name = title + ' ' + first_name + ' '+ ( isNull(middle_name)?'': middle_name) + ' ' + last_name;
+        this.applicant_name = ( title == null ? '' : title ) + ' ' + first_name + ' '+ ( isNull(middle_name)?'': middle_name) + ' ' + last_name;
         this.first_name = first_name;
         this.middle_name = middle_name;
         this.last_name = last_name;
