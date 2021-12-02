6d08b2adf284a53752055c3b8500ddf6d3ed49a8
sazzad ahmed <sazzad@enkaizen.com>
2021-12-02 09:30:59 +0600

    GNK-2: null issue fixed

--- app/Services/Agency/HubspotContactService.php
+++ app/Services/Agency/HubspotContactService.php
@@ -337,8 +337,9 @@ class HubspotContactService
             ConnectionApplication::SOURCE_FOXIE => 'Foxie',
             ConnectionApplication::SOURCE_HOOD => 'HOOD',
             ConnectionApplication::SOURCE_IGNITE => 'Ignite',
-//            4 => 'OurProperty',
-//            5 => 'PropertyMe'
+            4 => 'OurProperty',
+            5 => 'PropertyMe',
+            default => 'HOOD'
         };
 
     }
