0b8ea494881416229651f97b07fa7aae8851eed1
sazzad ahmed <sazzad@enkaizen.com>
2021-12-02 06:29:04 +0600

    sales api optimization for status check api

--- app/Services/Sales/GetSalesRequestStaus.php
+++ app/Services/Sales/GetSalesRequestStaus.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use GraphQL\Client;
 use GraphQL\Query;
 use GraphQL\Variable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Log;
 
 class GetSalesRequestStaus
@@ -106,6 +107,7 @@ class GetSalesRequestStaus
                     $status = ConnectionApplication::STATUS_EA_PROCESSING;
                 }
 
+
             }
             $connection = ConnectionApplication::where('ea_sales_id', $item->id)->first();
             if(!is_null($connection)) {
@@ -143,9 +145,9 @@ class GetSalesRequestStaus
                 ]
             );
 
-
+        $url = env('EA_SALES_URL','https://apigw-nonprod.energyaustralia.com.au/graphql');
         $client = new Client(
-            'https://apigw-nonprod.energyaustralia.com.au/graphql',
+            $url,
             ['Authorization' => $this->accessToken]);
         $results = $client->runQuery($gql, true, $da );
 
@@ -164,10 +166,11 @@ class GetSalesRequestStaus
         Log::info(json_encode($submitSallData));
         $quotes = $submitSallData?->quotes;
 
-        $processingFlag = false;
+
 
         foreach ($quotes as $quote)
         {
+
             $status = null;
             if($quote->status == 'REJECTED')
             {
@@ -189,16 +192,12 @@ class GetSalesRequestStaus
             {
                 $status = ConnectionService::AC_MANUAL_PROCESSING;
             }
-            Log::info($status);
-
-            $lead = ConnectionApplication::find($leadId);
-            $lead->update(['status'=>$status]);
 
             if($quote->fuel === 'GAS') {
-                $this->updateService( $lead->id, 'gas', $status);
+                $this->updateService( $leadId, 'gas', $status);
             }
             if($quote->fuel === 'ELE') {
-                $this->updateService($lead->id, 'power', $status);
+                $this->updateService($leadId, 'power', $status);
             }
 
         }
@@ -207,6 +206,7 @@ class GetSalesRequestStaus
     }
 
     public function updateService($id, $power, $status = null) {
+        info('put some message here', [$id, $power, $status]);
         $service = ConnectionService::query()
             ->where('connection_application_id', $id)
             ->where('service_type', $power)
@@ -217,7 +217,13 @@ class GetSalesRequestStaus
 
     public function fetchAllSubmittedLead() {
         $leads = ConnectionApplication::query()
+            ->with('connectionServices')
             ->where('status', '=', ConnectionApplication::STATUS_SUBMITTED)
+            ->whereNotNull('vendor_id')
+            ->whereHas('connectionServices', function (Builder $service) {
+                $service->where('provider_name','=','ea')
+                    ->whereIn('status', [ConnectionService::STATUS_EA_PROCESSINF, ConnectionService::AC_MANUAL_PROCESSING]);
+            })
             ->get();
 
         foreach ($leads as $lead) {
