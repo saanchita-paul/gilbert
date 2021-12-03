e0c72a9d46748876350176b8e8616125fc14c03c
Lenin Sheikh <justtlenin@gmail.com>
2021-12-03 12:37:46 +0600

    (PropertyMe)create hubspot contact after lead creation

--- app/Modules/PropertyMe/Services/SaveToConnectionApplication.php
+++ app/Modules/PropertyMe/Services/SaveToConnectionApplication.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\PropertyMe\Services;
 
+use App\Jobs\CreateHubspotProperty;
 use App\Models\ConnectionApplication;
 use App\Models\Office;
 use PropertyMe\PropertyMeLead;
@@ -55,6 +56,7 @@ class SaveToConnectionApplication
         ]);
 
         $this->saveApplicationId($application->id, $lead);
+        CreateHubspotProperty::dispatch($application->id);
     }
 
 
