09aaf91aa0646a0ec4958f7a82fe66424a49f642
Lenin Sheikh <justtlenin@gmail.com>
2021-12-03 09:17:29 +0600

    [INT-12] fixed our property

--- resources/scripts/components/crm/leadmanagement/ApplicantTable.vue
+++ resources/scripts/components/crm/leadmanagement/ApplicantTable.vue
@@ -127,7 +127,8 @@ export default {
                 {text: 'Hood Agent Portal', value: 'hood', icon: '/assets/images/icons/company/hood.png'},
                 {text: 'Foxie CRM', value: 'foxie', icon: '/assets/images/icons/company/foxie.png'},
                 {text: 'Ignite ', value: 'ignite', icon: '/assets/images/icons/company/ignite.png'},
-                {text: 'PropertyMe ', value: 'property_me', icon: '/assets/images/icons/company/propertyMe.png'},
+                {text: 'Our Property ', value: 'our_property', icon: '/assets/images/icons/company/our-property.png'},
+                {text: 'Property Me ', value: 'property_me', icon: '/assets/images/icons/company/propertyMe.png'},
             ],
             headers:  [
                 {
