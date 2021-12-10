afbf2fb952fe7feefc8ab700e483247e68b9deb3
Lenin Sheikh <justtlenin@gmail.com>
2021-12-10 15:12:57 +0600

    [HCO-182] added X-EA-Env header

--- app/Services/Sales/PostSalesService.php
+++ app/Services/Sales/PostSalesService.php
@@ -137,9 +137,10 @@ class PostSalesService
 
         try{
             $url = env('EA_SALES_URL','https://apigw-nonprod.energyaustralia.com.au/graphql');
+            $XEAEnv = config('x_ea_env');
             $client = new Client(
                 $url,
-                ['Authorization' => $this->accessToken]);
+                ['Authorization' => $this->accessToken, 'X-EA-Env' => $XEAEnv]);
                 $gql = (new Mutation('submitSale'))
                 ->setVariables([new Variable('data', 'VendorSaleRequest!')])
                 ->setArguments(['input' => '$data'])
