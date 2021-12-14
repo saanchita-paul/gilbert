850bcd11ebdc8aa29315127ab7de8b14a73c3e37
Lenin Sheikh <justtlenin@gmail.com>
2021-12-14 15:16:18 +0600

    [Sales-dashboard] Added API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SalesDashboard\SalesDashboardController;
 use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Broadcast;
@@ -111,6 +112,17 @@ Route::post('/register/email-validation', [AuthController::class, 'isValidUser']
 Route::get('/users/is-unique-email', [AuthController::class, 'isEmailValid']);
 Route::get('/users/is-unique-email-update', [AuthController::class, 'isEmailTaken']);
 
+Route::get('/{id}/submit-water-lead', [ApplicationController::class, 'submitWaterLead']);
+
+
+/***
+ * Sales Dashboard
+ */
+Route::get('/sales-dashboard/home', [SalesDashboardController::class, 'home']);
+
+
+
+
 /**
  * test routes
  */
@@ -126,7 +138,6 @@ Route::post('/our-property/lead', [OurPropertyController::class, 'createOurPrope
 
 
 
-Route::get('/{id}/submit-water-lead', [ApplicationController::class, 'submitWaterLead']);
 
 
 Route::get("/karan/sales-status", function () {
