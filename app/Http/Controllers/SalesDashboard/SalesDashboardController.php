5fb632dd98da106528f9193b93bb8d965a042608
Lenin Sheikh <justtlenin@gmail.com>
2021-12-15 08:27:14 +0600

    [Sales-dashboard] mocked api response

--- app/Http/Controllers/SalesDashboard/SalesDashboardController.php
+++ app/Http/Controllers/SalesDashboard/SalesDashboardController.php
@@ -11,7 +11,23 @@ class SalesDashboardController
     {
         $data =  (new SaleDashboardService("2021-12-02", "2021-12-02"))->run();
         return response()->json(['data' => [
-            'energy' => $data,
+            'energy' => [
+                "submission" => [
+                    'total' => 100,
+                    'ea_power' => 10,
+                    'sumo_power' => 2,
+                    'sumo_gas' => 2,
+                    'ea_gas' => 2,
+                    "no_frills" => 12,
+                ],
+                "conversion" => [
+                    'total' => 100,
+                    'ea' => 10,
+                    'sumo' => 2,
+                    'power' => 33,
+                    'gas'
+                ]
+            ],
             'water' => []
         ]]);
     }
