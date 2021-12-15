4298bdccbccbb9bc87a017ca1cdb07a5f00486b0
Lenin Sheikh <justtlenin@gmail.com>
2021-12-15 10:15:40 +0600

    [Sales-dashboard] updated mocked response

--- app/Http/Controllers/SalesDashboard/SalesDashboardController.php
+++ app/Http/Controllers/SalesDashboard/SalesDashboardController.php
@@ -14,11 +14,14 @@ class SalesDashboardController
             'energy' => [
                 "submission" => [
                     'total' => 100,
-                    'ea_power' => 10,
-                    'sumo_power' => 2,
-                    'sumo_gas' => 2,
-                    'ea_gas' => 2,
-                    "no_frills" => 12,
+                    'ea_gas_total_plan' => 10,
+                    'ea_gas_no_frills' => 10,
+                    'ea_gas_basic_plan' => 10,
+                    'ea_power_total_plan' => 2,
+                    'ea_power_no_frills' => 2,
+                    'ea_power_basic_plan' => 2,
+                    'sumo_power_freedom' => 2,
+                    'sumo_gas_freedom' => 2,
                 ],
                 "conversion" => [
                     'total' => 100,
