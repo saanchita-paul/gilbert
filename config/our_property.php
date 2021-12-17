a53f97f72449124272972312a51f6a70d40fc8c5
Lenin Sheikh <justtlenin@gmail.com>
2021-12-17 16:02:26 +0600

    [INT-18] OurProperty dynamically assign agent and agency

---
+++ config/our_property.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'agent_email' => env("OUR_PROPERTY_AGENT_EMAIL", "kbailey.sunnybankhills@ljh.com.au"),
+    'agency_name' => env("OUR_PROPERTY_AGENCY_NAME", "LJ Hooker Sunnybank Hills"),
+    'agency_id' => env("OUR_PROPERTY_AGENCY_ID", "74651"),
+];
