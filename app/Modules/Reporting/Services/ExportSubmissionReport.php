acbf07ef3fb7907d9e1389b67d53ae32f97255b9
Tonmoy Enkaizen <tonmoy@enkaizen.com>
2021-12-21 16:15:37 +0600

    [HCO-239] Operations Dashboard - download report Issue

--- app/Modules/Reporting/Services/ExportSubmissionReport.php
+++ app/Modules/Reporting/Services/ExportSubmissionReport.php
@@ -20,6 +20,7 @@ class ExportSubmissionReport
     private string $endDate;
     private $timezone;
     private $serviceType;
+    private $type;
 
     private array $energyType = [
         ConnectionService::TYPE_ELECTRICITY,
@@ -33,6 +34,7 @@ class ExportSubmissionReport
     public function __construct(string $type, string $start, string $end)
     {
         $this->setDateRange($start, $end);
+        $this->type = $type;
         $this->serviceType = $type === 'energy' ? $this->energyType : $this->waterType;
     }
 
@@ -46,8 +48,9 @@ class ExportSubmissionReport
 
     private function export()
     {
-//        return  $this->leadsData;
-        $name = now()->format('U') . '.csv';
+        // $name = now()->format('U') . '.csv';
+        $date = now()->format('d_m_Y');
+        $name = 'Gilbert_leads_report_'.$this->type.'_'.$date.'.csv';
         return (new FastExcel($this->leadsData))->download($name);
     }
 
@@ -56,6 +59,12 @@ class ExportSubmissionReport
         foreach ($data as $datum) {
             $datum->Lead_Source = $this->getLeadSrc($datum->Lead_Source);
             $datum->Lead_Status = GilbertStatusMapper::getStatusAsText ($datum->Lead_Status);
+            
+            $this->setAgencyName($datum);
+
+            unset($datum->Foxie_Agency_Name);
+            unset($datum->Foxie_Agent_Name);
+
             $this->leadsData[] = $datum;
         }
     }
@@ -79,7 +88,10 @@ class ExportSubmissionReport
                 cs.provider_name as `Utility_Provider`,
                 cs.service_type as `Utility_Service`,
                 cs.plan_type as `Utility_Plan`,
-                cs.status as `Lead_Status`
+                cs.status as `Lead_Status`,
+                sl.agency_name as `Foxie_Agency_Name`,
+                sl.agent_name as `Foxie_Agent_Name`,
+                rr.reason_text as `Reason_Text`
             ")
             ->leftJoin('connection_applications as ca', 'cs.connection_application_id', '=', 'ca.id')
             ->leftJoin('agencies as ag', 'ca.agency_id', '=', 'ag.id')
@@ -88,9 +100,11 @@ class ExportSubmissionReport
                 $join->on('ap.id', '=', 'u.profile_id')
                     ->where('profile_type', AgentProfile::class);
             })
+            ->leftJoin('suger_leads as sl', 'ca.id', '=', 'sl.connection_application_id')
+            ->leftJoin('rejection_reasons as rr', 'cs.id', '=', 'rr.connection_service_id')
             ->where('cs.updated_at', '>=', $this->startDate)
             ->where('cs.updated_at', '<=', $this->endDate)
-            ->whereIn('service_type', $this->serviceType)
+            ->whereIn('cs.service_type', $this->serviceType)
             ->get()
             ->toArray();
     }
@@ -99,5 +113,16 @@ class ExportSubmissionReport
     {
         $res = array_search($src, ConnectionApplication::SOURCE_MAPPING);
         return $res ?: "Unknown";
+    }
+
+    private function setAgencyName(object $datum)
+    {
+        if( $datum->Foxie_Agency_Name && $datum->Foxie_Agency_Name !== 'null') {
+            $datum->Agency_Name = $datum->Foxie_Agency_Name;
+        }
+
+        if( $datum->Foxie_Agent_Name && $datum->Foxie_Agent_Name !== 'null') {
+            $datum->Agent_Name = $datum->Foxie_Agent_Name;
+        }
     }
 }
