9cf083877423372ed7f99ff10742297731eb0385
Lenin Sheikh <justtlenin@gmail.com>
2021-12-23 16:41:23 +0600

    [HCO-253] [Advance Search] Create DB migration for Full-Text-Search

---
+++ app/Services/FullTextSearch/FST.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\FullTextSearch;
+
+class FST
+{
+    const INDEX_FT_TENANT_NAME = 'ft_tenant_name';
+    const INDEX_FT_ADDRESS= 'ft_address';
+    const INDEX_FT_PHONE= 'ft_phone';
+}
