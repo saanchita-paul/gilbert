68458f10e04d381a7557ee7035791061c39a1400
riyadenkaizen <ahsan.riyad@enkaizen.com>
2021-12-27 12:14:27 +0600

    LeadSearchFilterModel added

---
+++ resources/scripts/models/LeadSearchFilterModel.js
@@ -0,0 +1,30 @@
+import { isEmpty } from "lodash-es";
+
+class LeadSearchFilterModel {
+    LeadSearchFilterModel({
+        tenancy_name,
+        source,
+        mobile,
+        address,
+        tenancy_type,
+    } = {}) {
+        this.tenancy_name = tenancy_name;
+        this.source = source;
+        this.mobile = mobile;
+        this.address = address;
+        this.tenancy_type = tenancy_type;
+    }
+
+    isSearchEmpty() {
+        const isNoValue =
+            isEmpty(this.tenancy_name) &&
+            isEmpty(this.source) &&
+            isEmpty(this.mobile) &&
+            isEmpty(this.address) &&
+            isEmpty(this.tenancy_type);
+        
+        return isNoValue ? true : false;
+    }
+}
+
+export { LeadSearchFilterModel };
