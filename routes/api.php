ac33eca8a03bc81c8854e3764e923cefdbb7cdb2
Tonmoy Enkaizen <tonmoy@enkaizen.com>
2022-01-05 13:11:44 +0600

    [HCO-176] Filter Agent list dropdown with search

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,7 @@ Route::namespace('agency')->middleware(['auth:sanctum'])->group(function () {
     Route::get('/offices/office/{id}', [OfficeController::class, 'getOnlyOffice']);
     Route::post('/offices/{id}/update', [OfficeController::class, 'updateOffice']);
     Route::get('/offices/{officeId}/users', [AgentProfileController::class, 'index']);
+    Route::get('/offices/{officeId}/agents', [AgentProfileController::class, 'getAgentList']);
     Route::post('/offices/{officeId}/users', [AgentProfileController::class, 'createAgent']);
 
     Route::get('/office-agents', [AgentProfileController::class, 'officeAgents']);
