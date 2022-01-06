f73cb44c3d85cbdb688bf061a28a3962624082a6
riyadenkaizen <ahsan.riyad@enkaizen.com>
2022-01-06 11:44:26 +0600

    agent name updated

--- resources/scripts/components/crm/agent/AgentApplicationTable.vue
+++ resources/scripts/components/crm/agent/AgentApplicationTable.vue
@@ -155,7 +155,8 @@ export default {
           this.loadApplication();
       },
       removeFilters(){
-          
+         this.searchFilterModel.clear();
+         this.loadApplication();
       }
     },
   watch: {
