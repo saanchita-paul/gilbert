5458c0d65887e70a3edb68a8c0072930a0168df8
sazzad ahmed <sazzad@enkaizen.com>
2022-01-07 08:20:36 +0600

    [HCO-292] bug fixing .. User is indefined

--- resources/scripts/components/crm/agent/AgentCreateNewApplication.vue
+++ resources/scripts/components/crm/agent/AgentCreateNewApplication.vue
@@ -772,7 +772,8 @@ export default {
                 state: "",
                 country: "",
             },
-            loadSubmit: false
+            loadSubmit: false,
+            user: null,
         }
 
     },
@@ -903,6 +904,9 @@ export default {
             this.authorized_person.dob = (new DayJs(this.authorized_person_dob).format('DD/MM/YYYY'));
         },
 
+    },
+    async mounted() {
+        this.user = await AuthService.getAuthUser();
     }
 };
 </script>
