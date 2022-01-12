036a90d4bd67377e0e103085b4d4d4a032f0d1cc
Lenin Sheikh <justtlenin@gmail.com>
2022-01-12 15:28:44 +0600

    [PROPME-18] added a test API to run fetch property_me lead from browser.

--- app/Modules/PropertyMe/route.php
+++ app/Modules/PropertyMe/route.php
@@ -1,6 +1,7 @@
 <?php
 
 use PropertyMe\Http\Controllers\AuthController;
+use PropertyMe\PropertyMeLead;
 use PropertyMe\Services\AuthService;
 
 Route::prefix('property-me')->group(function () {
@@ -9,4 +10,14 @@ Route::prefix('property-me')->group(function () {
 Route::get("/property-me/callback", [AuthController::class, "callback"]);
 
 
+/**
+ * This a test API to run a command.
+ * this API maybe removed in the future.
+ */
+Route::get("/property-me/run-fetch-contacts", function () {
+    $prevCount = PropertyMeLead::query()->count();
+    Artisan::call('property_me:save_contact');
+    $newLead = $prevCount - PropertyMeLead::query()->count();
 
+    return "New $newLead lead is saved!";
+});
