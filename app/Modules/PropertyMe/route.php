023345540f0e4362ea2c28229555308199535106
Lenin Sheikh <justtlenin@gmail.com>
2022-01-12 11:06:27 +0600

    [PROPME-17] fixed callback url

--- app/Modules/PropertyMe/route.php
+++ app/Modules/PropertyMe/route.php
@@ -6,12 +6,7 @@ use PropertyMe\Services\AuthService;
 Route::prefix('property-me')->group(function () {
     Route::get('/authorize', [AuthController::class, "authorizeWithCode"]);
 });
-Route::get("/home/callback", [AuthController::class, "callback"]);
+Route::get("/property-me/callback", [AuthController::class, "callback"]);
 
 
-Route::get("/pp", function () {
-
-    $url = '/okay?' . http_build_query(request()->toArray());
-    return redirect($url)->withHeaders(request()->headers);
-});
 
