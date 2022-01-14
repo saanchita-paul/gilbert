899403cb4f7cf27e8e6ec7bf1909fdd1e37baa1d
Lenin Sheikh <justtlenin@gmail.com>
2022-01-14 14:10:46 +0600

    [PROPERTY-ME] updated messages

--- app/Modules/PropertyMe/route.php
+++ app/Modules/PropertyMe/route.php
@@ -17,7 +17,8 @@ Route::get("/property-me/callback", [AuthController::class, "callback"]);
 Route::get("/property-me/run-fetch-contacts", function () {
     $prevCount = PropertyMeLead::query()->count();
     Artisan::call('property_me:save_contact');
-    $newLead = $prevCount - PropertyMeLead::query()->count();
 
-    return "New $newLead lead is saved!";
+    sleep(5);
+    $newLead = PropertyMeLead::query()->count() - $prevCount;
+    return "<div style='display: flex; flex-direction: column; align-items: center'><h1 style='width: 100%; color: #480a8a; text-align: center;'>New <span style='color: #5C229A; font-weight: bold; font-size: 60px'>$newLead</span><span> lead is saved!</h1><img src='https://thumbs.gfycat.com/BraveSoftInexpectatumpleco-max-1mb.gif' />";
 });
