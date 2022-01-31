95db035c2db0cc1ceab24318e80df3dce8d05027
riyad <riyad298@gmail.com>
2022-01-31 16:19:31 +0600

    [HCO-414] breadcrumb label sync

--- resources/scripts/components/crm/office/CrmOfficeDataTable.vue
+++ resources/scripts/components/crm/office/CrmOfficeDataTable.vue
@@ -65,6 +65,9 @@ import OfficeService from "@scripts/services/crm/OfficeService";
 import LeadMetrics from "@scripts/components/crm/LeadMetrics";
 import AgencyService from "@scripts/services/crm/AgencyService";
 import AgencyEditModal from "@scripts/components/crm/modals/AgencyEditModal";
+import AuthService from "@scripts/services/AuthService";
+
+
 export default {
 name: "CrmOfficeDataTable",
     props: {
@@ -213,6 +216,7 @@ name: "CrmOfficeDataTable",
             let payload = {name: agency}
            await AgencyService.updateAgency(payload, this.$route.params.id)
             this.editAgencyNameFlag = false;
+            AuthService.setBreadcrumbs(this.$route.meta.breadcrumbType, this.$route.params)
 
         },
 
