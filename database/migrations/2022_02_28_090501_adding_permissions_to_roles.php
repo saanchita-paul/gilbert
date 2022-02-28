3d87078056ffe7ee6e0ae43c33ea43f81c054b93
Tonmoy Enkaizen <tonmoy@enkaizen.com>
2022-02-28 16:20:45 +0600

    [HCO-539] Added Hood permissions seeder

--- database/migrations/2022_02_28_090501_adding_permissions_to_roles.php
+++ database/migrations/2022_02_28_090501_adding_permissions_to_roles.php
@@ -35,17 +35,17 @@ class AddingPermissionsToRoles extends Migration
         $hood_customer_rep = Role::whereName(RolePermission::ROLE_HOOD_CUSTOMER_REP)->first();
 
         $agencyRoles = [
-            $agency_agent,
-            $agency_office_allocator,
-            $agency_office_admin, 
-            $agency_office_director, 
-            $agency_office_property_manager,
-            $agency_office_senior_property_manager,
-            $agency_assistant_property_manager, 
-            $agency_office_real_estate_agent, 
-            $agency_office_business_development_manager,
-            $agency_sales_pa, 
-            $agency_receptionist, 
+            $agency_agent ? $agency_agent : null,
+            $agency_office_allocator ? $agency_office_allocator : null,
+            $agency_office_admin ? $agency_office_admin : null, 
+            $agency_office_director ? $agency_office_director : null,
+            $agency_office_property_manager ? $agency_office_property_manager : null,
+            $agency_office_senior_property_manager ? $agency_office_senior_property_manager : null,
+            $agency_assistant_property_manager ? $agency_assistant_property_manager : null, 
+            $agency_office_real_estate_agent ? $agency_office_real_estate_agent : null,
+            $agency_office_business_development_manager ? $agency_office_business_development_manager : null,
+            $agency_sales_pa ? $agency_sales_pa : null, 
+            $agency_receptionist ? $agency_receptionist : null
         ];
         
 
@@ -58,8 +58,26 @@ class AddingPermissionsToRoles extends Migration
             $model->save();
         }
 
+        foreach ($agencyRoles as $role) {
+            if ($role) {
+                $role->givePermissionTo(RolePermissionService::hoodAdminPermissions());
+            }
+        }
+
         if ($hood_admin) {
             $hood_admin->givePermissionTo(RolePermissionService::hoodAdminPermissions());
+        }
+
+        if ($hood_agent) {
+            $hood_agent->givePermissionTo(RolePermissionService::hoodAgentPermissions());
+        }
+
+        if ($hood_team_lead) {
+            $hood_team_lead->givePermissionTo(RolePermissionService::hoodTeamLeadPermissions());
+        }
+
+        if ($hood_customer_rep) {
+            $hood_customer_rep->givePermissionTo(RolePermissionService::hoodCustomerRepPermissions());
         }
     }
 
@@ -70,8 +88,60 @@ class AddingPermissionsToRoles extends Migration
      */
     public function down()
     {
-        Schema::table('roles', function (Blueprint $table) {
-            //
-        });
+        $agency_agent = Role::whereName(RolePermission::ROLE_AGENCY_AGENT)->first();
+        $agency_office_allocator = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_ALLOCATOR)->first();
+        $agency_office_admin = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_ADMIN)->first();
+        $agency_office_director = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_DIRECTOR)->first();
+        $agency_office_property_manager = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_PROPERTY_MANAGER)->first();
+        $agency_office_senior_property_manager = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_SENIOR_PROPERTY_MANAGER)->first();
+        $agency_assistant_property_manager = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_ASSISTANT_PROPERTY_MANAGER)->first();
+        $agency_office_real_estate_agent = Role::whereName(RolePermission::ROLE_AGENCY_OFFICE_REAL_ESTATE_AGENT)->first();
+        $agency_office_business_development_manager = Role::whereName(RolePermission::ROLE_AGENCY_BUSINESS_DEVELOPMENT_MANAGER)->first();
+        $agency_sales_pa = Role::whereName(RolePermission::ROLE_AGENCY_SALES_PA)->first();
+        $agency_receptionist = Role::whereName(RolePermission::ROLE_AGENCY_RECEPTIONIST)->first();
+        
+        $hood_admin = Role::whereName(RolePermission::ROLE_HOOD_ADMIN)->first();
+        $hood_agent = Role::whereName(RolePermission::ROLE_HOOD_AGENT)->first();
+        $hood_team_lead = Role::whereName(RolePermission::ROLE_HOOD_TEAM_LEAD)->first();
+        $hood_customer_rep = Role::whereName(RolePermission::ROLE_HOOD_CUSTOMER_REP)->first();
+
+        $agencyRoles = [
+            $agency_agent ? $agency_agent : null,
+            $agency_office_allocator ? $agency_office_allocator : null,
+            $agency_office_admin ? $agency_office_admin : null, 
+            $agency_office_director ? $agency_office_director : null,
+            $agency_office_property_manager ? $agency_office_property_manager : null,
+            $agency_office_senior_property_manager ? $agency_office_senior_property_manager : null,
+            $agency_assistant_property_manager ? $agency_assistant_property_manager : null, 
+            $agency_office_real_estate_agent ? $agency_office_real_estate_agent : null,
+            $agency_office_business_development_manager ? $agency_office_business_development_manager : null,
+            $agency_sales_pa ? $agency_sales_pa : null, 
+            $agency_receptionist ? $agency_receptionist : null
+        ];
+
+        foreach ($agencyRoles as $role) {
+            if ($role) {
+                $role->revokePermissionTo(RolePermissionService::hoodAdminPermissions());
+            }
+        }
+
+        if ($hood_admin) {
+            $hood_admin->revokePermissionTo(RolePermissionService::hoodAdminPermissions());
+        }
+
+        if ($hood_agent) {
+            $hood_agent->revokePermissionTo(RolePermissionService::hoodAgentPermissions());
+        }
+
+        if ($hood_team_lead) {
+            $hood_team_lead->revokePermissionTo(RolePermissionService::hoodTeamLeadPermissions());
+        }
+
+        if ($hood_customer_rep) {
+            $hood_customer_rep->revokePermissionTo(RolePermissionService::hoodCustomerRepPermissions());
+        }
+
+        $permissions = RolePermissionService::allPermission();
+        $model = Permission::whereIn('name', $permissions)->delete();
     }
 }
