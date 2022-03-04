68c745bb924c5a2b4b1efc87577cb9a9b310fd40
riyadenkaizen <ahsan.riyad@enkaizen.com>
2022-03-04 10:04:18 +0600

    [HCO-548] metric logic updated

--- app/Models/ConnectionService.php
+++ app/Models/ConnectionService.php
@@ -151,10 +151,10 @@ class ConnectionService extends Model
             WHEN CA.status = 6 THEN 1 ELSE 0 END) AS nonpayable"),
 
                 DB::raw("SUM(CASE
-            WHEN CS.service_type = 'power' and CS.status = 12 AND CA.status = 4 THEN 1 ELSE 0 END) AS power"),
+            WHEN CS.service_type = 'power' and (CS.status = 12 or CS.status = 4 or CS.status=5) AND CA.status = 4 THEN 1 ELSE 0 END) AS power"),
 
                 DB::raw("SUM(CASE
-            WHEN CS.service_type = 'gas' and CS.status = 12 AND CA.status = 4 THEN 1 ELSE 0 END) AS gas"),
+            WHEN CS.service_type = 'gas' and (CS.status = 12 or CS.status = 4 or CS.status=5) AND CA.status = 4 THEN 1 ELSE 0 END) AS gas"),
 
                 DB::raw("SUM(CASE
             WHEN CS.service_type = 'internet' and CS.status = 4 AND CA.status = 4 THEN 1 ELSE 0 END) AS internet"),
