0c91d1c12813c30427a4391f9da9aeb273c5dbba
sazzad ahmed <sazzad@enkaizen.com>
2022-03-14 15:41:02 +0600

    HCO-623 same day next day connecton

--- resources/scripts/services/ea/EAAfterHourService.js
+++ resources/scripts/services/ea/EAAfterHourService.js
@@ -28,10 +28,12 @@ function isTodayAfterHourFlag(ea_distributor, state) {
         return dis.distributor === ea_distributor && dis.state === selectedState.value?.toUpperCase()
     })
 
-    let envTimezone = process.env.MIX_TIME_ZONE;
+    let envTimezone = process.env.MIX_EA_AU_TIME_ZONE;
+    if(!envTimezone) {
+        envTimezone = 0;
+    }
     let dateTime = dayJs().utcOffset(parseInt(envTimezone) * 60);
     let  time = parseInt(dateTime.format('Hm'));
-
     if(selectedDistributor && time <= 1130) {
         return false;
     }
@@ -41,7 +43,10 @@ function isTodayAfterHourFlag(ea_distributor, state) {
 }
 
 function isTomorrowAfterHourFlag() {
-    let envTimezone = process.env.MIX_TIME_ZONE;
+    let envTimezone = process.env.MIX_EA_AU_TIME_ZONE;
+    if(!envTimezone) {
+        envTimezone = 0;
+    }
     let dateTime = dayJs().utcOffset(parseInt(envTimezone) * 60);
     const  time = parseInt(dateTime.format('Hm'));
 
@@ -81,16 +86,16 @@ export default {
             return false;
         }
 
-       let currentDate = parseInt(dayJs().format('D'));
-       let givenDate = parseInt(dayJs(movingDate).format('D'));
+        let currentDate = parseInt(dayJs().format('D'));
+        let givenDate = parseInt(dayJs(movingDate).format('D'));
 
-       if(currentDate === givenDate) {
-           return isTodayAfterHourFlag(ea_distributor, state);
-       }
-       if(currentDate + 1 === givenDate) {
+        if(currentDate === givenDate) {
+            return isTodayAfterHourFlag(ea_distributor, state);
+        }
+        if(currentDate + 1 === givenDate) {
             return isTomorrowAfterHourFlag();
         }
-       return false;
+        return false;
     },
 
 
