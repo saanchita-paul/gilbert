fa2ccef68ce9c5412321f3873b07a43b58fcf379
sazzad ahmed <sazzad@enkaizen.com>
2022-03-22 13:10:03 +0600

    HCO-623 add log with after hour calcultion

--- app/Services/Sales/PostSalesService.php
+++ app/Services/Sales/PostSalesService.php
@@ -211,6 +211,24 @@ class PostSalesService
     {
         $afterHourFlag = $this->connection->getAfterHourPayee();
         $this->connection->update(['after_hour_flag' => $afterHourFlag]);
+
+        $eaService = ConnectionService::query()->where('connection_application_id', $this->connection->id)
+            ->where('service_type', ConnectionService::TYPE_ELECTRICITY)
+            ->first();
+        $distributor = null;
+        if(!empty($eaService))
+        {
+            $distributor = $eaService->distributor;
+        }
+
+        $state = $this->stateMap( $this->connection->state);
+        Log::info('After Hour Flags ', [
+            'state'=> $state,
+            'distributor'=> $distributor,
+            'connection_date'=> $this->connection->moving_date,
+            'after_hour_flag'=> $afterHourFlag,
+        ]);
+
         return $afterHourFlag;
 
 
