389e9b4ad477abb2297a841df4f88f2915dd2cbf
Lenin Sheikh <justtlenin@gmail.com>
2022-03-31 12:14:35 +0600

    [HCO-741] Updated length to 200 character  for name fields in connection_applications table

---
+++ database/migrations/2022_03_31_060631_updated_first_name_length_in_connections_applications.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatedFirstNameLengthInConnectionsApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('connection_applications', function (Blueprint $table) {
+            $table->string('first_name', 200)->change();
+            $table->string('middle_name', 200)->change();
+            $table->string('last_name', 200)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
