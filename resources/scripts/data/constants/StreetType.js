d8c3207ee086fe0092e8c9aa7ed712f7c340fa2b
Tonmoy Asif <tonmoy@enkaizen.com>
2022-04-08 15:47:39 +0600

    [HCO-760] Added all street type in Frontend

--- resources/scripts/data/constants/StreetType.js
+++ resources/scripts/data/constants/StreetType.js
@@ -1,27 +1,814 @@
 export const street_type = [
-    {text: 'ALLEY', value: 'ALLY'},
-    {text: 'ARCADE', value: 'ARC'},
-    {text: 'AVENUE', value: 'AVE'},
-    {text: 'BOULEVARD', value: 'BVD'},
-    {text: 'CIRCUIT', value: 'CCT'},
-    {text: 'CLOSE', value: 'CL'},
-    {text: 'CORNER', value: 'CRN'},
-    { text: 'COURT', value: 'CT' },
-    { text: 'CRESCENT', value: 'CRES' },
-    { text: 'CUL-DE-SAC', value: 'CDS' },
-    { text: 'DRIVE', value: 'DR' },
-    { text: 'ESPLANADE', value: 'ESP' },
-    { text: 'GREEN', value: 'GRN' },
-    { text: 'GROVE', value: 'GR' },
-    { text: 'JUNCTION', value: 'JNC' },
-    { text: 'LANE', value: 'LANE' },
-    { text: 'LINK', value: 'LINK' },
-    { text: 'MEWS', value: 'MEWS' },
-    { text: 'PLACE', value: 'PL' },
-    { text: 'RIDGE', value: 'RDGE' },
-    { text: 'ROAD', value: 'RD' },
-    { text: 'SQUARE', value: 'SQ' },
-    { text: 'STREET', value: 'ST' },
-    { text: 'TERRACE', value: 'TCE' },
-    { text: 'PARADE', value: 'PDE' },
+    {
+        value: "ACCS",
+        text: "Access"
+    },
+    {
+        value: "ALLY",
+        text: "Alley"
+    },
+    {
+        value: "ALWY",
+        text: "Alleyway"
+    },
+    {
+        value: "AMBL",
+        text: "Amble"
+    },
+    {
+        value: "ANCG",
+        text: "Anchorage"
+    },
+    {
+        value: "APP",
+        text: "Approach"
+    },
+    {
+        value: "ARC",
+        text: "Arcade"
+    },
+    {
+        value: "ART",
+        text: "Artery"
+    },
+    {
+        value: "AVE",
+        text: "Avenue"
+    },
+    {
+        value: "BASN",
+        text: "Basin"
+    },
+    {
+        value: "BCH",
+        text: "Beach"
+    },
+    {
+        value: "BDGE",
+        text: "Bridge"
+    },
+    {
+        value: "BDWY",
+        text: "Broadway"
+    },
+    {
+        value: "BEND",
+        text: "Bend"
+    },
+    {
+        value: "BLK",
+        text: "Block"
+    },
+    {
+        value: "BRAE",
+        text: "Brae"
+    },
+    {
+        value: "BRCE",
+        text: "Brace"
+    },
+    {
+        value: "BRK",
+        text: "Break"
+    },
+    {
+        value: "BROW",
+        text: "Brow"
+    },
+    {
+        value: "BVD",
+        text: "Boulevard"
+    },
+    {
+        value: "BYPA",
+        text: "Bypass"
+    },
+    {
+        value: "BYWY",
+        text: "Byway"
+    },
+    {
+        value: "CAUS",
+        text: "Causeway"
+    },
+    {
+        value: "CCT",
+        text: "Circuit"
+    },
+    {
+        value: "CDS",
+        text: "Cul-De-Sac"
+    },
+    {
+        value: "CH",
+        text: "Chase"
+    },
+    {
+        value: "CIR",
+        text: "Circle"
+    },
+    {
+        value: "CL",
+        text: "Close"
+    },
+    {
+        value: "CLDE",
+        text: "Colonnade"
+    },
+    {
+        value: "CLT",
+        text: "Circlet"
+    },
+    {
+        value: "CMMN",
+        text: "Common"
+    },
+    {
+        value: "CNR",
+        text: "Corner"
+    },
+    {
+        value: "CNWY",
+        text: "Centreway"
+    },
+    {
+        value: "CON",
+        text: "Concourse"
+    },
+    {
+        value: "COVE",
+        text: "Cove"
+    },
+    {
+        value: "COWY",
+        text: "Crossway"
+    },
+    {
+        value: "CPS",
+        text: "Copse"
+    },
+    {
+        value: "CRCS",
+        text: "Circus"
+    },
+    {
+        value: "CRD",
+        text: "Crossroad"
+    },
+    {
+        value: "CRES",
+        text: "Crescent"
+    },
+    {
+        value: "CRSG",
+        text: "Crossing"
+    },
+    {
+        value: "CRSS",
+        text: "Cross"
+    },
+    {
+        value: "CRST",
+        text: "Crest"
+    },
+    {
+        value: "CSO",
+        text: "Corso"
+    },
+    {
+        value: "CT",
+        text: "Court"
+    },
+    {
+        value: "CTR",
+        text: "Centre"
+    },
+    {
+        value: "CTTG",
+        text: "Cutting"
+    },
+    {
+        value: "CTYD",
+        text: "Courtyard"
+    },
+    {
+        value: "CUWY",
+        text: "Cruiseway"
+    },
+    {
+        value: "DALE",
+        text: "Dale"
+    },
+    {
+        value: "DELL",
+        text: "Dell"
+    },
+    {
+        value: "DEVN",
+        text: "Deviation"
+    },
+    {
+        value: "DIP",
+        text: "Dip"
+    },
+    {
+        value: "DR",
+        text: "Drive"
+    },
+    {
+        value: "DRWY",
+        text: "Driveway"
+    },
+    {
+        value: "DSTR",
+        text: "Distributor"
+    },
+    {
+        value: "EDGE",
+        text: "Edge"
+    },
+    {
+        value: "ELB",
+        text: "Elbow"
+    },
+    {
+        value: "END",
+        text: "End"
+    },
+    {
+        value: "ENT",
+        text: "Entrance"
+    },
+    {
+        value: "ESP",
+        text: "Esplanade"
+    },
+    {
+        value: "EST",
+        text: "Estate"
+    },
+    {
+        value: "EXP",
+        text: "Expressway"
+    },
+    {
+        value: "EXTN",
+        text: "Extension"
+    },
+    {
+        value: "FAWY",
+        text: "Fairway"
+    },
+    {
+        value: "FITR",
+        text: "Firetrail"
+    },
+    {
+        value: "FLAT",
+        text: "Flat"
+    },
+    {
+        value: "FOLW",
+        text: "Follow"
+    },
+    {
+        value: "FORM",
+        text: "Formation"
+    },
+    {
+        value: "FRNT",
+        text: "Front"
+    },
+    {
+        value: "FRTG",
+        text: "Frontage"
+    },
+    {
+        value: "FSHR",
+        text: "Foreshore"
+    },
+    {
+        value: "FTRK",
+        text: "Fire Track"
+    },
+    {
+        value: "FTWY",
+        text: "Footway"
+    },
+    {
+        value: "FWY",
+        text: "Freeway"
+    },
+    {
+        value: "GAP",
+        text: "Gap"
+    },
+    {
+        value: "GDN",
+        text: "Garden"
+    },
+    {
+        value: "GDNS",
+        text: "Gardens"
+    },
+    {
+        value: "GLD",
+        text: "Glade"
+    },
+    {
+        value: "GLEN",
+        text: "Glen"
+    },
+    {
+        value: "GLY",
+        text: "Gully"
+    },
+    {
+        value: "GR",
+        text: "Grove"
+    },
+    {
+        value: "GRA",
+        text: "Grange"
+    },
+    {
+        value: "GRN",
+        text: "Green"
+    },
+    {
+        value: "GRND",
+        text: "Ground"
+    },
+    {
+        value: "GTE",
+        text: "Gate"
+    },
+    {
+        value: "GTES",
+        text: "Gates"
+    },
+    {
+        value: "HILL",
+        text: "Hill"
+    },
+    {
+        value: "HRD",
+        text: "Highroad"
+    },
+    {
+        value: "HTS",
+        text: "Heights"
+    },
+    {
+        value: "HWY",
+        text: "Highway"
+    },
+    {
+        value: "INTG",
+        text: "Interchange"
+    },
+    {
+        value: "INTN",
+        text: "Intersection"
+    },
+    {
+        value: "JNC",
+        text: "Junction"
+    },
+    {
+        value: "KEY",
+        text: "Key"
+    },
+    {
+        value: "LANE",
+        text: "Lane"
+    },
+    {
+        value: "LDG",
+        text: "Landing"
+    },
+    {
+        value: "LEES",
+        text: "Lees"
+    },
+    {
+        value: "LINE",
+        text: "Line"
+    },
+    {
+        value: "LINK",
+        text: "Link"
+    },
+    {
+        value: "LKT",
+        text: "Lookout"
+    },
+    {
+        value: "LNWY",
+        text: "Laneway"
+    },
+    {
+        value: "LOOP",
+        text: "Loop"
+    },
+    {
+        value: "LT",
+        text: "Little"
+    },
+    {
+        value: "LWR",
+        text: "Lower"
+    },
+    {
+        value: "MALL",
+        text: "Mall"
+    },
+    {
+        value: "MEW",
+        text: "Mew"
+    },
+    {
+        value: "MEWS",
+        text: "Mews"
+    },
+    {
+        value: "MNDR",
+        text: "Meander"
+    },
+    {
+        value: "MT",
+        text: "Mount"
+    },
+    {
+        value: "MWY",
+        text: "Motorway"
+    },
+    {
+        value: "NOOK",
+        text: "Nook"
+    },
+    {
+        value: "OTLK",
+        text: "Outlook"
+    },
+    {
+        value: "PARK",
+        text: "Park"
+    },
+    {
+        value: "PART",
+        text: "Part"
+    },
+    {
+        value: "PASS",
+        text: "Pass"
+    },
+    {
+        value: "PATH",
+        text: "Path"
+    },
+    {
+        value: "PDE",
+        text: "Parade"
+    },
+    {
+        value: "PHWY",
+        text: "Pathway"
+    },
+    {
+        value: "PIAZ",
+        text: "Piazza"
+    },
+    {
+        value: "PKLD",
+        text: "Parklands"
+    },
+    {
+        value: "PKT",
+        text: "Pocket"
+    },
+    {
+        value: "PKWY",
+        text: "Parkway"
+    },
+    {
+        value: "PL",
+        text: "Place"
+    },
+    {
+        value: "PLAT",
+        text: "Plateau"
+    },
+    {
+        value: "PLZA",
+        text: "Plaza"
+    },
+    {
+        value: "PNT",
+        text: "Point"
+    },
+    {
+        value: "PORT",
+        text: "Port"
+    },
+    {
+        value: "PROM",
+        text: "Promenade"
+    },
+    {
+        value: "PSGE",
+        text: "Passage"
+    },
+    {
+        value: "QDGL",
+        text: "Quadrangle"
+    },
+    {
+        value: "QDRT",
+        text: "Quadrant"
+    },
+    {
+        value: "QUAD",
+        text: "Quad"
+    },
+    {
+        value: "QY",
+        text: "Quay"
+    },
+    {
+        value: "QYS",
+        text: "Quays"
+    },
+    {
+        value: "RAMP",
+        text: "Ramp"
+    },
+    {
+        value: "RCH",
+        text: "Reach"
+    },
+    {
+        value: "RD",
+        text: "Road"
+    },
+    {
+        value: "RDGE",
+        text: "Ridge"
+    },
+    {
+        value: "RDS",
+        text: "Roads"
+    },
+    {
+        value: "RDSD",
+        text: "Roadside"
+    },
+    {
+        value: "RDWY",
+        text: "Roadway"
+    },
+    {
+        value: "RES",
+        text: "Reserve"
+    },
+    {
+        value: "REST",
+        text: "Rest"
+    },
+    {
+        value: "RGWY",
+        text: "Ridgeway"
+    },
+    {
+        value: "RIDE",
+        text: "Ride"
+    },
+    {
+        value: "RING",
+        text: "Ring"
+    },
+    {
+        value: "RISE",
+        text: "Rise"
+    },
+    {
+        value: "RMBL",
+        text: "Ramble"
+    },
+    {
+        value: "RND",
+        text: "Round"
+    },
+    {
+        value: "RNDE",
+        text: "Ronde"
+    },
+    {
+        value: "RNGE",
+        text: "Range"
+    },
+    {
+        value: "ROW",
+        text: "Row"
+    },
+    {
+        value: "ROWY",
+        text: "Right of way"
+    },
+    {
+        value: "RSBL",
+        text: "Rosebowl"
+    },
+    {
+        value: "RTE",
+        text: "Route"
+    },
+    {
+        value: "RTT",
+        text: "Retreat"
+    },
+    {
+        value: "RTY",
+        text: "Rotary"
+    },
+    {
+        value: "RUE",
+        text: "Rue"
+    },
+    {
+        value: "RUN",
+        text: "Run"
+    },
+    {
+        value: "RVR",
+        text: "River"
+    },
+    {
+        value: "RVRA",
+        text: "Riviera"
+    },
+    {
+        value: "RVWY",
+        text: "Riverway"
+    },
+    {
+        value: "SBWY",
+        text: "Subway"
+    },
+    {
+        value: "SDNG",
+        text: "Siding"
+    },
+    {
+        value: "SHWY",
+        text: "State highway"
+    },
+    {
+        value: "SLPE",
+        text: "Slope"
+    },
+    {
+        value: "SND",
+        text: "Sound"
+    },
+    {
+        value: "SPUR",
+        text: "Spur"
+    },
+    {
+        value: "SQ",
+        text: "Square"
+    },
+    {
+        value: "ST",
+        text: "Street"
+    },
+    {
+        value: "STPS",
+        text: "Steps"
+    },
+    {
+        value: "STRA",
+        text: "Strand"
+    },
+    {
+        value: "STRP",
+        text: "Strip"
+    },
+    {
+        value: "STRS",
+        text: "Stairs"
+    },
+    {
+        value: "SWY",
+        text: "Service way"
+    },
+    {
+        value: "TARN",
+        text: "Tarn"
+    },
+    {
+        value: "TCE",
+        text: "Terrace"
+    },
+    {
+        value: "THOR",
+        text: "Thoroughfare"
+    },
+    {
+        value: "TKWY",
+        text: "Trunkway"
+    },
+    {
+        value: "TLWY",
+        text: "Tollway"
+    },
+    {
+        value: "TOP",
+        text: "Top"
+    },
+    {
+        value: "TOR",
+        text: "Tor"
+    },
+    {
+        value: "TRI",
+        text: "Triangle"
+    },
+    {
+        value: "TRK",
+        text: "Track"
+    },
+    {
+        value: "TRL",
+        text: "Trail"
+    },
+    {
+        value: "TRLR",
+        text: "Trailer"
+    },
+    {
+        value: "TURN",
+        text: "Turn"
+    },
+    {
+        value: "TWRS",
+        text: "Towers"
+    },
+    {
+        value: "UPAS",
+        text: "Underpass"
+    },
+    {
+        value: "UPR",
+        text: "Upper"
+    },
+    {
+        value: "VALE",
+        text: "Vale"
+    },
+    {
+        value: "VDCT",
+        text: "Viaduct"
+    },
+    {
+        value: "VIEW",
+        text: "View"
+    },
+    {
+        value: "VLLS",
+        text: "Villas"
+    },
+    {
+        value: "VSTA",
+        text: "Vista"
+    },
+    {
+        value: "WADE",
+        text: "Wade"
+    },
+    {
+        value: "WALK",
+        text: "Walk"
+    },
+    {
+        value: "WAY",
+        text: "Way"
+    },
+    {
+        value: "WHRF",
+        text: "Wharf"
+    },
+    {
+        value: "WKWY",
+        text: "Walkway"
+    },
+    {
+        value: "WYND",
+        text: "Wynd"
+    },
+    {
+        value: "YARD",
+        text: "Yard"
+    }
 ];
