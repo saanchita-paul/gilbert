1b24dcc982e33f248ab277ab02e2bafba1a04f74
adamanis98 <103982825+adamanis98@users.noreply.github.com>
2022-05-13 18:46:04 +1000

    [HOC-797] update get token url

--- config/origin.php
+++ config/origin.php
@@ -8,7 +8,7 @@ return [
         'get_product_info' => '/sap/opu/odata/sap/PRODUCT_CATALOG/Products',
         'validate_address_nmi_mirn' => '/sap/opu/odata/sap/SALES/ValidateSupplyAddressesByExtID',
         'check_fuel' => '/sap/opu/odata/sap/PRODUCT_CATALOG/GetOfferedDivisionByAddress',
-        'get_xcsrf_token' => 'sap/opu/odata/SAP/SALES/FunctionTypes',
+        'get_xcsrf_token' => '/sap/opu/odata/SAP/SALES/FunctionTypes',
         'submit_order' => '/sap/opu/odata/sap/SALES/OrderHeaders',
         'check_order' => '/sap/opu/odata/sap/SALES/OrderItemStatuses',
     ],
