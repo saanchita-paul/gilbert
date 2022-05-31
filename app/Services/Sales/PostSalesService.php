9d0e3b3f59a55313b170e5f015c25255978d9284
kaisarjamil-enkaizen <kaisar.jamil@enkaizen.com>
2022-05-31 12:19:17 +0600

    [HCO-847] is email billing

--- app/Services/Sales/PostSalesService.php
+++ app/Services/Sales/PostSalesService.php
@@ -118,9 +118,8 @@ class PostSalesService
             'postcode'=> $this->connection->postcode,
         ];
 
-
-        //TODO need to fix here HCO-847
         $billDeliveryMethod = $this->connection->is_email_billing?'EMAIL':'POST';
+
         $lifeSupport = $this->connection->has_life_support?true:false;
 
         $eaData = [
