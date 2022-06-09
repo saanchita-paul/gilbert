7654eefe8340ef4711f0523bd34fdee26e003dfa
Shakil Hossain <shakil.hossain@enkaizen.com>
2022-06-09 16:10:51 +0600

    [HCO-881] Replace failed status for water

--- resources/scripts/services/crm/LeadApplicationService.js
+++ resources/scripts/services/crm/LeadApplicationService.js
@@ -193,7 +193,8 @@ export default {
             case connectionServicesMapper.STATUS_REJECTED:
                 return { text: "Rejected", color: "red" };
             case connectionServicesMapper.STATUS_FAILED:
-                return { text: "Failed", color: "red" };
+                // return { text: "Failed", color: "red" };
+                return {text: 'Manual Processing', color: 'orange'};
             default:
                 return {
                     text: "Not Selected",
