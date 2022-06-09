2d2e2dcbe16113de142be533bd7fb6198f8517c2
kaisarjamil-enkaizen <kaisar.jamil@enkaizen.com>
2022-06-09 15:11:35 +0600

    [HCO-781] close app

--- resources/scripts/api/AppCloseReasonAPI.js
+++ resources/scripts/api/AppCloseReasonAPI.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 import AppCloseReasonMapper from "@scripts/api/mappers/AppCloseReasonMapper";
 
-// const ROOT = `${process.env.MIX_BOT_ROOT_URL}/hood-dashboard/api`;
-const ROOT = `http://127.0.0.1:8000/api`;
-// const ROOT = "https://demo.chatbot.hood.ai/hood-dashboard/api"
 
 export default {
     getAppCloseReasonData: async () => {
-        try {
-            const closeReasons = (await axios.get(`${ROOT}/app-close-reasons`)).data.data;
+        try {                    
+            const closeReasons = (await axios.get('/api/app-close-reasons')).data.data;
 
             return AppCloseReasonMapper.mapAppCloseReasonData(closeReasons);
 
