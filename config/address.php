a0e9bb63b10bc93dc51b0a37b85f55ce5f1862bf
kaisarjamil-enkaizen <kaisar.jamil@enkaizen.com>
2022-07-05 12:36:46 +0600

    [HCO-965] Manage GBG credentials from env

--- config/address.php
+++ config/address.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'gbgUserId' => env("MIX_GBG_USER_ID", 'hoodmovetech_test_user'),
-    'gbgPassword' => env("MIX_GBG_PASSWORD", 'f3se7N14GrCxHWQDgAJTu7wluFw7jDW9'),
+    'gbgUserId' => env("GBG_USER_ID", 'hoodmovetech_test_user'),
+    'gbgPassword' => env("GBG_PASSWORD", 'f3se7N14GrCxHWQDgAJTu7wluFw7jDW9'),
 ];
