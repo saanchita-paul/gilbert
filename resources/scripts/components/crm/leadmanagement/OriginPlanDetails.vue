f0b471b399094a7e56bd03b7b0c2d3f89bac8e5d
adamanis98 <adam@hood.ai>
2022-07-06 10:20:57 +1000

    [HCO-957] get origin plan everytime new NMI is loaded

--- resources/scripts/components/crm/leadmanagement/OriginPlanDetails.vue
+++ resources/scripts/components/crm/leadmanagement/OriginPlanDetails.vue
@@ -139,6 +139,9 @@ export default {
 		isBothEnergySubmit() {
             return UtilityStoreService.getIsBothEnergySelected() || this.serviceType == 'energy';
         },
+		getNMIPrefix() {
+			return this.leadSummary.nmi.substr(0, 2);
+		},
 		willShowELectricity() {
 			return this.planDetails?.plans?.electricity && (this.serviceType == "power" || this.isBothEnergySubmit);
 		},
@@ -179,7 +182,10 @@ export default {
 	watch: {
 		isBothEnergySubmit() {
 			this.getOriginData()
-		}
+		},
+		getNMIPrefix() {
+			this.getOriginData()
+		},
 	},
 	mounted() {
 		if('plans' in this.leadSummary){
@@ -196,19 +202,18 @@ export default {
 					query = {
 					state: this.state,
 					postcode: this.leadSummary.postcode,
-					nmi_prefix: this.leadSummary.nmi.substr(0, 2),
+					nmi_prefix: this.getNMIPrefix,
 				}
 			} else {
 					query = {
 					service_type: this.service_Type,
 					state: this.state,
 					postcode: this.leadSummary.postcode,
-					nmi_prefix: this.leadSummary.nmi.substr(0, 2),
+					nmi_prefix: this.getNMIPrefix,
 				}
 			}
 			
 			this.planDetails = await OriginService.getOriginData(query);
-			// console.log("Origin Plan Details Response", this.planDetails)
 		},
         closeDialog(){
             this.$emit('toggleDialog')
