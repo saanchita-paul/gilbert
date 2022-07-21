e50fc5d8a6cfd2a6d8d9121afb28b7fe5572eac6
sazzad ahmed <sazzad@enkaizen.com>
2022-07-21 12:43:58 +0600

    HCO-1019 Powershop Data get API

--- app/Services/Application/SearchConnectionApplication.php
+++ app/Services/Application/SearchConnectionApplication.php
@@ -4,6 +4,7 @@ namespace App\Services\Application;
 
 use App\Models\AgentProfile;
 use App\Models\ConnectionApplication;
+use App\Models\ConnectionService;
 use App\Models\User;
 use App\Modules\Reporting\Services\SetDateRage;
 use App\Services\FullTextSearch\FullTextQueryInterface;
@@ -56,6 +57,8 @@ class SearchConnectionApplication
     private ?string $startDate = null;
     private ?string $endDate = null;
 
+    private ?string $provider = null;
+
     /**
      * @param array $request
      */
@@ -72,6 +75,7 @@ class SearchConnectionApplication
         $this->appId = !empty($request['app_id']) ? $request['app_id'] : null;
         $this->agentId = !empty($request['agent_id']) ? $request['agent_id'] : null;
         $this->tenantEmail = !empty($request['tenant_email']) ? $request['tenant_email'] : null;
+        $this->provider = !empty($request['provider']) ? $request['provider'] : null;
 
         !empty($request['moving_date']) && $this->setDateRangeNoTz($request['moving_date'], $request['moving_date']);
 
@@ -107,6 +111,7 @@ class SearchConnectionApplication
             ->applyFilterMovingDate()
             ->applyFilterAgentId()
             ->applyFilterTenantEmail()
+            ->applyFilterByProvider()
             ->applySearch();
 
         $this->builder = $this->applySorting($this->builder);
@@ -320,6 +325,19 @@ class SearchConnectionApplication
             }),
             default => $this->builder
         };
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterByProvider(): static
+    {
+        if ($this->provider) {
+            $this->builder = $this->builder->whereHas('connectionServices', function(Builder $query) {
+                $query->where('provider_name', $this->provider);
+            });
+        }
         return $this;
     }
 }
