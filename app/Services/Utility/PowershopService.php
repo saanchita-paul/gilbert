902f81bb7eb8192e925da403f92d21a2c0b9a8cb
kaisarjamil-enkaizen <kaisar.jamil@enkaizen.com>
2022-07-25 15:15:37 +0600

    [HCO-995] data mapping in powershop service

--- app/Services/Utility/PowershopService.php
+++ app/Services/Utility/PowershopService.php
@@ -36,8 +36,8 @@ class PowershopService
     ];
 
     const MAP_PROPERTY_TYPE = [
-        1 => 'Residential',
-        2 => 'Business',
+        1 => 'residential',
+        2 => 'business',
     ];
 
     const MAP_ELIGIBLE_FOR_CONCESSION = [
@@ -59,11 +59,18 @@ class PowershopService
     {
         try {
             $this->application = ConnectionApplication::findOrFail($id);
-
+//             dd($this->getCustomerData());
             $url = config('powershop.base_url').config('powershop.send_customer_data_url');
+            // $response = Http::post($url, $this->getCustomerData());
 
-            $response = Http::put($url, $this->getCustomerData());
-
+            $response = Http::withHeaders([
+                'content-type' => 'application/json',
+                'accept' => 'application/json',
+                'Authorization' => 'Token token=bd3ebe302f742553eef05f496ad6946a'
+            ])
+                ->withBody(json_encode($this->getCustomerData()),'application-json')
+                ->post($url);
+//            dd($response->body());
             return json_decode($response->body(), true);
         } catch (Exception $exception) {
             throw $exception;
@@ -73,62 +80,91 @@ class PowershopService
     private function getCustomerData(): array
     {
         return [
-            'account_setup[account_type]' => $this->getMappedProperty($this->application->property_type),
-
-            'account_holder[title]' => $this->application->title,
-            'account_holder[first_name]' => $this->application->first_name,
-            'account_holder[last_name]' => $this->application->last_name,
-            'account_holder[date_of_birth]' => $this->application->dob,
-            'account_holder[phone_number]' => $this->application->phone,
-
-            'login[email]' => $this->application->email,
-
-            'secondary_account_holders[title]' => $this->application->authorizedPerson?->title,
-            'secondary_account_holders[first_name]' => $this->application->authorizedPerson?->first_name,
-            'secondary_account_holders[last_name]' => $this->application->authorizedPerson?->last_name,
-            'secondary_account_holders[date_of_birth]' => $this->application->authorizedPerson?->dob,
-            'secondary_account_holders[phone_number]' => $this->application->authorizedPerson?->phone,
-
-            'property_information[current_situation]' => "Moving",
-            'property_information[supply_address].house_number' => $this->application->address_unit . $this->application->street_number,
-            'property_information[supply_address].street_name' => $this->application->street_name,
-            'property_information[supply_address].street_type' => $this->application->street_type,
-            'property_information[supply_address].postcode' => $this->application->postcode,
-            'property_information[supply_address].suburb' => $this->application->city,
-            'property_information[supply_address].region' => $this->getMappedState($this->application->state),
-
-            'property_information[postal_address].house_number' => $this->application->billing_address_unit . $this->application->billing_street_number,
-            'property_information[postal_address].street_name' => $this->application->billing_street_name,
-            'property_information[postal_address].street_type' => $this->application->billing_street_type,
-            'property_information[postal_address].postcode' => $this->application->billing_postcode,
-            'property_information[postal_address].suburb' => $this->application->billing_city,
-            'property_information[postal_address].region' => $this->getMappedState($this->application->billing_state),
-
-//            'utility_details[0].utility_type' => $this->getMappedService($this->application->connectionServices?->pluck('service_type')->toArray()),
-            'utility_details[0].estimated_billing.cost' => $this->application->estimated_elec_billing_cost,
-            'utility_details[0].estimated_billing.period' => $this->application->estimated_elec_billing_period,
-//            'utility_details[1].utility_type' => $this->getMappedService($this->application->connectionServices?->pluck('service_type')->toArray()),
-            'utility_details[1].estimated_billing.cost' => $this->application->estimated_gas_billing_cost,
-            'utility_details[1].estimated_billing.period' => $this->application->estimated_gas_billing_period,
-
-            'vulnerabilities[dependency_type]' => $this->getMappedLifeSupport($this->application),
-            'vulnerabilities[medical_reason]' => $this->getMappedLifeSupportType($this->application),
-            'vulnerabilities[medical_details_disclaimer_accepted_at]' => $this->application->life_support_accepted_at,
-
-            'payment_details[card].card_type' => "mastercard",
-            'payment_details[card].masked_card_number' => "xxxxxxxxxxxxx74",
-            'payment_details[card].expiry_date' => "122022",
-            'payment_details[card].cardholder_name' => "Messi",
-            'payment_details[card].token' => "281174b44b44b397b38a",
-            'payment_details[card].terms_and_conditions_accepted_at' => "preferred",
-            'payment_details[card].preferred' => true,
-
-            'eligible_for_concessions' => $this->getMappedConcession($this->application->eligible_for_concessions),
-            'terms_and_conditions_accepted_at' => $this->application->terms_and_conditions_accepted_at,
-
-
+            "account_setup" => [
+                'account_type' => $this->getMappedProperty($this->application->property_type),
+            ],
+            "account_holder" => [
+                'title' => $this->application->title,
+                'first_name' => $this->application->first_name,
+                'last_name' => $this->application->last_name,
+                'date_of_birth' => $this->application->dob,
+                'phone_number' => $this->application->phone,
+            ],
+            "login" => [
+                'email' => $this->application->email,
+            ],
+            "secondary_account_holders" => [
+                [
+                    'title' =>  $this->application->authorizedPerson->title,
+                    'first_name' => $this->application->authorizedPerson->first_name ? $this->application->authorizedPerson->first_name : "N/A",
+                    'last_name' => $this->application->authorizedPerson->last_name,
+                    'date_of_birth' => $this->application->authorizedPerson->dob,
+                    'phone_number' => $this->application->authorizedPerson->phone,
+                ]
+            ],
+            'property_information' => [
+                'current_situation' => "existing",
+                'supply_address' => [
+                    'flat_number' => $this->application->unit_number,
+                    'house_number' => $this->application->street_number,
+                    'street_name' => $this->application->street_name_only,
+                    'street_type' => $this->application->street_type,
+                    'postcode' => $this->application->postcode,
+                    'suburb' => $this->application->city,
+                    'region' => $this->getMappedState($this->application->state),
+                ],
+                'postal_address' => [
+                    'flat_number' => $this->application->billing_address_unit,
+                    'house_number' => $this->application->billing_street_number,
+                    'street_name' => $this->application->billing_street_name_only,
+                    'street_type' => $this->application->billing_street_type,
+                    'postcode' =>  $this->application->billing_postcode,
+                    'suburb' => $this->application->billing_city,
+                    'region' => $this->getMappedState($this->application->billing_state),
+                ],
+            ],
+            "utility_details" => [
+                [
+                    "utility_type" => $this->getMappedGasUtilityType($this->application->id),
+                    "estimated_billing" => [
+                        'cost' => $this->application->estimated_elec_billing_cost,
+                        'period' => $this->application->estimated_elec_billing_period,
+                    ],
+                ],
+                [
+                    "utility_type" => $this->getMappedPowerUtilityType($this->application->id),
+                    "meter_inaccessible_reason" =>
+                        $this->application->additional_access_information . "," .
+                        $this->getMappedUnrestrainedAnimal() . "," . $this->getMappedRenovation(),
+                    "estimated_billing" => [
+                        'cost' => $this->application->estimated_gas_billing_cost,
+                        'period' => $this->application->estimated_gas_billing_period,
+                    ],
+                ]
+            ],
+            "vulnerabilities" => [
+                'dependency_type' => $this->getMappedLifeSupport($this->application->id),
+                'medical_reason' => $this->getMappedLifeSupportType($this->application->id),
+                'medical_details_disclaimer_accepted_at' => $this->application->life_support_accepted_at,
+            ],
+            "payment_details" => [
+                "card" => [
+                    'card_type' => "mastercard",
+                    'masked_card_number' => "xxxxxxxxxxxxx74",
+                    'expiry_date' => "122022",
+                    'cardholder_name' => "Messi",
+                    'token' => "281174b44b44b397b38a",
+                    'terms_and_conditions_accepted_at' => "2022-03-25T12:39:55+1100",
+                    'preferred' => true,
+                ]
+            ],
+            "eligible_for_concessions" => $this->getMappedConcession($this->application->eligible_for_concessions),
+            "terms_and_conditions_accepted_at" => $this->application->terms_and_conditions_accepted_at,
+            "promotion_code" => $this->application->promotion_code,
+            "promotion_terms_and_conditions_accepted_at" => $this->application->promotion_terms_and_conditions_accepted_at,
         ];
     }
+
 
     private function getMappedProperty($property): string
     {
@@ -140,25 +176,38 @@ class PowershopService
         return $state ? PowershopService::MAP_STATE[$state] : '';
     }
 
-//    private function getMappedService($services): array
-//    {
-//        $data = [];
-//        foreach ( $services as $service) {
-//            if (isset(PowershopService::MAP_SERVICE[$service])) {
-//                $data[] =  PowershopService::MAP_SERVICE[$service];
-//            }
-//        }
-//        return $data;
-//    }
+    private function getMappedGasUtilityType($id)
+    {
+        $type = ConnectionService::where('connection_application_id', $id)
+            ->where('service_type', 'gas')->firstOrFail();
+
+        if ($type->provider_name == "powershop")
+        {
+            return "gas";
+        }
+        return null;
+    }
+
+    private function getMappedPowerUtilityType($id)
+    {
+        $type = ConnectionService::where('connection_application_id', $id)
+            ->where('service_type', 'power')->firstOrFail();
+
+        if ($type->provider_name == "powershop")
+        {
+            return "electricity";
+        }
+        return null;
+    }
 
     private function getMappedLifeSupport($id)
     {
         $application = ConnectionApplication::whereId($id)->firstOrFail();
         if ($application->is_gas_life_support === 1 || $application->is_power_life_support === 1){
-            return true;
+            return "Life Support";
         }
         else {
-            return false;
+            return null;
         }
     }
 
@@ -169,19 +218,33 @@ class PowershopService
         {
             return "Life support power and gas";
         }
-        elseif ($application->is_gas_life_support === 1 && $application->is_power_life_support === 0)
+        elseif ($application->is_gas_life_support === 1 && ($application->is_power_life_support === 0 || $application->is_power_life_support === null))
         {
             return "Life support gas";
         }
-        elseif ($application->is_gas_life_support === 0 && $application->is_power_life_support === 1)
+        elseif (($application->is_gas_life_support === 0 || $application->is_gas_life_support === null) && $application->is_power_life_support === 1)
         {
             return "Life support power";
         }
+        else {
+            return "Default";
+        }
     }
 
-    private function getMappedConcession($concession): string
+    private function getMappedConcession($concession) : bool
     {
         return $concession ? PowershopService::MAP_ELIGIBLE_FOR_CONCESSION[$concession] : '';
+    }
+
+    private function getMappedUnrestrainedAnimal()
+    {
+        return $this->application->is_any_unrestrained_animal ? "unrestrained animal on the property" : null;
+    }
+
+    private function getMappedRenovation()
+    {
+        return $this->application->is_renovation_on ? "renovation going on" : null;
+
     }
 
 }
--- config/powershop.php
+++ config/powershop.php
@@ -2,5 +2,5 @@
 
 return [
     'base_url' => env('POWERSHOP_BASE_URL', 'https://qa.test.powershop.com.au/'),
-    'send_customer_data_url' => env('SEND_CUSTOMER_DATA_URL', 'signup/')
+    'send_customer_data_url' => env('SEND_CUSTOMER_DATA_URL', 'api/signup/')
 ];
