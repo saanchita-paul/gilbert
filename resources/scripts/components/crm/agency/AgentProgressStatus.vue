d442b338b943d68d0ab777db5ebc2cf04fdc581f
Shakil Hossain <shakil.hossain@enkaizen.com>
2022-08-03 17:15:37 +0600

    [HCO_1034] agent status progressbar

---
+++ resources/scripts/components/crm/agency/AgentProgressStatus.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <div class="progress">
+                    <div class="progress-item" v-for="item in steps">
+                        <div class="label">
+                            <span>{{ item.label }}</span>
+                        </div>
+                        <div class="title">
+                            <span>{{ item.title }}</span>
+                        </div>
+                    </div>
+                </div>
+            </v-col>
+        </v-row>
+
+    </v-container>
+</template>
+
+<script>
+
+
+export default {
+    name: "AgentProgressStatus",
+    data() {
+        return {
+            steps: [
+                {label: '1', title: 'New'},
+                {label: '2', title: 'Confirming'},
+                {label: '3', title: 'Contact'},
+                {label: '4', title: 'Test'},
+                {label: '5', title: 'Boring'},
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.progress {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    text-align: center;
+}
+
+.progress-item {
+
+}
+
+.progress .progress-item:last-child .title {
+    border-top: 0;
+}
+
+.progress .progress-item:last-child .title span::before {
+    top: -12px !important;
+    left: -1px !important;
+}
+
+.label {
+    width: 200px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+}
+
+.title {
+    display: flex;
+    border-top: 4px solid #542E89;
+    position: relative;
+}
+
+.title span {
+    font-weight: 400;
+    padding-top: 20px;
+    color: #542E89;
+}
+
+.title span:before {
+    content: '';
+    width: 25px;
+    height: 25px;
+    background-color: #FFFFFF;
+    border-radius: 25px;
+    border: 4px solid #542E89;
+    position: absolute;
+    top: -15px;
+}
+
+
+</style>
--- resources/scripts/routes/router.js
+++ resources/scripts/routes/router.js
@@ -24,6 +24,7 @@ import SalesEnergyPage from "@scripts/modules/sales/pages/SalesEnergyPage";
 import SalesWaterPage from "@scripts/modules/sales/pages/SalesWaterPage";
 import ApplicationSearchList from '@scripts/pages/ApplicationSearchList'
 import agent_routes from "@scripts/routes/agent_routes";
+import AgentProgressStatus from "@scripts/components/crm/agency/AgentProgressStatus";
 
 
 Vue.use(VueRouter);
@@ -300,6 +301,11 @@ const router = new VueRouter({
            meta: {
                isProtected: false
            }
+        },
+        {
+            path: '/agent-status',
+            component: AgentProgressStatus,
+            name:'agent.status',
         }
     ]
 })
