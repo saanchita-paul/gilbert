537984a8a54a987ecb6492adbc4dd00dde864c69
adamanis98 <adam@hood.ai>
2022-08-05 11:38:31 +1000

    [HCO-1027] update agent no password mail

--- app/Mail/AgentNotFoundMail.php
+++ app/Mail/AgentNotFoundMail.php
@@ -13,14 +13,14 @@ class AgentNotFoundMail extends Mailable
 
     
     private array $leadInfo;
-    private bool $isNotOnboarded;
+    private bool $isNoPassword;
     private bool $isNotActive;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(string $connection_application_id, string $submitType, string $agencyName, string $officeName, string $agentEmail, bool $isNotOnboarded = false, bool $isNotActive = false)
+    public function __construct(string $connection_application_id, string $submitType, string $agencyName, string $officeName, string $agentEmail, bool $isNoPassword = false, bool $isNotActive = false)
     {
         $this->leadInfo = [
             'App ID' => $connection_application_id,
@@ -30,7 +30,7 @@ class AgentNotFoundMail extends Mailable
             'Agent Email' => $agentEmail,
         ];
 
-        $this->isNotOnboarded = $isNotOnboarded;
+        $this->isNoPassword = $isNoPassword;
         $this->isNotActive = $isNotActive;
     }
 
@@ -44,13 +44,14 @@ class AgentNotFoundMail extends Mailable
         $subject = "Lead Submit Failed - Agent not found";
         $reason = "This application has an agent email that does not exist in Gilbert!";
 
-        if ($this->isNotOnboarded) {
-            $subject = "Lead Submit Failed - Agent is not fully onboarded";
-            $reason = "This application has an existing agent email in Gilbert but is not fully onboarded";
-        } else if ($this->isNotActive) {
+        if ($this->isNotActive) {
             $subject = "Lead Submit Failed - Agent inactive";
             $reason = "This application has an existing agent email in Gilbert but is inactive";            
         }
+        else if ($this->isNoPassword) {
+            $subject = "Lead Submit Failed - Agent not set Password";
+            $reason = "This application has an existing agent email in Gilbert but has not set password credential";
+        } 
 
         return $this->view('email.agent_not_found')
             ->subject($subject)
--- app/Services/NotifyBadAgentMailService.php
+++ app/Services/NotifyBadAgentMailService.php
@@ -11,18 +11,18 @@ class NotifyBadAgentMailService
     public static function check(ConnectionApplication $application, string $submitType, string $agencyName, string $officeName, string $agentEmail)
     {
         $isNotExist = !$application->created_by;
-        $isNotOnboarded = false;
+        $isNoPassword = false;
         $isNotActive = false;
 
         if (!$isNotExist) {
             $agentProf = $application->createdBy;
             $agentUser = $agentProf->user;
-            $isNotOnboarded = empty($agentUser->password);
+            $isNoPassword = empty($agentUser->password);
             $isNotActive = !$agentUser->is_active;
         }
 
-        if($isNotExist || $isNotOnboarded || $isNotActive){
-            $mail = new AgentNotFoundMail($application->id, $submitType, $agencyName, $officeName, $agentEmail, $isNotOnboarded, $isNotActive);
+        if($isNotExist || $isNoPassword || $isNotActive){
+            $mail = new AgentNotFoundMail($application->id, $submitType, $agencyName, $officeName, $agentEmail, $isNoPassword, $isNotActive);
     
             $emails = explode(',', config('support_email.agent_not_found'));
             foreach ($emails as $recipient) {
