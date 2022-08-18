d49641bf33b45d4f575c2be6eb8f4a9455e97f45
Shakil Hossain <shakil.hossain@enkaizen.com>
2022-08-18 09:29:31 +0600

    [HCO_1034] Added not applicable status

--- app/Services/Agency/AgentServiceApplicationStatusMapper.php
+++ app/Services/Agency/AgentServiceApplicationStatusMapper.php
@@ -54,8 +54,8 @@ class AgentServiceApplicationStatusMapper
             ConnectionService::STATUS_ACCEPTED => ['status_name' => 'Confirmed', 'description' => 'The connection has been processed and accepted by the provider.'],
             ConnectionService::STATUS_REJECTED,
             ConnectionService::STATUS_CANT_CONNECT => ['status_name' => 'Declined', 'description' => 'The connection has been processed but was declined by the provider.'],
-            ConnectionService::AC_MANUAL_PROCESSING,
-            ConnectionService::STATUS_EA_PROCESSINF => ['status_name' => 'Awaiting Confirmation', 'description' => 'The connection is processed and we are awaiting confirmation from the provider.'],
+            ConnectionService::AC_MANUAL_PROCESSING => ['status_name' => 'Awaiting Confirmation', 'description' => 'The connection is processed and we are awaiting confirmation from the provider.'],
+            ConnectionService::STATUS_EA_PROCESSINF => ['status_name' => 'Not Applicable', 'description' => 'The connection is not required or is yet to be selected by the customer.'],
             default => self::DEFAULT_STATUS
         };
 
@@ -74,8 +74,8 @@ class AgentServiceApplicationStatusMapper
             ConnectionService::STATUS_SUBMITTED => ['status_name' => 'Confirmed', 'description' => 'The connection has been processed and accepted by the provider.'],
             ConnectionService::STATUS_REJECTED => ['status_name' => 'Declined', 'description' => 'The connection has been processed but was declined by the provider.'],
             ConnectionService::STATUS_FAILED => ['status_name' => 'Manual Processing', 'description' => 'There was an issue processing the water connection automatically, but our team is fixing it.'],
-            ConnectionService::STATUS_EA_PROCESSINF,
             ConnectionService::AC_MANUAL_PROCESSING => ['status_name' => 'Awaiting Confirmation', 'description' => 'The connection is processed and we are awaiting confirmation from the provider.'],
+            ConnectionService::STATUS_EA_PROCESSINF => ['status_name' => 'Not Applicable', 'description' => 'The connection is not required or is yet to be selected by the customer.'],
             default => self::DEFAULT_STATUS
         };
 
