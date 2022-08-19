37b8fb8e59d67f951f995ca2f9ed8228bd1002ce
Lenin Sheikh <justtlenin@gmail.com>
2022-08-19 11:01:21 +0600

    [HCO-1040] added index

--- database/migrations/2022_08_12_095607_update_tsa_call_histories_table.php
+++ database/migrations/2022_08_12_095607_update_tsa_call_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropJsonColumnFromTsaCallHistoriesTable extends Migration
+class UpdateTsaCallHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,9 @@ class DropJsonColumnFromTsaCallHistoriesTable extends Migration
      */
     public function up()
     {
+        Schema::table('tsa_call_histories', function (Blueprint $table) {
+            $table->index('attempt_id', 'tsa_call_histories_attempt_id_index');
+        });
         if (Schema::hasColumn('tsa_call_histories', 'all_fields_dump')) {
             Schema::table('tsa_call_histories', function (Blueprint $table) {
                 $table->dropColumn('all_fields_dump');
@@ -27,6 +30,11 @@ class DropJsonColumnFromTsaCallHistoriesTable extends Migration
      */
     public function down()
     {
-        //no need to reverse
+        Schema::table('tsa_call_histories', function (Blueprint $table) {
+            $table->dropIndex('tsa_call_histories_attempt_id_index');
+        });
+
+        //no need to reverse json column
+
     }
 }
