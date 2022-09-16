b2ac7e13e7edd8154d4cac3f22aa1a6cca8ba9df
Lenin Sheikh <justtlenin@gmail.com>
2022-09-16 14:49:59 +0600

    [HCO-1100] Updated Agency Report

--- app/Services/Agency/Report/ExportReaAgenciesReport.php
+++ app/Services/Agency/Report/ExportReaAgenciesReport.php
@@ -13,6 +13,15 @@ class ExportReaAgenciesReport
 {
     private Collection $mappedCSVData;
 
+    const STATES  = [
+        'VIC',
+        'NSW',
+        'QLD',
+        'ATC',
+        'SA',
+        'WA'
+    ];
+
     public function __construct()
     {
         $this->fetchData();
@@ -31,17 +40,22 @@ class ExportReaAgenciesReport
     {
         try {
             $tz = '+' . TimeZoneService::getTimeZoneInt() . ':00';
+            $regex = $this->getStatesRegex();
+//            dd($regex);
+
             $query = DB::table('offices as o')
-            ->join('agencies as a', 'o.agency_id', '=', 'a.id')
-            ->select(
-                DB::raw(" 
+                ->join('agencies as a', 'o.agency_id', '=', 'a.id')
+                ->select(
+                    DB::raw("
                     o.name as 'Office Name',
-                    IF(a.type=0, 'Independent', a.name) as Agency,
+                    UPPER(REGEXP_SUBSTR(o.address, '$regex')) as State,
                     DATE_FORMAT(CONVERT_TZ((select created_at from connection_applications where office_id = o.id order by created_at asc limit 1), '+00:00', '$tz'), '%d/%m/%Y') as 'First Application Date',
                     DATE_FORMAT(CONVERT_TZ((select created_at from connection_applications where office_id = o.id order by created_at desc limit 1), '+00:00', '$tz'), '%d/%m/%Y') as 'Last Application Date',
-                    IF((select count(*) from connection_applications where office_id = o.id > 0), IFNULL((select DATEDIFF(CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', '$tz'), CONVERT_TZ(created_at, '+00:00', '+10:00')) from connection_applications where office_id = o.id order by created_at desc limit 1), 0), null) as 'Days Since Last Application'
+                    IF((select count(*) from connection_applications where office_id = o.id > 0), IFNULL((select DATEDIFF(CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', '$tz'), CONVERT_TZ(created_at, '+00:00', '$tz')) from connection_applications where office_id = o.id order by created_at desc limit 1), 0), null) as 'Days Since Last Application',
+                    (select count(*) from connection_applications where office_id = o.id) as `Total Applications`,
+                    IFNULL(o.rent_roll, 0) as PUM
                 ")
-            )->get();
+                )->get();
 
             $this->mappedCSVData = $query;
             return $this;
@@ -61,7 +75,23 @@ class ExportReaAgenciesReport
             'errorMessage' => $e->getMessage(),
             'errorTrace' => $e->getTraceAsString(),
         ]);
-        
+
         throw $e;
+    }
+
+    /**
+     * Building regex for states
+     *
+     * @return string
+     */
+    private function getStatesRegex(): string
+    {
+        $r = '';
+        foreach (self::STATES as $state) {
+            $r .= !empty($r) ? '|' : '';
+
+            $r .= '\\\\b' . $state . '\\\\b';
+        }
+        return $r;
     }
 }
