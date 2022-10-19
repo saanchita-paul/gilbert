404a00bf16c766fa81e3a79d0eb5498b2504a081
Lenin Sheikh <justtlenin@gmail.com>
2022-10-19 15:26:14 +0600

    [HCO-930] fixed service type fixing

--- app/Services/GilbertToCB/ChatbotToGilbertSyncService.php
+++ app/Services/GilbertToCB/ChatbotToGilbertSyncService.php
@@ -423,15 +423,12 @@ class ChatbotToGilbertSyncService
         $app = ConnectionApplication::where('chatbot_id', $this->chatbotId)->firstOrFail();
         foreach ($serviceData as $service) {
             if($service['service_type']) {
-//                dd($service['service_type']);
-//                $mappedServiceData['connection_application_id'] = $this->id;
-//                $mappedServiceData['status'] = $service['status'];
+                $serviceType = strtolower($service['service_type']) === 'electricity' ? 'power' : $service['service_type'];
+
                 ConnectionService::query()->where('connection_application_id', $app->id)->with('reasons')
-                    ->updateOrCreate(['service_type'   => $service['service_type']], [
-                        'connection_application_id'   => $app->id,
-                        'service_type'   => strtolower($service['service_type']) === 'electricity'
-                            ? 'power'
-                            : $service['service_type'],
+                    ->updateOrCreate(['service_type' => $serviceType], [
+                        'connection_application_id' => $app->id,
+                        'service_type'   => $serviceType,
                         'plan_type'   => $service['plan_type'],
                         'provider_name'   => $service['provider_name'],
                         'status'   => $this->mapServiceStatus($service['status']),
