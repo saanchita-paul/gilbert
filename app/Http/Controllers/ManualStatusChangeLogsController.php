584beaec8cfeb0088f084b4a66b747cd068a1294
mi.shajib <mazharul.islam@enkaizen.com>
2022-10-28 12:35:29 +0600

    [HCO_1159] Added manual status change logs table and added endpoint for get logs.

--- app/Http/Controllers/ManualStatusChangeLogsController.php
+++ app/Http/Controllers/ManualStatusChangeLogsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ManualStatusChangeLogResource;
 use App\Models\ManualStatusChangeLog;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class ManualStatusChangeLogsController extends Controller
 {
@@ -29,39 +28,5 @@ class ManualStatusChangeLogsController extends Controller
     {
         $logs = ManualStatusChangeLog::where('connection_application_id', $id)->get();
         return ManualStatusChangeLogResource::collection($logs)->response();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
