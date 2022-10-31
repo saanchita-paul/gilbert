9a25d2553f57ad609378521e55f6049a4de64d0a
Shakil Hossain <shakil.hossain@enkaizen.com>
2022-10-31 10:37:48 +0600

    [HCO-1156] Work on UI

--- resources/scripts/components/crm/leadmanagement/ApplicationNotes.vue
+++ resources/scripts/components/crm/leadmanagement/ApplicationNotes.vue
@@ -1,67 +1,75 @@
 <template>
     <v-row>
         <v-col cols="12">
-            <p class="sub-title">Notes</p>
-<!--            <ValidationObserver ref="submit_note">
-            <ValidationProvider name="Expired Date" rules="required"  v-slot="{ errors }">-->
+            <v-card color="basil">
+                <v-tabs
+                    v-model="tab"
+                    background-color="transparent"
+                    color="basil"
+                    grow
+                >
+                    <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
+                </v-tabs>
 
-            <v-textarea v-model ="note.text"
-                outlined
-                hide-details="auto"
-                placeholder="Notes goes here."
-            ></v-textarea>
-<!--            </ValidationProvider>
-            </ValidationObserver>-->
+                <v-tabs-items v-model="tab">
+                    <v-tab-item v-for="item in items" :key="item">
 
-            <v-btn class="ma-2 float-right" @click="saveNote">Submit Note</v-btn>
-        </v-col>
-
-        <v-card color="basil">
-            <v-tabs
-            v-model="tab"
-            background-color="transparent"
-            color="basil"
-            grow
-            >
-                <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
-            </v-tabs>
-
-            <v-tabs-items v-model="tab">
-                <v-tab-item v-for="item in items" :key="item">
-
-                    <v-col v-if="item == 'Internal Notes'" cols="12" class="notes-container">
-                        <v-timeline dense>
-                                <v-timeline-item color="primary" small v-for="nt in notes" :color="getColor(nt.active)" :key="nt.id">
-                                    <SubmittedNote v-if="nt.type == 'submitted_connection'" :note="nt"> </SubmittedNote>
-                                    <SubmittedOriginNote v-if="nt.type == 'submitted_origin'" :note="nt"> </SubmittedOriginNote>
-                                    <SubmittedPowershopNote v-if="nt.type == 'submitted_powershop'" :note="nt"> </SubmittedPowershopNote>
-                                    <InvalidNote v-else-if="['invalid_property_me_note', 'Close Connection'].includes(nt.type)" :note="nt"> </InvalidNote>
+                        <v-col v-if="item == 'Internal Notes'" cols="12" class="notes-container">
+                            <v-timeline dense>
+                                <v-timeline-item color="primary" small v-for="nt in notes" :color="getColor(nt.active)"
+                                                 :key="nt.id">
+                                    <SubmittedNote v-if="nt.type == 'submitted_connection'" :note="nt"></SubmittedNote>
+                                    <SubmittedOriginNote v-if="nt.type == 'submitted_origin'"
+                                                         :note="nt"></SubmittedOriginNote>
+                                    <SubmittedPowershopNote v-if="nt.type == 'submitted_powershop'"
+                                                            :note="nt"></SubmittedPowershopNote>
+                                    <InvalidNote
+                                        v-else-if="['invalid_property_me_note', 'Close Connection'].includes(nt.type)"
+                                        :note="nt"></InvalidNote>
                                     <Note v-else :note="nt"></Note>
                                 </v-timeline-item>
-                        </v-timeline>
-                    </v-col>
-
-                    <div v-if="item == 'Call History'">
-                        <v-col cols="12" class="notes-container" v-if="leadSummary.tsa_call_histories.length">
-                            <v-timeline dense>
-                                <v-timeline-item color="primary" small v-for="(nt, index) in leadSummary.tsa_call_histories" :key="nt.attempt_id">
-                                    <div style="font-weight: bold;"> Attempt {{index+1}} </div>
-                                    <div> {{nt.attempt_initiated_timestamp}} </div>
-                                    <div> {{nt.attempt_outcome}} </div>
-                                </v-timeline-item>
-                        </v-timeline>
-                    </v-col>
-                    </div>
-
-                    <div v-if="item == 'Status Log'">
-                        <v-col cols="12" class="notes-container">
-                            <StatusLog :application-id="leadSummary.id"></StatusLog>
+                            </v-timeline>
                         </v-col>
-                    </div>
 
-                </v-tab-item>
-            </v-tabs-items>
-        </v-card>
+                        <div v-if="item == 'Call History'">
+                            <v-col cols="12" class="notes-container" v-if="leadSummary.tsa_call_histories.length">
+                                <v-timeline dense>
+                                    <v-timeline-item color="primary" small
+                                                     v-for="(nt, index) in leadSummary.tsa_call_histories"
+                                                     :key="nt.attempt_id">
+                                        <div style="font-weight: bold;"> Attempt {{ index + 1 }}</div>
+                                        <div> {{ nt.attempt_initiated_timestamp }}</div>
+                                        <div> {{ nt.attempt_outcome }}</div>
+                                    </v-timeline-item>
+                                </v-timeline>
+                            </v-col>
+                        </div>
+
+                        <div v-if="item == 'Status Log'">
+                            <v-col cols="12" class="notes-container">
+                                <StatusLog :application-id="leadSummary.id"></StatusLog>
+                            </v-col>
+                        </div>
+
+                    </v-tab-item>
+                </v-tabs-items>
+            </v-card>
+        </v-col>
+        <v-col cols="12" v-if="isInternalNotes">
+            <p class="sub-title">Notes</p>
+            <ValidationObserver ref="submit_note">
+                <ValidationProvider name="Note" rules="required" v-slot="{ errors }">
+
+                    <v-textarea v-model="note.text"
+                                outlined
+                                hide-details="auto"
+                                placeholder="Notes goes here."
+                    ></v-textarea>
+                </ValidationProvider>
+            </ValidationObserver>
+
+            <v-btn class="mt-2 float-right white--text note-button" @click="saveNote" color="#542E89">Submit Note</v-btn>
+        </v-col>
     </v-row>
 </template>
 
@@ -98,6 +106,11 @@ export default {
         //   text: 'Lorem ipsm'
       }
     },
+    computed: {
+        isInternalNotes() {
+            return this.tab === 0;
+        }
+    },
     methods: {
         getColor(isActive) {
 
@@ -133,5 +146,9 @@ export default {
 }
 .v-tab {
     text-transform: capitalize !important;
+}
+.note-button{
+    font-weight: 700;
+    font-size: 14px;
 }
 </style>
