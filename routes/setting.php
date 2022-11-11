4c66e514c1cdc7db9b1349669a435407cd3bd4fb
enkaizen.shajib <mazharul.islam@enkaizen.com>
2022-11-11 12:00:07 +0600

    [HS-93] Auto assign application functionality done except check valid address, nmi & mirn.

--- routes/setting.php
+++ routes/setting.php
@@ -4,8 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('is-chatbot-office', 'SettingsController@getIsChatbotOffice');
 Route::post('is-chatbot-office', 'SettingsController@setIsChatbotOffice');
-
-Route::get('test', function () {
-    $user = \App\Models\User::with(['profile', 'profile.office', 'profile.agency'])->where('email', 'test4@demo.com')->first();
-    dd($user);
-});
