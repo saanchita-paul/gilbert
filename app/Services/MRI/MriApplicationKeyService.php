d9d8e368874a51774fcdc814276b06f9bf226e31
adamanis98 <adam@hood.ai>
2022-11-23 16:21:02 +1100

    [HS-24] update mri application key service
    (cherry picked from commit a950c184cb256e359e86aeb755985a852c303882)

--- app/Services/MRI/MriApplicationKeyService.php
+++ app/Services/MRI/MriApplicationKeyService.php
@@ -6,18 +6,24 @@ use Illuminate\Support\Facades\Http;
 
 class MriApplicationKeyService
 {
+
+    /**
+     * @var string
+     */
+
     /**
      * Get MRI office key pairs
      */
     public function getData()
     {
         $url = $this->getURL();
+        $subKey = empty(config('mri.subscription_key')) ? '574800735b3b4effa9d8ef84d57d345f' : config('mri.subscription_key');
 
         try {
             $response = Http::withHeaders([
                 "Accept" => "application/json",
                 "Content-Type" => "application/json",
-                "Ocp-Apim-Subscription-Key" => config('mri.subscription_key')
+                "Ocp-Apim-Subscription-Key" => $subKey
             ])->get($url);
 
             return json_decode($response->body(), true);
@@ -33,6 +39,20 @@ class MriApplicationKeyService
      */
     private function getURL(): string
     {
-        return config('mri.base_url') . config('mri.endpoints.get_mri_office_key_pairs') . config('mri.app_key');
+        $url = empty(config('mri.base_url')) ? 'https://uatapi.propertytree.io' : config('mri.base_url');
+        $endpoint = empty(config('mri.endpoints.get_mri_office_key_pairs')) ? '/apikey/v1/application_keys/' : config('mri.endpoints.get_mri_office_key_pairs');
+        $appKey = empty(config('mri.app_key')) ? '4e1df42e-5c53-4762-b07a-79f8d731e0bc' : config('mri.app_key');
+        
+        return $url . $endpoint . $appKey;
+
     }
+
+    // private function getDefaultConfig()
+    // {
+    //     if (config('mri.subscription_key')) {
+    //         return config('mri.subscription_key');
+    //     }
+        
+    //     return config('app.env') !== 'production' ? '574800735b3b4effa9d8ef84d57d345f' : throw new \Exception('MRI_SUBSCRIPTION_KEY is null');
+    // }
 }
