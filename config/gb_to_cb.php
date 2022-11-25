c93b405aaa61d677418c7e991f6e2247c59e3748
enkaizen.shajib <mazharul.islam@enkaizen.com>
2022-11-25 15:14:19 +0600

    [HS-94] url changed for sync bot url.

--- config/gb_to_cb.php
+++ config/gb_to_cb.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'root_url' => env('BOT_ROOT_URL', 'http://127.0.0.1:8081'),
+    'root_url' => env('BOT_ROOT_URL', 'http://localhost:8000'),
     'endpoints' => [
         'gb_to_cb_sync' => '/api/gb-to-cb-sync/',
     ],
