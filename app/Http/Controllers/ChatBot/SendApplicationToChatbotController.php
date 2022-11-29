4eed8a2b384cefe5d545358fe5d7a6587d66728c
enkaizen.shajib <mazharul.islam@enkaizen.com>
2022-11-29 13:04:32 +0600

    [HS-94] removed old sync service file.

--- app/Http/Controllers/ChatBot/SendApplicationToChatbotController.php
+++ app/Http/Controllers/ChatBot/SendApplicationToChatbotController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers\ChatBot;
 
 use App\Http\Controllers\Controller;
-use App\Models\ConnectionApplication;
-use App\Services\ChatBot\SendAppGilbertToChatbotService;
 use App\Services\GilbertToCB\GilbertToChatbotSyncService;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class SendApplicationToChatbotController extends Controller
 {
