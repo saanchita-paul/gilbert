e6b649fde6cf8745378ff3f6bf90905fdd27f19e
Shihab jamil <shihab.jamil@enkaizen.com>
2022-12-15 13:04:26 +0600

    [HS-256] Able to Generate NBN CAF file now

---
+++ app/Modules/NBN/Services/SearchNbnConnectionApplication.php
@@ -0,0 +1,400 @@
+<?php
+
+namespace App\Modules\NBN\Services;
+
+use App\Models\AgentProfile;
+use App\Models\ConnectionApplication;
+use App\Models\ConnectionService;
+use App\Models\User;
+use App\Modules\Reporting\Services\SetDateRage;
+use App\Services\Application\ApplicationStatusFilterMapper;
+use App\Services\FullTextSearch\FullTextQueryInterface;
+use App\Services\FullTextSearch\FullTextSearchInterface;
+use App\Traits\Agency\Sortable;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use function optional;
+use function resolve;
+
+/**
+ *
+ */
+class SearchNbnConnectionApplication
+{
+
+    use Sortable;
+    use SetDateRage;
+
+    /**
+     * @var mixed|null
+     */
+    private ?int $perPage;
+    /**
+     * status
+     * @var string|mixed|null
+     */
+    private ?string $leadType;
+    /**
+     * @var mixed|int
+     */
+    private mixed $source;
+    /**
+     * @var FullTextQueryInterface[]
+     */
+    private array $searchQueries = [];
+
+    /**
+     * @var Builder
+     */
+    private Builder $builder;
+    private ?int $tenancyType;
+    private ?int $triage;
+    private $officeId;
+
+    private $appId;
+    private $movingDate;
+    private $agentId;
+    private $tenantEmail;
+    private ?string $startDate = null;
+    private ?string $endDate = null;
+    private bool $isDuplicate;
+
+    private ?string $dateStart = null;
+    private ?string $dateEnd = null;
+
+    /**
+     * @var string|null
+     */
+    private $duplication_group_id;
+
+    private ?string $provider = null;
+
+    private ?string $application_service_type = null;
+
+    /**
+     * @param array $request
+     */
+    public function __construct(array $request)
+    {
+        $this->createFullTextQueries($request);
+
+        $this->perPage = empty($request['per_page']) ? null : (int)$request['per_page'];
+        $this->leadType = optional($request)['active_lead_type'];
+        $this->source = !empty($request['source']) ? (ConnectionApplication::SOURCE_MAPPING[$request['source']] ?? null) : null;
+        $this->tenancyType = !empty($request['tenancy_type']) ? ConnectionApplication::TENANCY_MAPPING[$request['tenancy_type']] ?? null : null;
+        $this->triage = !empty($request['triage']) ? ConnectionApplication::TRIAGE_MAPPING[$request['triage']] ?? null : null;
+        $this->officeId = !empty($request['office_id']) ? $request['office_id'] : null;
+        $this->appId = !empty($request['app_id']) ? $request['app_id'] : null;
+        $this->agentId = !empty($request['agent_id']) ? $request['agent_id'] : null;
+        $this->tenantEmail = !empty($request['tenant_email']) ? $request['tenant_email'] : null;
+        $this->provider = !empty($request['provider_name']) ? $request['provider_name'] : null;
+        $this->isDuplicate = !empty($request['is_duplicate']) ? (bool)$request['is_duplicate'] : false;
+        $this->duplication_group_id = !empty($request['duplication_group_id']) ? $request['duplication_group_id'] : null;
+
+        !empty($request['moving_date']) && $this->setDateRangeNoTz($request['moving_date'], $request['moving_date']);
+
+        if (empty($request['sort_by'])) {
+            $this->setSortBy('created_at', 'true');
+        } else {
+            $this->setSortBy(optional($request)['sort_by'], optional($request)['is_descending']);
+        }
+
+        $this->dateStart = !empty($request['start_date']) ? $request['start_date'] : null;
+        $this->dateEnd = !empty($request['end_date']) ? $request['end_date'] : null;
+        $this->application_service_type = !empty($request['application_service_type']) ? $request['application_service_type'] : null;
+    }
+
+    /**
+     * @param User $user
+     * @return LengthAwarePaginator
+     */
+    public function get(User $user): LengthAwarePaginator
+    {
+        $this->builder = ConnectionApplication::query()
+            ->with('connectionServices.reasons')
+            ->with('SugerLead')
+            ->with('assignedTo')
+            ->with('submittedByUser')
+            ->with('powershopPaymentInfo')
+            ->with('office')
+            ->with('authorizedPerson')
+            ->with('createdBy')
+            ->with('identification')
+            ->with('submittedByUser');
+
+
+        $this->applyFilterLeadType($user)
+            ->applyFilterUserOffice($user)
+            // ->applyFilterCreatedBy($user)
+            ->applyFilterSource()
+            ->applyFilterOfficeId()
+            ->applyFilterForFoxie()
+            ->applyFilterTenancyType()
+            ->applyFilterTriage()
+            ->applyFilterAppId()
+            ->applyFilterMovingDate()
+            ->applyFilterAgentId()
+            ->applyFilterTenantEmail()
+            ->applyDuplicateFilter()
+            ->applyDateRangeFilter()
+            ->applyFilterByService()
+            ->applySearch();
+
+        $this->builder = $this->applySorting($this->builder);
+
+        return $this->builder->paginate($this->perPage);
+    }
+
+    public function getApplicationForAgency(User $user): LengthAwarePaginator
+    {
+        $this->builder = ConnectionApplication::query()
+            ->with('connectionServices.reasons')
+            ->with('SugerLead')
+            ->with('assignedTo')
+            ->with('submittedByUser');
+
+        $this->applyFilterLeadType($user)
+            ->applyFilterUserOffice($user)
+            ->applyFilterSource()
+            // ->applyFilterCreatedBy($user)
+            ->applyFilterOfficeId()
+            ->applyFilterTenancyType()
+            ->applySearch();
+
+        $this->builder = $this->applySorting($this->builder);
+
+        return $this->builder->paginate($this->perPage);
+    }
+
+    /**
+     * view business docs here: "/docs/business/applications_card_filters.md"
+     * @return $this
+     */
+    private function applyFilterLeadType(User $user): static
+    {
+        if (!$this->leadType) {
+            return $this;
+        }
+
+        $statuses = ApplicationStatusFilterMapper::getStatuses($this->leadType);
+
+
+        if (sizeof($statuses) > 0) {
+            $this->builder = $this->builder->whereIn('status', $statuses);
+        }
+
+        $this->builder = match ($this->leadType) {
+            ConnectionApplication::MY_APPLICATIONS => $this->builder->where('assigned_to', $user->profile->id),
+            #note: if any  status needs some special conditions, then add more cases here.
+            default => $this->builder
+        };
+
+        return $this;
+    }
+
+    /**
+     * @return static
+     */
+    private function applyFilterSource(): static
+    {
+        if (isset($this->source) && gettype($this->source) == 'integer') {
+            $this->builder = $this->builder->where('source', $this->source);
+        }
+        return $this;
+
+    }
+
+    private function applyFilterAppId(): static
+    {
+        if ($this->appId) {
+            $this->builder = $this->builder->where('id', $this->appId);
+        }
+        return $this;
+
+    }
+
+    private function applyFilterMovingDate(): static
+    {
+        if ($this->startDate && $this->endDate) {
+            $this->builder = $this->builder
+                ->where('moving_date', '>=', $this->startDate)
+                ->where('moving_date', '<=', $this->endDate);
+        }
+        return $this;
+    }
+
+    private function applyFilterTenantEmail(): static
+    {
+        if ($this->tenantEmail) {
+            $this->builder = $this->builder
+                ->where('email', 'like', "%$this->tenantEmail%");
+        }
+        return $this;
+    }
+
+    private function applyFilterAgentId(): static
+    {
+        if ($this->agentId) {
+            $this->builder = $this->builder->where('created_by', $this->agentId);
+        }
+        return $this;
+
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterOfficeId(): static
+    {
+        if ($this->officeId) {
+            $this->builder = $this->builder->where('office_id', $this->officeId);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterTenancyType(): static
+    {
+        if ($this->tenancyType) {
+            $this->builder = $this->builder->where('tenancy_type', $this->tenancyType);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterTriage(): static
+    {
+        if ($this->triage) {
+            $this->builder = $this->builder->where('is_triage', $this->triage);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterUserOffice(User $user): static
+    {
+        if ($user->profile_type === AgentProfile::class) {
+            $this->builder = $this->builder
+                ->where('office_id', $user->profile->office_id)
+                ->where('agency_id', $user->profile->agency_id);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterCreatedBy(User $user): static
+    {
+        if ($user->profile_type === AgentProfile::class) {
+            $this->builder = $this->builder
+                ->where('created_by', $user->profile->id);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function applySearch(): static
+    {
+        /** @var FullTextSearchInterface $fts */
+        $fts = resolve(FullTextSearchInterface::class);
+        $this->builder = $fts->applyAndSearches($this->builder, queries: $this->searchQueries);
+
+        return $this;
+    }
+
+    /**
+     * building search query
+     *
+     * @param array $filters
+     *
+     * @return void
+     */
+    private function createFullTextQueries(array $filters): void
+    {
+        /** @var FullTextQueryInterface $query */
+        $query = resolve(FullTextQueryInterface::class);
+
+        if (!empty($filters['tenant_name'])) {
+            $this->searchQueries[] = $query->createNew(text: $filters['tenant_name'], index: 'first_name, middle_name, last_name');
+        }
+        if (!empty($filters['phone'])) {
+            $this->searchQueries[] = $query->createNew(text: $filters['phone'], index: 'phone,homephone');
+        }
+        if (!empty($filters['address'])) {
+            $index = 'unit_number,street_number,street_name_only,city,postcode,state,country,street_address,address_text';
+            $this->searchQueries[] = $query->createNew(text: $filters['address'], index: $index);
+        }
+    }
+
+    /**
+     * @return $this
+     */
+    private function applyFilterForFoxie(): static
+    {
+        $this->builder = match ($this->source) {
+            ConnectionApplication::SOURCE_FOXIE => $this->builder->whereHas('SugerLead', function (Builder $query) {
+                $query->whereNull('compare_connect_id')
+                    ->orWhere('compare_connect_id', 'N/A');
+            }),
+            null => $this->builder->where(function (Builder $builder) {
+                $builder->doesntHave('SugerLead')
+                    ->orWhereHas("SugerLead", fn(Builder $id) => $id->whereNull('compare_connect_id')->orWhere('compare_connect_id', 'N/A'));
+            }),
+            default => $this->builder
+        };
+        return $this;
+    }
+
+    private function applyDuplicateFilter(): static
+    {
+        if ($this->isDuplicate) {
+            $this->builder = $this->builder
+                ->where('is_duplicate', true);
+        }
+
+        if (!empty($this->duplication_group_id)) {
+            $this->builder = $this->builder
+                ->where('duplication_group_id', $this->duplication_group_id);
+        }
+        return $this;
+    }
+
+    private function applyDateRangeFilter(): static
+    {
+        if ($this->dateStart && $this->dateEnd) {
+            $this->dateStart = Carbon::parse($this->dateStart)->toDateTimeString();
+            $this->dateEnd = Carbon::parse($this->dateEnd)
+                ->addHours(23)
+                ->addMinutes(59)
+                ->addSeconds(59)
+                ->toDateTimeString();
+
+            $this->builder = $this->builder
+                ->where('created_at', '>=', $this->dateStart)
+                ->where('created_at', '<=', $this->dateEnd);
+        }
+        return $this;
+    }
+
+    private function applyFilterByService(): static
+    {
+        if ($this->application_service_type) {
+            $this->builder = $this->builder->whereHas('connectionServices', function (Builder $query) {
+                $query->where('service_type', 'internet');
+            });
+
+        }
+        return $this;
+
+    }
+}
---
+++ resources/scripts/pages/GilbertFilterWrapper.vue
@@ -0,0 +1,120 @@
+<template>
+    <v-row>
+        <v-col cols="12">
+            <h3>Filters</h3>
+            <GilbertApplicationCafFileFilter
+                :selectedCafFile="selectedCafFile"
+                v-model="advanceSearchModelFilter"
+                :gilbertApplications="applications"
+                :isSearchEmpty="advanceSearchModelFilter.isSearchEmpty()"
+                @updatePageOnFilterChange="updatePageOnFilterChange"
+                @updateDates="updateDates">
+            </GilbertApplicationCafFileFilter>
+        </v-col>
+        <v-col cols="12">
+            <GilbertApplicationCafFileTable
+                v-model="selectedCafFile"
+                :gilbertApplications="applications"
+                :totalItems="totalItems"
+                :pages="pages"
+                @reloadDataTable="reloadDataTable"
+                @selectRowCafFiles="selectRowCafFiles"
+            >
+            </GilbertApplicationCafFileTable>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+
+ import GilbertApplicationCafFileFilter from "@scripts/pages/GilbertApplicationCafFileFilter.vue";
+ import GilbertApplicationCafFileTable from "@scripts/pages/GilbertApplicationCafFileTable.vue";
+ import {CafFileSearchFilterModel} from "@scripts/models/CafFileSearchFilterModel";
+ import {isEqual, omit} from "lodash-es";
+ import ApplicationCafFileService from "@scripts/services/crm/ApplicationCafFileService";
+
+ export default {
+     name: "GilbertFilterWrapper",
+     props : ["filterFor"],
+     components: {GilbertApplicationCafFileTable, GilbertApplicationCafFileFilter},
+     data(){
+         return {
+             advanceSearchModelFilter :  new CafFileSearchFilterModel(),
+             applications : [],
+             pages : 1,
+             totalItems : null,
+             dateRange: null,
+             sorts_search_meta : null,
+             itemsPerPages : 10,
+             selectedCafFile : []
+
+         }
+     },
+     watch:{
+         advanceSearchModelFilter: {
+             handler(value) {
+                 let params = {...this.$route.query, ...value}
+                 if (isEqual(this.$route.query, value)) return;
+                 this.$router.push({
+                     name: "caf.files",
+                     query: params,
+                 });
+                 this.resetPage();
+                 this.fetchApplications();
+             },
+             deep: true
+         },
+     },
+     methods:{
+         updatePageOnFilterChange() {
+             this.pages = 1;
+         },
+         updateDates(dateRange) {
+             if (dateRange) {
+                 this.advanceSearchModelFilter.start_date = dateRange.start
+                 this.advanceSearchModelFilter.end_date = dateRange.end
+             }
+
+         },
+         reloadDataTable(meta) {
+             this.pages = meta.page;
+             this.sorts_search_meta = omit({...meta}, 'page');
+             this.fetchApplications();
+         },
+         resetPage() {
+             this.page = 1;
+         },
+         async fetchApplications() {
+             let data = ''
+             if(this.$route.query.tab === 'nbnGilbertApplication'){
+                 data = await ApplicationCafFileService.getGilbertNBNApplicationData({...this.sorts_search_meta, ...{page: this.pages}}, this.advanceSearchModelFilter);
+             }else{
+                 data = await ApplicationCafFileService.getGilbertApplicationData({...this.sorts_search_meta, ...{page: this.pages}}, this.advanceSearchModelFilter);
+             }
+
+             this.applications = data.data;
+             this.pages = data.pagination.current_page;
+             this.itemsPerPages = data.pagination.per_page;
+             this.totalItems = data.pagination.total;
+
+
+         },
+         selectRowCafFiles(item) {
+             let index = this.selectedCafFile.findIndex(dt => dt.id === item.id);
+             if(index === -1) {
+                 this.selectedCafFile.push(item);
+             } else {
+                 this.selectedCafFile.splice(index, 1);
+             }
+         },
+     },
+     mounted(){
+         this.advanceSearchModelFilter.application_service_type = this.filterFor
+     },
+
+ }
+</script>
+
+<style scoped>
+
+</style>
