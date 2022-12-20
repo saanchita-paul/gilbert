7c079463954e95078bd1200cfc81b7b32246c255
enkaizen.shajib <mazharul.islam@enkaizen.com>
2022-12-20 13:16:26 +0600

    [HS-412] Auto-assignment should work for applications synced from prop-tech systems. (COMPLETED)

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,27 +9,20 @@ use App\Listeners\ConnectionApplicationClosedOrEscalatedListener;
 use App\Listeners\AutoAssignG2CBListener;
 use App\Listeners\NotifyAgentAfterLeadCreationListener;
 use App\Listeners\Agency\EnergySubmitListener;
-use App\Listeners\SumoSubmitListener;
 use App\Models\ConnectionApplication;
-use App\Models\ConnectionService;
 use App\Models\Identification;
 use App\Observers\ConnectionApplicationObserver;
-use App\Observers\ConnectionServiceObserver;
 use App\Observers\IdentificationObserver;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use App\Listeners\WaterServiceListener;
 use App\Listeners\HTTP\LogRequestSending;
 use App\Listeners\HTTP\LogRequestReceiving;
 use App\Events\Agency\CreateApplicationEvent;
 use App\Events\Agency\SubmitApplicationEvent;
-use App\Listeners\Agency\SendApplicationToEA;
 use App\Listeners\Agency\CreateHubSpotContact;
 use App\Listeners\Agency\UpdateHubSpotContact;
-use App\Listeners\Agency\SendNotificationToSupportListener;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use App\Listeners\OriginSubmitListener;
 
 class EventServiceProvider extends ServiceProvider
 {
