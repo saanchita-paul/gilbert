98f1a42c012ab578e872368870d26a640eefb81d
Shihab Jamil <shihab.jamil@enkaizen.com>
2022-12-28 14:20:17 +0600

    application notes creation

--- resources/scripts/api/mappers/ChatbotApplicationMapper.js
+++ resources/scripts/api/mappers/ChatbotApplicationMapper.js
@@ -12,13 +12,13 @@ export default {
         const id_detail = new IdDetail(application);
         const personal_detail = new PersonalDetail(application);
         const property_detail = new PropertyDetail(application);
-        const application_notes = new ApplicationNote(application.application_notes)
+        // const application_notes = new ApplicationNote(application.application_notes)
 
         return new ChatbotApplication({
             id_detail: id_detail,
             personal_details: personal_detail,
             property_details: property_detail,
-            application_notes : application_notes
+            // application_notes : application_notes
         });
 
     }
--- resources/scripts/models/chatbot/ChatbotApplication.js
+++ resources/scripts/models/chatbot/ChatbotApplication.js
@@ -2,14 +2,12 @@ export default class {
     constructor({
                     id_detail = null,
                     personal_details = null,
-                    property_details = null,
-                    application_notes = null
+                    property_details = null
                 }) {
 
         this.id_detail = id_detail;
         this.personal_details = personal_details;
         this.property_details = property_details;
-        this.application_notes = application_notes;
     }
 
     setIdDetails(idDetail) {
