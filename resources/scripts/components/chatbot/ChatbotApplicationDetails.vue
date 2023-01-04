f41b729010a166f0a4b565ae5c59f521a6b7e905
Sanchita Paul <sanchita.paul@enkaizen.com>
2023-01-04 14:34:25 +0600

    [HS_391_caf_file] added chatbot constants

--- resources/scripts/components/chatbot/ChatbotApplicationDetails.vue
+++ resources/scripts/components/chatbot/ChatbotApplicationDetails.vue
@@ -761,6 +761,7 @@ import {isNull} from "lodash-es";
 import CustomerService from "@scripts/services/CustomerService";
 import RejectionReasonModal from "@scripts/components/crm/modals/RejectionReasonModal";
 import IdCopyToClipboard from "@scripts/components/common/IdCopyToClipboard";
+import CHATBOT_APP_DATA from "@scripts/data/constants/CHATBOT_APP_DATA";
 export default {
     name: "ChatbotApplicationDetails",
     components: {
@@ -775,230 +776,27 @@ export default {
             chatbot_app_backup : null,
             app_id: null,
             isProfileEditMode: false,
-            specialNumberDD: [1,2,3,4,5,6,7,8],
+            specialNumberDD: CHATBOT_APP_DATA.SPECIAL_NUMBER_DD,
             showExpireDate: false,
             expire_date:  '',
             titlesDropDown: titlesMapperForDropdownCb,
-            accessRequirement: [
-                {
-                    text: "Yes",
-                    value: 1,
-                },
-                {
-                    text: "No",
-                    value: 0,
-                },
-            ],
-            solarPowerDD: [
-                {
-                    text: "Yes",
-                    value: 'solar',
-                },
-                {
-                    text: "No",
-                    value: "no_solar",
-                },
-            ],
-            idenficationTypeDD: [
-                {
-                    text: "Passport",
-                    value: 'identity_passport',
-                },
-                {
-                    text: "Driver's License",
-                    value: 'identity_driving_license',
-                },
-                {
-                    text: "Medicare Card",
-                    value: 'identity_medicare',
-                },
-            ],
-            colorDD: [
-                {
-                    text: "Green",
-                    value: "green",
-                },
-                {
-                    text: "Blue",
-                    value: "blue",
-                },
-                {
-                    text: "Yellow",
-                    value: "yellow",
-                },
-                ],
-            statesDD: [
-                { text: "NSW", value: "New South Wales" },
-                { text: "VIC", value: "Victoria" },
-                { text: "QLD", value: "Queensland" },
-                { text: "SA", value: "South Australia" },
-                { text: "NT", value: "Northern Territory" },
-                { text: "TAS", value: "Tasmania" },
-                { text: "ACT", value: "Australian Capital Territory" },
-                { text: "WA", value: "Western Australia" }, // TODO state definition can be updated
-            ],
-            tenantTypeDD: [
-                {
-                    text: "Renter",
-                    value: "1",
-                },
-                {
-                    text: "Owner",
-                    value: "0",
-                },
-                {
-                    text: "Rea Partner",
-                    value: "2",
-                },
-            ],
-            homeRenovationDD: [
-                {
-                    text: "No",
-                    value: 0,
-                },
-                {
-                    text: "Yes",
-                    value: 1,
-                },
-            ],
-            electricityDD: [
-                {
-                    text: "No",
-                    value: 0,
-                },
-                {
-                    text: "Yes",
-                    value: 1,
-                },
-            ],
-            formData : {
-                power_status: null,
-                gas_status: null,
-            },
-            powerStatus : [
-                {
-                    id: 13,
-                    type: "service",
-                    display_text: "Accepted",
-                    display_text_alias: "Connected",
-                    status_value: 5,
-                    text: "Accepted",
-                    value: 5
-                },
-                {
-                    id: 15,
-                    type: "service",
-                    display_text: "Not Submitted",
-                    display_text_alias: "In progress",
-                    status_value: 7,
-                    text: "Not Submitted",
-                    value: 7
-                },
-                {
-                    id: 17,
-                    type: "service",
-                    display_text: "Rejected",
-                    display_text_alias: "Rejected",
-                    status_value: 9,
-                    text: "Rejected",
-                    value: 9
-                },
-                {
-                    id: 19,
-                    type: "service",
-                    display_text: "Manual Processing",
-                    display_text_alias: "Manual Processing",
-                    status_value: 11,
-                    text: "Manual Processing",
-                    value: 11
-                },
-                {
-                    id: 20,
-                    type: "service",
-                    display_text: "In Progress",
-                    display_text_alias: "In Progress",
-                    status_value: 12,
-                    text: "In Progress",
-                    value: 12
-                }
-            ],
-            gasStatus : [
-                {
-                    id: 13,
-                    type: "service",
-                    display_text: "Accepted",
-                    display_text_alias: "Connected",
-                    status_value: 5,
-                    text: "Accepted",
-                    value: 5
-                },
-                {
-                    id: 15,
-                    type: "service",
-                    display_text: "Not Submitted",
-                    display_text_alias: "In progress",
-                    status_value: 7,
-                    text: "Not Submitted",
-                    value: 7
-                },
-                {
-                    id: 17,
-                    type: "service",
-                    display_text: "Rejected",
-                    display_text_alias: "Rejected",
-                    status_value: 9,
-                    text: "Rejected",
-                    value: 9
-                },
-                {
-                    id: 19,
-                    type: "service",
-                    display_text: "Manual Processing",
-                    display_text_alias: "Manual Processing",
-                    status_value: 11,
-                    text: "Manual Processing",
-                    value: 11
-                },
-                {
-                    id: 20,
-                    type: "service",
-                    display_text: "In Progress",
-                    display_text_alias: "In Progress",
-                    status_value: 12,
-                    text: "In Progress",
-                    value: 12
-                }
-            ],
+            accessRequirement: CHATBOT_APP_DATA.ACCESS_REQUIREMENT,
+            solarPowerDD: CHATBOT_APP_DATA.SOLAR_POWER_DD,
+            idenficationTypeDD: CHATBOT_APP_DATA.IDENTIFICATION_TYPE_DD,
+            colorDD: CHATBOT_APP_DATA.COLOR_DD,
+            statesDD: CHATBOT_APP_DATA.STATES_DD,
+            tenantTypeDD: CHATBOT_APP_DATA.TENANT_TYPE_DD,
+            homeRenovationDD: CHATBOT_APP_DATA.HOME_RENOVATION_DD,
+            electricityDD: CHATBOT_APP_DATA.ELECTRICITY_DD,
+            formData : CHATBOT_APP_DATA.FORM_DATA,
+            powerStatus : CHATBOT_APP_DATA.POWER_STATUS,
+            gasStatus : CHATBOT_APP_DATA.GAS_STATUS,
             showDateOfBirth : false,
             dob : null,
             showMovingDate : false,
             moved_at : null,
-            expansionPanel : {
-                profile: [0],
-                property: [],
-                identification: [],
-                service : [],
-                application : [],
-                concession: []
-            },
-            concessionCard: [
-                {
-                    text: "DVA Health",
-                    value: "1",
-                },
-                {
-                    text: "Health Care Card",
-                    value: "2",
-                },
-                {
-                    text: "Pensioner Concession",
-                    value: "3",
-                },
-                {
-                    text: "Queensland Seniors",
-                    value: "4",
-                },
-            ],
+            expansionPanel : CHATBOT_APP_DATA.EXPANSION_PANEL,
+            concessionCard: CHATBOT_APP_DATA.CONCESSION_CARD,
             isConcessionStartDate: false,
             isConcessionEndDate: false,
             concession_start_date: null,
---
+++ resources/scripts/data/constants/CHATBOT_APP_DATA.js
@@ -0,0 +1,219 @@
+export default {
+    SPECIAL_NUMBER_DD: [1,2,3,4,5,6,7,8],
+    ACCESS_REQUIREMENT: [
+        {
+            text: "Yes",
+            value: 1,
+        },
+        {
+            text: "No",
+            value: 0,
+        },
+    ],
+    SOLAR_POWER_DD: [
+        {
+            text: "Yes",
+            value: 'solar',
+        },
+        {
+            text: "No",
+            value: "no_solar",
+        },
+    ],
+    IDENTIFICATION_TYPE_DD: [
+        {
+            text: "Passport",
+            value: 'identity_passport',
+        },
+        {
+            text: "Driver's License",
+            value: 'identity_driving_license',
+        },
+        {
+            text: "Medicare Card",
+            value: 'identity_medicare',
+        },
+    ],
+    COLOR_DD: [
+        {
+            text: "Green",
+            value: "green",
+        },
+        {
+            text: "Blue",
+            value: "blue",
+        },
+        {
+            text: "Yellow",
+            value: "yellow",
+        },
+    ],
+    STATES_DD: [
+        { text: "NSW", value: "New South Wales" },
+        { text: "VIC", value: "Victoria" },
+        { text: "QLD", value: "Queensland" },
+        { text: "SA", value: "South Australia" },
+        { text: "NT", value: "Northern Territory" },
+        { text: "TAS", value: "Tasmania" },
+        { text: "ACT", value: "Australian Capital Territory" },
+        { text: "WA", value: "Western Australia" }, // TODO state definition can be updated
+    ],
+    TENANT_TYPE_DD: [
+        {
+            text: "Renter",
+            value: "1",
+        },
+        {
+            text: "Owner",
+            value: "0",
+        },
+        {
+            text: "Rea Partner",
+            value: "2",
+        },
+    ],
+    HOME_RENOVATION_DD: [
+        {
+            text: "No",
+            value: 0,
+        },
+        {
+            text: "Yes",
+            value: 1,
+        },
+    ],
+    ELECTRICITY_DD: [
+        {
+            text: "No",
+            value: 0,
+        },
+        {
+            text: "Yes",
+            value: 1,
+        },
+    ],
+    FORM_DATA : {
+        power_status: null,
+        gas_status: null,
+    },
+    POWER_STATUS : [
+        {
+            id: 13,
+            type: "service",
+            display_text: "Accepted",
+            display_text_alias: "Connected",
+            status_value: 5,
+            text: "Accepted",
+            value: 5
+        },
+        {
+            id: 15,
+            type: "service",
+            display_text: "Not Submitted",
+            display_text_alias: "In progress",
+            status_value: 7,
+            text: "Not Submitted",
+            value: 7
+        },
+        {
+            id: 17,
+            type: "service",
+            display_text: "Rejected",
+            display_text_alias: "Rejected",
+            status_value: 9,
+            text: "Rejected",
+            value: 9
+        },
+        {
+            id: 19,
+            type: "service",
+            display_text: "Manual Processing",
+            display_text_alias: "Manual Processing",
+            status_value: 11,
+            text: "Manual Processing",
+            value: 11
+        },
+        {
+            id: 20,
+            type: "service",
+            display_text: "In Progress",
+            display_text_alias: "In Progress",
+            status_value: 12,
+            text: "In Progress",
+            value: 12
+        }
+    ],
+    GAS_STATUS : [
+        {
+            id: 13,
+            type: "service",
+            display_text: "Accepted",
+            display_text_alias: "Connected",
+            status_value: 5,
+            text: "Accepted",
+            value: 5
+        },
+        {
+            id: 15,
+            type: "service",
+            display_text: "Not Submitted",
+            display_text_alias: "In progress",
+            status_value: 7,
+            text: "Not Submitted",
+            value: 7
+        },
+        {
+            id: 17,
+            type: "service",
+            display_text: "Rejected",
+            display_text_alias: "Rejected",
+            status_value: 9,
+            text: "Rejected",
+            value: 9
+        },
+        {
+            id: 19,
+            type: "service",
+            display_text: "Manual Processing",
+            display_text_alias: "Manual Processing",
+            status_value: 11,
+            text: "Manual Processing",
+            value: 11
+        },
+        {
+            id: 20,
+            type: "service",
+            display_text: "In Progress",
+            display_text_alias: "In Progress",
+            status_value: 12,
+            text: "In Progress",
+            value: 12
+        }
+    ],
+    EXPANSION_PANEL : {
+        profile: [0],
+        property: [],
+        identification: [],
+        service : [],
+        application : [],
+        concession: []
+    },
+    CONCESSION_CARD: [
+        {
+            text: "DVA Health",
+            value: "1",
+        },
+        {
+            text: "Health Care Card",
+            value: "2",
+        },
+        {
+            text: "Pensioner Concession",
+            value: "3",
+        },
+        {
+            text: "Queensland Seniors",
+            value: "4",
+        },
+    ],
+}
