a2a24727065dcc76511e5282ed6389d2d19acdfb
Sanchita Paul <sanchita.paul@enkaizen.com>
2023-01-06 14:06:55 +0600

    [HS_476 & HS_469] removed concession from personal

--- resources/scripts/models/chatbot/PersonalDetail.js
+++ resources/scripts/models/chatbot/PersonalDetail.js
@@ -11,10 +11,6 @@ export default class PersonalDetail{
                     dob= null,
                     phone_type = null,
                     homephone =null,
-                    concession_card_type =  null,
-                    concession_card_number =  null,
-                    concession_card_start_date =  null,
-                    concession_end_date =  null,
                     business_name =  null,
                     abn =  null,
 
@@ -29,10 +25,6 @@ export default class PersonalDetail{
         this.dob = dayJs(dob).format("DD/MM/YYYY");
         this.phone_type = phone_type;
         this.homephone = homephone;
-        this.concession_card_type = concession_card_type;
-        this.concession_card_number = concession_card_number;
-        this.concession_end_date = concession_end_date;
-        this.concession_card_start_date = concession_card_start_date;
         this.business_name = business_name ?? "";
         this.abn = abn ?? "";
 
