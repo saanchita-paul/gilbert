af7ae32824d8b3b4dbe87db37b51ec349a2b99e1
Shihab Jamil <shihab.jamil@enkaizen.com>
2023-01-09 10:16:12 +0600

    skeleton loader added and event added for table update after save or cancel something from expansion panel

---
+++ resources/scripts/data/SkeletonLoaderData.js
@@ -0,0 +1,6 @@
+export default {
+    class: 'mb-6',
+    boilerplate: true,
+    elevation: 2,
+    height : '100%'
+}
--- resources/scripts/pages/chatbot/ChatbotApplicationPage.vue
+++ resources/scripts/pages/chatbot/ChatbotApplicationPage.vue
@@ -30,9 +30,15 @@
                     </v-card-text>
                 </v-card>
             </v-col>
-            <v-col cols="4" v-if="chatbotApps.length" >
-                <ChatbotApplicationDetails  ></ChatbotApplicationDetails>
+            <v-col cols="4"  >
+                <v-skeleton-loader
+                    v-bind="attrs"
+                    type="article, actions , article,  actions, article, actions"
+                    v-if="isLoadSkeleton"
+                ></v-skeleton-loader>
+                <ChatbotApplicationDetails v-if="shouldShowApplicationDetails" @applicationDetailsUpdated="fetchCafFiles" ></ChatbotApplicationDetails>
             </v-col>
+
         </v-row>
     </v-container>
 </template>
@@ -49,6 +55,7 @@ import ChatbotApplicationDetails from "@scripts/components/chatbot/ChatbotApplic
 import ChatbotApplicationFilter from "@scripts/components/chatbot/ChatbotApplicationFilter";
 import ChatbotCafTable from "@scripts/components/chatbot/ChatbotCafTable";
 import Store from '@scripts/store/index';
+import SkeletonLoaderData from "@scripts/data/SkeletonLoaderData";
 
 export default {
 name: "ChatbotApplicationPage",
@@ -84,7 +91,9 @@ name: "ChatbotApplicationPage",
             totalItems: null,
             option: {},
             advanceSearchModel: new CafFileSearchFilterModel(),
-            selectedApp: null
+            selectedApp: null,
+            isLoadSkeleton : false,
+            attrs: SkeletonLoaderData,
         }
     },
 
@@ -167,14 +176,16 @@ name: "ChatbotApplicationPage",
         },
 
         async fetchCafFiles() {
+            this.isLoadSkeleton = true;
             let data = await ApplicationCafFileService.getChatbotApplication({...this.sort_search_meta, ...{page: this.page}}, this.advanceSearch);
             this.page = data.pagination.current_page;
             this.itemsPerPage = data.pagination.per_page;
             this.totalItem = data.pagination.total;
             if(data.data.length > 0) {
                 const query = this.$route.query;
-                await this.$router.replace({query: {...query, app_id: data.data[0].id}});
+                await this.$router.replace({query: {...query, app_id: data.data[0].id}}).catch((error)=>{});
             }
+            this.isLoadSkeleton = false;
         },
 
         async fetchGilbertApplications() {
@@ -223,6 +234,10 @@ name: "ChatbotApplicationPage",
     computed: {
         chatbotApps() {
            return Store.getters.applications;
+        },
+        shouldShowApplicationDetails(){
+            return (this.isLoadSkeleton === false) && (this.chatbotApps.length > 0);
+
         }
     }
 
