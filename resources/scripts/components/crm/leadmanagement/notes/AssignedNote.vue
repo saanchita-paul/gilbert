7ebce622b3ed79ce1b18ddbe608299eab792645b
enkaizen.shajib <mazharul.islam@enkaizen.com>
2023-01-19 15:19:48 +0600

    [HS-576] Add note when application assigned to "Chatbot user". (COMPLETED)

---
+++ resources/scripts/components/crm/leadmanagement/notes/AssignedNote.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card class="elevation-2 timeline-card" color="gray" :class="{active:false}">
+        <p class="title">{{ note.title }}</p>
+        <p>{{ note.text }}</p>
+        <p>by [{{ note.user_role }}]</p>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "AssignedNote",
+    props: {
+        note: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
