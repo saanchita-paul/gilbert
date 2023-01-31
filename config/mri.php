a321fae6a7fa555ba7ea2409e566f4ce9ee29960
adamanis98 <adam@hood.ai>
2023-01-31 16:06:18 +1100

    change mri support email default to adam

--- config/mri.php
+++ config/mri.php
@@ -5,7 +5,7 @@ return [
     'app_id' => env('MRI_APP_ID', 'f89d9246-4e4a-437f-a6ba-1940282b097d'),
     'app_key' => env('MRI_APP_KEY', '4e1df42e-5c53-4762-b07a-79f8d731e0bc'),
     'subscription_key' => env('MRI_SUBSCRIPTION_KEY', '574800735b3b4effa9d8ef84d57d345f'),
-    'support_emails' => env('MRI_SUPPORT_EMAILS', 'mdshakilhossain091@gmail.com'),
+    'support_emails' => env('MRI_SUPPORT_EMAILS', 'adam@hood.ai'),
     'endpoints' => [
         'get_mri_office_key_pairs' => '/apikey/v1/application_keys/',
         'get_all_agents' => '/residentialproperty/v1/Agents',
