17e6e9d288449540e63eb9cd98f207bf8e7314d5
mi.shajib <mishajib222@gmail.com>
2023-02-10 09:41:50 +0600

    [HS-707] Migrate old data service added.

--- app/Services/HazardService.php
+++ app/Services/HazardService.php
@@ -12,7 +12,7 @@ class HazardService
     /**
      * @return Collection|Hazard[]
      */
-    public static function getHazards()
+    public static function getHazards(): Collection|array
     {
         return Hazard::where('is_active', true)->get([
             'id',
@@ -21,13 +21,70 @@ class HazardService
         ]);
     }
 
-    public static function isDogExists($app)
+    /**
+     * Check if application has already dog hazard
+     *
+     * @param $app
+     * @return bool
+     */
+    public static function isDogExists($app): bool
     {
-        return $app->hazards()->where('powershop_value', 'dog')->exists();
+        return (bool)$app->hazards()->where('powershop_value', 'dog')->exists();
     }
 
-    public static function isRenovationExists($app)
+    /**
+     * Check if application has already renovation hazard
+     *
+     * @param $app
+     * @return bool
+     */
+    public static function isRenovationExists($app): bool
     {
-        return $app->hazards()->where('powershop_value', 'electrical_safety_issue')->exists();
+        return (bool)$app->hazards()->where('powershop_value', 'electrical_safety_issue')->exists();
+    }
+
+    /**
+     * Migrate old hazard data to new hazard table
+     *
+     * @return void
+     */
+    public static function migrateOldData(): void
+    {
+        ini_set('max_execution_time', 0);
+        $apps = ConnectionApplication::query()->select(['id', 'is_any_unrestrained_animal', 'is_renovation_on'])
+            ->where('is_any_unrestrained_animal', 1)
+            ->orWhere('is_renovation_on', 1)->cursor();
+
+        foreach ($apps as $app) {
+            // Set dog hazard
+            if ($app->is_any_unrestrained_animal && !self::isDogExists($app)) {
+                $app->hazards()->attach(self::getDogHazardId());
+            }
+
+            // Set renovation hazard
+            if ($app->is_renovation_on && !self::isRenovationExists($app)) {
+                $app->hazards()->attach(self::getRenovationHazardId());
+            }
+        }
+    }
+
+    /**
+     * Get renovation hazard id
+     *
+     * @return int
+     */
+    public static function getRenovationHazardId(): int
+    {
+        return (int)Hazard::where('powershop_value', 'electrical_safety_issue')->first()->id;
+    }
+
+    /**
+     * Get dog hazard id
+     *
+     * @return int
+     */
+    public static function getDogHazardId(): int
+    {
+        return (int)Hazard::where('powershop_value', 'dog')->first()->id;
     }
 }
