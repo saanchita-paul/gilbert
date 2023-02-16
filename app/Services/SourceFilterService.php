ca75f3f22cff2f9fbef07a607cb77701033819b4
shakil-enkaizen <shakil.hossain@enkaizen.com>
2023-02-16 16:11:48 +0600

    [HS-755] Call source from cache function call

--- app/Services/SourceFilterService.php
+++ app/Services/SourceFilterService.php
@@ -8,9 +8,6 @@ class SourceFilterService
 {
     public function getSource()
     {
-        return ExternalSource::query()
-            ->where('is_active', true)
-            ->orderBy('order')
-            ->get();
+        return ExternalSource::fromCache();
     }
 }
