7ae3b5ee868a1e0f8d55ee177de1d6977b262a4c
Shihab Jamil <shihab.jamil@enkaizen.com>
2023-02-24 12:31:03 +0600

    typo fixed HS-836

--- resources/scripts/api/mappers/crm/GilbertApplicationCafFileMapper.js
+++ resources/scripts/api/mappers/crm/GilbertApplicationCafFileMapper.js
@@ -62,7 +62,7 @@ const mapStatus = (status, is_generated_caf=false) => {
 }
 
 const mapSupplier = services => {
-    let service = services.find(svc => ((svc.service_type === 'power' || svc.service_type === 'gas') &&  !!sv.provider_name ));
+    let service = services.find(svc => ((svc.service_type === 'power' || svc.service_type === 'gas') &&  !!svc.provider_name ));
     return service?.provider_name;
 
 }
