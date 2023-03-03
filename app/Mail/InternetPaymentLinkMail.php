169719bc7908817b999549a9792438d0f85e91fd
mi.shajib <mishajib222@gmail.com>
2023-03-03 11:34:34 +0600

    [HS-111] email template added for internet payment link mail

---
+++ app/Mail/InternetPaymentLinkMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InternetPaymentLinkMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $name = 'John Doe';
+        $paymentUrl = '#';
+        return $this->view('email.internet-payment-link-mail', [
+            'name' => $name,
+            'paymentUrl' => $paymentUrl,
+        ]);
+    }
+}
