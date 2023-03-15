626a2d823f6ef0a46609490bd5e216f0aaf4dc87
sazzad ahmed <sazzad@enkaizen.com>
2023-03-15 10:52:14 +0600

    HS-933 [GILBERT] - CAF Gilbert Origin: Incorrect Date of Sale

--- app/Services/ApplicationCafService/OriginExporter.php
+++ app/Services/ApplicationCafService/OriginExporter.php
@@ -4,41 +4,21 @@ namespace App\Services\ApplicationCafService;
 
 use App\Models\ConnectionApplication;
 use App\Models\ConnectionService;
-use App\Models\OriginPlan;
 use App\Services\Utility\StateMapService;
-use Auth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Origin\Services\StoreProductInfoAPI;
-use Rap2hpoutre\FastExcel\Facades\FastExcel;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class OriginExporter
 {
-    /**
-     * @var array
-     */
-    private array $applicationIdList;
-    /**
-     * @var
-     */
+
+    /* @var $applicationList */
     private $applicationList;
+
     /**
      * @var array
      */
     private array $mappedApplicationList = [];
-    /**
-     * @var array
-     */
-    private array $gasPromotionData;
-    /**
-     * @var array
-     */
-    private array $elePromotionData;
-    /**
-     * @var
-     */
-    private $cafToken;
+
     /**
      * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|mixed
      */
@@ -46,14 +26,7 @@ class OriginExporter
 
     private $campaignInfo ;
 
-    /**
-     *
-     */
     public const HAS_UNRESTING_ANIMAL = 1;
-    /**
-     *
-     */
-    public const NO_UNRESTING_ANIMAL = 0;
 
     /**
      * @param array $applicationIdList
@@ -61,49 +34,24 @@ class OriginExporter
     public function __construct($applicationList)
     {
         $this->chatbotUri = config('bot.root_url');
-        $this->getPromotionCode();
         $this->applicationList = $applicationList;
-
+        $this->loadCampaignData();
         $this->mapApplications();
 
     }
 
-
-
     /**
-     * @param mixed $get
+     * map application for generating caf file
      */
-
     public function mapApplications()
     {
-
-
-        $base = config('bot.root_url');
-        $endpoint = '/hood-dashboard/api/origin-campaign-codes';
-
-
-        $url = $base . $endpoint;
-        $headers = [
-            "Accept" => "application/json",
-        ];
-        $response = Http::withOptions([
-            "headers" => $headers,
-            "verify" => false,
-        ])->get($url);
-        $response->throw();
-
-
-        $this->campaignInfo = json_decode($response->body(), true);
-
-
         $selectedId = [];
         foreach ($this->applicationList as $app) {
             try{
-                $cafToken = $this->getPowerShopCafToken($app);
                 $this->mappedApplicationList[] = [
                     'Broker Name' => 'HOOD',
                     'Origin Receive Date' => '',
-                    'Date of Sale' => date('d/m/Y', strtotime($app->moving_date??'')),
+                    'Date of Sale' => $this->setDateOfSales($app),
                     'Reference ID' => $app->sales_reference_id,
                     'Business Name' => '',
                     'ABN Identification Type' => '',
@@ -206,32 +154,6 @@ class OriginExporter
      * @param $app
      * @return string|null
      */
-    private function getTenancyType($app)
-    {
-        return match($app->tenancy_type) {
-            1 => 'Renter',
-            2 => 'Home Owner',
-            default => null
-        };
-    }
-
-    /**
-     * @param $app
-     * @return string|null
-     */
-    private function checkElectricity($app)
-    {
-        return match($app->has_electricity) {
-            1 => 'Yes',
-            2 => 'No',
-            default => null
-        };
-    }
-
-    /**
-     * @param $app
-     * @return string|null
-     */
     private function getConcessionCardType($app){
 
         return match($app->concession_card_type) {
@@ -241,71 +163,6 @@ class OriginExporter
             'QSC'=> 'Queensland Seniors',
             default => null
         };
-
-    }
-
-    /**
-     * @param $app
-     * @return string
-     */
-    private function getLifeSensitive($app): string
-    {
-        if ($app->is_gas_life_support == 1 && $app->is_power_life_support == 1){
-            return 'Life support elec and gas';
-        }
-        elseif ($app->is_power_life_support === 1){
-            return 'Life support elec';
-        }
-        else if($app->is_gas_life_support === 1){
-            return 'Life support gas';
-        }
-        return '';
-    }
-
-    /**
-     * @param $app
-     * @return string
-     */
-    private function getSupplyAddress($app): string
-    {
-        if($app->unit_number == ''){
-            return $app->street_number .' , '. $app->street_name_only .' , '. $app->street_type;
-        }
-        elseif ($app->street_number == ''){
-            return $app->unit_number. ' , ' . $app->street_name_only .' , '. $app->street_type;
-        }
-        elseif ($app->street_name_only == ''){
-            return $app->unit_number. ' , ' .$app->street_number .' , '. $app->street_type;
-        }
-        elseif ($app->street_type == ''){
-            return $app->unit_number. ' , ' .$app->street_number .' , '. $app->street_name_only ;
-        }
-        else{
-            return $app->unit_number. ' , ' .$app->street_number .' , '. $app->street_name_only .' , '. $app->street_type;
-        }
-    }
-
-    /**
-     * @param $app
-     * @return string
-     */
-    private function getMailingAddress($app): string
-    {
-        if($app->billing_unit_number == ''){
-            return $app->billing_street_number .' , '. $app->billing_street_name_only .' , '. $app->billing_street_type;
-        }
-        elseif ($app->billing_street_number == ''){
-            return $app->billing_unit_number. ' , ' . $app->billing_street_name_only .' , '. $app->billing_street_type;
-        }
-        elseif ($app->billing_street_name_only == ''){
-            return $app->billing_unit_number. ' , ' .$app->billing_street_number .' , '. $app->billing_street_type;
-        }
-        elseif ($app->billing_street_type == ''){
-            return $app->billing_unit_number. ' , ' .$app->billing_street_number .' , '. $app->billing_street_name_only ;
-        }
-        else{
-            return $app->billing_unit_number. ' , ' .$app->billing_street_number .' , '. $app->billing_street_name_only .' , '. $app->billing_street_type;
-        }
 
     }
 
@@ -348,15 +205,6 @@ class OriginExporter
             3 => 'medicare',
             default => null
         };
-    }
-
-    /**
-     * @param $app
-     * @return mixed
-     */
-    private function getSecondTitle($app): mixed
-    {
-        return $app->authorizedPerson?->title;
     }
 
     /**
@@ -434,148 +282,6 @@ class OriginExporter
     }
 
     /**
-     * @param $is_any_unrestrained_animal
-     * @param $is_renovation_on
-     * @return string
-     */
-    private function getHazard($is_any_unrestrained_animal, $is_renovation_on): string
-    {
-        $hasrestineAnymal = false;
-        $hazard = '';
-        if( !is_null($is_any_unrestrained_animal) && !empty(trim($is_any_unrestrained_animal)) ) {
-            $hazard =  'Animal on property';
-            $hasrestineAnymal = true;
-        }
-        if(!is_null($is_renovation_on) && !empty(trim($is_renovation_on))) {
-            if($hasrestineAnymal) {
-                return $hazard .', '. 'renovation going on';
-            }
-        }
-        return $hazard;
-
-    }
-
-    /**
-     * @param $is_access_require
-     * @param $additional_access_information
-     * @return string
-     */
-    private function getAccessReq($is_access_require, $additional_access_information): string
-    {
-        if($is_access_require) {
-            return  'Yes, '. $additional_access_information;
-        } else {
-            return 'No';
-        }
-
-    }
-
-
-    /**
-     * call chatbot api for finding promotion code
-     */
-    private function getPromotionCode()
-    {
-        try {
-            $url = $this->chatbotUri.'/hood-dashboard/api/power-shop/promo-code';
-
-            if (config('app.env') == 'local') $response = Http::withOptions(['verify' => false,])->get($url);
-            else $response = Http::get($url);
-
-            if($response->status() == 200) {
-                $this->mapPromotionCode(json_decode($response->body(), true));
-            }
-        } catch (\Exception $e) {
-            Log::warning('No promotion code is found'.$e->getMessage());
-        }
-    }
-
-    /**
-     * @param array|null $promotionData
-     */
-    private function mapPromotionCode(?array $promotionData): void
-    {
-        $this->gasPromotionData = data_get($promotionData, 'gas', []);
-        $this->elePromotionData = data_get($promotionData, 'electricity', []);
-    }
-
-    /**
-     * @param $state
-     * @param string $service
-     * @return null|string
-     */
-    private function getPromo($app, string $service) : null| string
-    {
-        $promo = '';
-
-        $state = $app->state;
-        $postcode = $app->postcode;
-
-        $conService = ConnectionService::where('connection_application_id', $app->id)
-            ->where('provider_name', ConnectionService::PROVIDER_POWER_SHOP)
-            ->where('service_type', $service)
-            ->first();
-
-        if ($conService) {
-            $plan_name = $conService->plan_type;
-
-            if ($service == ConnectionService::TYPE_ELECTRICITY && !empty($this->elePromotionData)){
-                $nmi_prefix = substr($app->nmi, 0, 3);
-
-                foreach($this->elePromotionData as $data){
-                    if ((in_array($state, $data['state']) || in_array($this->stateMap($state), $data['state'])) &&
-                        in_array($plan_name, $data['plan_name']) &&
-                        in_array($postcode, $data['postcode'])
-                    ){
-                        if (empty($promo) || in_array($nmi_prefix, $data['nmi_prefix'])){
-                            $promo = $data['promo_code'];
-                        }
-                    }
-                }
-            }
-
-            if ($service == ConnectionService::TYPE_GAS && !empty($this->gasPromotionData)){
-
-                foreach($this->gasPromotionData as $data){
-                    if ((in_array($state, $data['state']) || in_array($this->stateMap($state), $data['state'])) &&
-                        in_array($plan_name, $data['plan_name']) &&
-                        in_array($postcode, $data['postcode'])
-                    ){
-                        $promo = $data['promo_code'];
-                    }
-                }
-            }
-        }
-
-        return $promo;
-    }
-
-    /**
-     * @param $state
-     * @return mixed|string
-     */
-    private function stateMap($state)
-    {
-        $stateList = ['New South Wales'=>'NSW','Victoria'=>'VIC','Queensland'=>'QLD',
-            'South Australia'=>'SA','Northern Territory'=>'NT','TAS'=>'Tasmania','ACT'=>'Australian Capital Territory','WA' => 'Western Australia'];
-        if(array_key_exists($state, $stateList))
-        {
-            return $stateList[$state];
-        }
-        return $state;
-
-    }
-
-    /**
-     * @param $app
-     * @return string
-     */
-    private function getPowerShopCafToken($app): string
-    {
-        return (string) $app?->powershopPaymentInfo?->px_dps_billing_id;
-    }
-
-    /**
      * @param $app
      * @return string|null
      */
@@ -646,8 +352,9 @@ class OriginExporter
      */
     private function getElectricityPlanName(ConnectionApplication $app):string
     {
-        $electricity = ConnectionService::query()->where('connection_application_id', $app->id)
-            ->where('service_type', ConnectionService::TYPE_ELECTRICITY)->first();
+        $electricity = $app->connectionServices->filter(function ($svc) {
+            $svc->service_type === ConnectionService::TYPE_ELECTRICITY;
+            })->first();
 
         if(empty($electricity) || empty($electricity->plan_type)) {
             return '';
@@ -661,8 +368,10 @@ class OriginExporter
      */
     private function getGasPlanName(ConnectionApplication $app): string
     {
-        $gas = ConnectionService::query()->where('connection_application_id', $app->id)
-            ->where('service_type', ConnectionService::TYPE_GAS)->first();
+        $gas = $app->connectionServices->filter(function ($svc) {
+            $svc->service_type === ConnectionService::TYPE_GAS;
+        })->first();
+
         if(empty($gas)) {
             return '';
         }
@@ -683,8 +392,52 @@ class OriginExporter
         return $filteredCode ?? "";
     }
 
-    public function getCollection(){
+    public function getCollection(): \Illuminate\Support\Collection
+    {
         return collect($this->mappedApplicationList);
+    }
+
+    /**
+     * @param mixed $app
+     * @return string
+     */
+    private function setDateOfSales(mixed $app):string
+    {
+        $electricySubmittedDate = '';
+        $GasSubmittedDate = '';
+
+        foreach ($app->connectionServices as $svc) {
+            if($svc->service_type === ConnectionService::TYPE_ELECTRICITY && !empty($svc->submitted_at)) {
+                $electricySubmittedDate = date('d/m/Y', strtotime($svc->submitted_at));
+            }
+            if($svc->service_type === ConnectionService::TYPE_GAS && !empty($svc->submitted_at)) {
+                $GasSubmittedDate = date('d/m/Y', strtotime($svc->submitted_at));
+            }
+        }
+
+        $multipleSubmissionDate = [];
+        if(!empty($electricySubmittedDate)) {
+            $multipleSubmissionDate[] = $electricySubmittedDate;
+        }
+        if(!empty($GasSubmittedDate)) {
+            $multipleSubmissionDate[] = $GasSubmittedDate;
+        }
+        return join(',', $multipleSubmissionDate);
+
+    }
+
+
+    private function loadCampaignData()
+    {
+        $campaignUrl = config('bot.root_url').'/hood-dashboard/api/origin-campaign-codes';
+        $headers = ["Accept" => "application/json",];
+        $response = Http::withOptions([
+            "headers" => $headers,
+            "verify" => false,
+        ])->get($campaignUrl);
+
+        $response->throw();
+        $this->campaignInfo = json_decode($response->body(), true);
     }
 
 
