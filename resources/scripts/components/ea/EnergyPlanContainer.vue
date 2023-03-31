cbcdf02480929baa6b856e29ea7895c4095fc54f
shakil-enkaizen <shakil.hossain@enkaizen.com>
2023-03-31 12:51:18 +0600

    [HS-1002] EA Compliance: change color

--- resources/scripts/components/ea/EnergyPlanContainer.vue
+++ resources/scripts/components/ea/EnergyPlanContainer.vue
@@ -239,7 +239,7 @@
                         $ {{ planDetails.connection_fees.gas }} <small>(Incl. GST)</small>
                         <!-- <v-icon small> mdi mdi-alert-circle</v-icon> -->
                     </p>
-                    <p v-if="planDetails.distributor_name.gas === 'Envestra VIC'" class="line-height-18t mb-2">
+                    <p v-if="planDetails.distributor_name.gas === 'Envestra VIC'" class="line-height-18 mb-2">
                         The connection fee for your gas is up to $15.07 including GST, which will appear on your first bill.
                     </p>
                     <p v-if="isPlanHasElectricity" class="mb-2"><span class="font-weight-bold">Electricity Connection Fee: </span>
