5482bc3c2f615398b6d253ab9d80295cd96c99c0
adamanis98 <adam@hood.ai>
2023-04-05 16:35:39 +1000

    [HS-939] add response body in external lead api log table

--- app/Modules/ExternalLead/Services/SaveRawData.php
+++ app/Modules/ExternalLead/Services/SaveRawData.php
@@ -15,7 +15,7 @@ class SaveRawData
     {
         $newDump = new ExternalLeadApiLog();
         $newDump->lead_id = $data['lead_reference'] ?? null;
-        $newDump->all_fields_dump = json_encode($data);
+        $newDump->api_request_body = json_encode($data);
         $newDump->external_source_id = $externalSourceId;
 //        $newDump->agent_name = !empty($data['agent_firstname']) ? ($data['agent_firstname'] . ($data['agent_lastname'] ? " " . $data['agent_lastname'] : '')) : null;
         $newDump->agency_name = $data['agency']['agency_name'] ?? null;
